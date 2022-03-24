e04182feeac21c7bc6dd697afa0cabdc47ab9a54
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-24 08:07:42 -0400

    Time: 4 ms (83.21%), Space: 4.4 MB (88.06%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	var result [][]int
+	dfs(root,targetSum, 0, nil, &result)
+	return result
+}
+func dfs(root *TreeNode, targetSum, cs int, paths []int, result *[][]int) {
+	if root == nil {
+		return
+	}
+	cs += root.Val
+	paths = append(paths, root.Val)
+
+	if cs == targetSum && root.Left == nil && root.Right == nil {
+		*result = append(*result, append([]int{}, paths...))
+        return
+	}
+
+	dfs(root.Left, targetSum, cs, paths, result)
+	dfs(root.Right, targetSum, cs, paths, result)
+}
