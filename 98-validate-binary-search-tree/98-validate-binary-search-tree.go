f0c6276e8770cd4c3eded2a99b8cbd6f8c12341c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-25 16:14:49 -0400

    Time: 7 ms (58.97%), Space: 5.2 MB (84.34%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.go
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/*
+    Validate BST == inorder traversal
+    
+    which means, if we perform a inorder traversal on a valid BST,
+    then nodes will be processed in a sorted manner
+    
+    So how do we verify that there sorted? Sure we can visit them but how to verify?
+    
+    appraoch 1:
+    store each visited node in a list
+    and compare the list
+    
+    time:
+    space: o(h) + o(n)
+    
+    
+    approach 2:
+    Just like LinkedLists, maintain a prev pointer.
+    Previous pointer here is the previous node for a current node in "inorder" fashion.
+    Which means go all the left first
+    Then set the prev
+    Then use the prev to compare with next root node
+    if its a valid bst, the prev must be smaller each time its compared with a next inorder root node
+    ( which also means, if its valid, update the prev to current so the next inorder can compare its value with the correct prev one. )
+    
+    
+
+*/
+func isValidBST(root *TreeNode) bool {
+	b := &bst{
+		prev: nil,
+		flag: true,
+	}
+	b.dfs(root)
+	return b.flag
+}
+
+type bst struct {
+	prev *TreeNode
+	flag bool
+}
+
+func (b *bst) dfs(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	b.dfs(root.Left)
+	if b.prev != nil {
+		if b.prev.Val >= root.Val {
+			b.flag = false
+		}
+	}
+	b.prev = root
+	b.dfs(root.Right)
+}
