e81f800328c53d43ed32acfd76c84b21e7d9ead1
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-26 17:06:07 -0400

    Time: 8 ms (38.06%), Space: 7 MB (25.61%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.go
+++ 113-path-sum-ii/113-path-sum-ii.go
@@ -1,27 +1,58 @@
-func pathSum(root *TreeNode, targetSum int) [][]int {
-	var result [][]int
-    inorderDfs(root,targetSum,nil, &result)
-	return result
-}
-func inorderDfs(root *TreeNode, targetSum int, paths []int, result *[][]int) {
-	if root == nil {
-		return
-	}
-    
-	targetSum -= root.Val
-    paths = append(paths, root.Val)
+// func pathSum(root *TreeNode, targetSum int) [][]int {
+// 	var result [][]int
+//     inorderDfs(root,targetSum,nil, &result)
+// 	return result
+// }
+// func inorderDfs(root *TreeNode, targetSum int, paths []int, result *[][]int) {
+// 	if root == nil {
+// 		return
+// 	}
+//         fmt.Println("root: ", root.Val)
+//     fmt.Printf("before adding root -- paths: %v and slice mem address: %p len: %d and cap: %d\n", paths, paths, len(paths), cap(paths) )
 
-    
-    inorderDfs(root.Left, targetSum, paths, result)
-    
-    
-    if targetSum == 0 && root.Left == nil && root.Right == nil {
-        newPath := make([]int, len(paths))
-        copy(newPath, paths)
-		*result = append(*result, newPath)
-	}
-    
-    
-    inorderDfs(root.Right, targetSum, paths, result)
+// 	targetSum -= root.Val
+//     paths = append(paths, root.Val)
 
+//     fmt.Printf("after adding root -- paths: %v and slice mem address: %p len: %d and cap: %d\n", paths, paths, len(paths), cap(paths) )
+
+//     inorderDfs(root.Left, targetSum, paths, result)
+    
+    
+//     if targetSum == 0 && root.Left == nil && root.Right == nil {
+//         newPath := make([]int, len(paths))
+//         copy(newPath, paths)
+// 		*result = append(*result, newPath)
+// 	}
+    
+    
+//     inorderDfs(root.Right, targetSum, paths, result)
+
+
+// }
+
+
+func pathSum(root *TreeNode, sum int) [][]int {
+    var paths [][]int
+
+    if root == nil {
+        return paths
+    }
+
+    if root.Left == nil && root.Right == nil {
+        if sum == root.Val {
+            return append(paths, []int{ root.Val })
+        }
+
+        return paths
+    }
+
+    for _, path := range pathSum(root.Left, sum - root.Val) {
+        paths = append(paths, append([]int{ root.Val}, path... ))
+    }
+
+    for _, path := range pathSum(root.Right, sum - root.Val) {
+        paths = append(paths, append([]int{ root.Val}, path... ))
+    }
+
+    return paths
 }
