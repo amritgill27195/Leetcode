c21debf2a078aa5bcb92e7425d6cdec1a4a3b6c5
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-26 14:45:45 -0400

    Time: 9 ms (28.98%), Space: 4.6 MB (74.20%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.go
+++ 113-path-sum-ii/113-path-sum-ii.go
@@ -8,27 +8,26 @@
  */
 func pathSum(root *TreeNode, targetSum int) [][]int {
 	var result [][]int
-    inorderDfs(root,targetSum, []int{}, &result)
+    inorderDfs(root,targetSum,nil, &result)
 	return result
 }
 func inorderDfs(root *TreeNode, targetSum int, paths []int, result *[][]int) {
 	if root == nil {
 		return
 	}
-    // fmt.Println("Starting node: ",root.Val)
+    
 	targetSum -= root.Val
-	paths = append(paths, root.Val)
+    // fmt.Printf("Root: %v\n", root.Val)
+    // fmt.Printf("Current paths: %v, and slice mem addr: %p\n",paths, paths)
+    paths = append(paths, root.Val)
+        // fmt.Printf("Current paths: %v, and slice mem addr: %p\n",paths, paths)
     
     inorderDfs(root.Left, targetSum, paths, result)
     
-    // fmt.Println("Back to: ", root.Val)
-    // fmt.Println("Paths on this node are: ", paths)
-
-    
     if targetSum == 0 && root.Left == nil && root.Right == nil {
-        // fmt.Println("Adding the following path to result: ", paths)
         newPath := make([]int, len(paths))
         copy(newPath, paths)
+        
 		*result = append(*result, newPath)
 	}
     
