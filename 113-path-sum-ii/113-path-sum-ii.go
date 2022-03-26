a7f4bf11526bc61d2a60eae5b9551680df16d914
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-26 17:07:11 -0400

    Time: 0 ms (100.00%), Space: 4.5 MB (86.51%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.go
+++ 113-path-sum-ii/113-path-sum-ii.go
@@ -1,58 +1,27 @@
-// func pathSum(root *TreeNode, targetSum int) [][]int {
-// 	var result [][]int
-//     inorderDfs(root,targetSum,nil, &result)
-// 	return result
-// }
-// func inorderDfs(root *TreeNode, targetSum int, paths []int, result *[][]int) {
-// 	if root == nil {
-// 		return
-// 	}
-//         fmt.Println("root: ", root.Val)
-//     fmt.Printf("before adding root -- paths: %v and slice mem address: %p len: %d and cap: %d\n", paths, paths, len(paths), cap(paths) )
-
-// 	targetSum -= root.Val
-//     paths = append(paths, root.Val)
-
-//     fmt.Printf("after adding root -- paths: %v and slice mem address: %p len: %d and cap: %d\n", paths, paths, len(paths), cap(paths) )
-
-//     inorderDfs(root.Left, targetSum, paths, result)
-    
-    
-//     if targetSum == 0 && root.Left == nil && root.Right == nil {
-//         newPath := make([]int, len(paths))
-//         copy(newPath, paths)
-// 		*result = append(*result, newPath)
-// 	}
-    
-    
-//     inorderDfs(root.Right, targetSum, paths, result)
-
-
-// }
-
-
-func pathSum(root *TreeNode, sum int) [][]int {
-    var paths [][]int
-
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	var result [][]int
+    inorderDfs(root,targetSum,nil, &result)
+	return result
+}
+func inorderDfs(root *TreeNode, targetSum int, paths []int, result *[][]int) {
+	// BASE
     if root == nil {
-        return paths
-    }
+		return
+	}
 
-    if root.Left == nil && root.Right == nil {
-        if sum == root.Val {
-            return append(paths, []int{ root.Val })
-        }
+    
+    // logic
+	targetSum -= root.Val
+    paths = append(paths, root.Val)
 
-        return paths
-    }
+    inorderDfs(root.Left, targetSum, paths, result)
+        
+    if targetSum == 0 && root.Left == nil && root.Right == nil {
+        newPath := make([]int, len(paths))
+        copy(newPath, paths)
+		*result = append(*result, newPath)
+	}    
+    inorderDfs(root.Right, targetSum, paths, result)
 
-    for _, path := range pathSum(root.Left, sum - root.Val) {
-        paths = append(paths, append([]int{ root.Val}, path... ))
-    }
 
-    for _, path := range pathSum(root.Right, sum - root.Val) {
-        paths = append(paths, append([]int{ root.Val}, path... ))
-    }
-
-    return paths
 }
