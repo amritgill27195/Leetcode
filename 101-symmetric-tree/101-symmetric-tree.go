9d0d9a3c46e4553a5f349ec8a58734ff7c82a979
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-26 20:02:17 -0400

    Time: 9 ms (7.57%), Space: 2.9 MB (83.88%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.go
+++ 101-symmetric-tree/101-symmetric-tree.go
@@ -18,33 +18,33 @@ func isSymmetric(root *TreeNode) bool {
 }
 
 // preorder dfs ( processing first, then left and right )
-// func dfs(a *TreeNode, b *TreeNode) bool {
-//     if a == nil && b == nil {
-//         return true
-//     }
-//     if a == nil || b == nil || a.Val != b.Val {return false}
-    
-//     return dfs(a.Left, b.Right) && dfs(a.Right, b.Left)
-// }
-
-// inorder dfs ( go left, then process, and then go right )
-// this is slightly messy, I prefer preorder in this case 
 func dfs(a *TreeNode, b *TreeNode) bool {
-    // base
     if a == nil && b == nil {
         return true
     }
-    if a == nil || b == nil {
-        return false 
-    }
+    if a == nil || b == nil || a.Val != b.Val {return false}
     
-    // logic
-    if left := dfs(a.Left, b.Right); !left {
-        return false // no need to go to next if this is already failing
-    }
-    if a == nil || b == nil || a.Val != b.Val {
-        return false
-    }
-    return dfs(a.Right, b.Left)
-    
+    return dfs(a.Left, b.Right) && dfs(a.Right, b.Left)
 }
+
+// inorder dfs ( go left, then process, and then go right )
+// this is slightly messy, I prefer preorder in this case 
+// func dfs(a *TreeNode, b *TreeNode) bool {
+//     // base
+//     if a == nil && b == nil {
+//         return true
+//     }
+//     if a == nil || b == nil {
+//         return false 
+//     }
+    
+//     // logic
+//     if left := dfs(a.Left, b.Right); !left {
+//         return false // no need to go to next if this is already failing
+//     }
+//     if a == nil || b == nil || a.Val != b.Val {
+//         return false
+//     }
+//     return dfs(a.Right, b.Left)
+    
+// }
