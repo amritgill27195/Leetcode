e89967abd81956183723e449216c5243b45b3aa9
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-28 10:48:22 -0400

    Time: 27 ms (74.06%), Space: 10.2 MB (39.62%) - LeetHub

---
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.go
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/*
+    We need 1 additional ptr that keeps track of our current inorder position
+    (i.e: where are we currently in the inorder traversal )
+*/
+type BSTIterator struct {
+    stack []*TreeNode
+}
+
+
+func Constructor(root *TreeNode) BSTIterator {
+    b := BSTIterator{ stack : []*TreeNode{}}
+    b.inorder(root)
+    return b
+}
+
+func (this *BSTIterator) inorder(root *TreeNode) {
+    for root != nil {
+        this.stack = append(this.stack, root)
+        root = root.Left
+    }
+}
+
+
+func (this *BSTIterator) Next() int {
+    if this.HasNext() {
+        top := this.stack[len(this.stack)-1]
+        this.stack = this.stack[:len(this.stack)-1]
+        this.inorder(top.Right)
+        return top.Val
+    }
+    return -1
+}
+
+
+func (this *BSTIterator) HasNext() bool {
+    return len(this.stack) != 0
+}
+
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Next();
+ * param_2 := obj.HasNext();
+ */
