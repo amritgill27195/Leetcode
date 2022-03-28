8fd448dea730a7ae257674e5dd82e6a66add3c3e
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-27 22:48:23 -0400

    Time: 69 ms (10.19%), Space: 7.4 MB (15.05%) - LeetHub

---
+++ 704-binary-search/704-binary-search.go
@@ -0,0 +1,30 @@
+
+/*
+    classic binary search:
+    time: o(logn)
+    space: o(1)
+*/
+
+func search(nums []int, target int) int {
+    if nums == nil || len(nums) == 0 || target < nums[0] || target > nums[len(nums)-1] {
+        return -1
+    }
+    
+    left := 0
+    right := len(nums)-1
+    
+    for left <= right {
+        mid := left + ((right-left)/2)
+        if nums[mid] == target{
+            return mid
+        } else if target > nums[mid] {
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    
+    return -1
+}
+
+    
