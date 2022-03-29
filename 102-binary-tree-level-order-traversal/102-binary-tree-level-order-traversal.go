51af453f476dbc750e8d4a07d850dea8685122f4
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-29 17:54:19 -0400

    Time: 0 ms (100.00%), Space: 2.9 MB (27.47%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.go
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.go
@@ -6,27 +6,66 @@
  *     Right *TreeNode
  * }
  */
-func levelOrder(root *TreeNode) [][]int {
-    if root == nil {
-        return nil
-    }
-    out := [][]int{}
-    queue := []*TreeNode{root}
-    
-    for len(queue) != 0 {
-        qSize := len(queue)
-        cl := []int{}
-        for qSize != 0 {
-            dq := queue[0]
-            queue = queue[1:]
-            cl = append(cl, dq.Val)
-            if dq.Left != nil {queue = append(queue, dq.Left)}
-            if dq.Right != nil {queue = append(queue, dq.Right)}
 
-            qSize--
-        }
-        out = append(out, cl)
-    }
-    return out
+// iterative using BFS
+// time: o(n)
+// space: o(maxWidthOfTheTree)
+// func levelOrder(root *TreeNode) [][]int {
+//     if root == nil {
+//         return nil
+//     }
+//     out := [][]int{}
+//     queue := []*TreeNode{root}
+    
+//     for len(queue) != 0 {
+//         qSize := len(queue)
+//         cl := []int{}
+//         for qSize != 0 {
+//             dq := queue[0]
+//             queue = queue[1:]
+//             cl = append(cl, dq.Val)
+//             if dq.Left != nil {queue = append(queue, dq.Left)}
+//             if dq.Right != nil {queue = append(queue, dq.Right)}
+
+//             qSize--
+//         }
+//         out = append(out, cl)
+//     }
+//     return out
+// }
+
+
+
+// recursive using DFS
+// time: o(n)
+// space: o(h) -- at worse, we will have the max height number of nodes in our recursion stack
+
+type sol struct {
+    result [][]int
 }
 
+func levelOrder(root *TreeNode) [][]int{
+    sol := new(sol)
+    sol.result = [][]int{}
+    sol.preorderDfs(root, 0)
+    return sol.result
+}
+
+
+func (s *sol) preorderDfs(root *TreeNode, level int) {
+    
+    // base
+    if root == nil {
+        return
+    }
+    
+    //logic
+    if len(s.result) == level {
+        s.result = append(s.result, []int{})
+    }
+    s.result[level] = append(s.result[level], root.Val)
+    level = level+1
+    
+    s.preorderDfs(root.Left, level)
+    s.preorderDfs(root.Right, level)
+}
