fc23e7217bbf43f965e0e6e851befc53904a57df
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-31 12:19:27 -0400

    Time: 16 ms (10.41%), Space: 4.2 MB (80.25%) - LeetHub

--- 733-flood-fill/733-flood-fill.go
+++ 733-flood-fill/733-flood-fill.go
@@ -24,70 +24,65 @@
 
 
 // using level order with BFS
-// func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
-    
-//     if image == nil || len(image) == 0 {
-//         return image
-//     }
-//     newColor *= -1
-//     queue := [][]int{ {sr,sc} } // [ [r,c] ]
-//     oldColor := image[sr][sc]
-//     dirs := [][]int{ {1,0},{-1,0},{0,1},{0,-1} }
-//     image[sr][sc] = newColor
-//     m := len(image)
-//     n := len(image[0])
-    
-//     for len(queue) != 0 {
-//         dq := queue[0]
-//         queue = queue[1:]
-        
-//         for _, dir := range dirs {
-//             r := dq[0] + dir[0]
-//             c := dq[1] + dir[1]
-//             if r >= 0 && r < m && c >= 0 && c < n && image[r][c] == oldColor {
-//                 image[r][c] = newColor
-//                 queue = append(queue, []int{r,c})
-//             }
-//         }   
-//     }
-    
-//     for i := 0; i < m; i++ {
-//         for j := 0; j < n; j++ {
-//             if image[i][j] == newColor {
-//                 image[i][j] *= -1
-//             }
-//         }
-//     }
-//     return image
-    
-// }
 func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
+    
     if image == nil || len(image) == 0 {
         return image
     }
-
     oldColor := image[sr][sc]
+    
     if oldColor != newColor {
-    
-    
-        var dfs func(img [][]int, r, c, oc, nc int)
-        dfs = func(img [][]int, r, c, oc, nc int){
+        queue := [][]int{ {sr,sc} } // [ [r,c] ]
+        dirs := [][]int{ {1,0},{-1,0},{0,1},{0,-1} }
+        image[sr][sc] = newColor
+        m := len(image)
+        n := len(image[0])
 
-            // base
-            // boundary checks
-            if img == nil || r < 0 || r >= len(img) || c < 0 || c >= len(img[0])  {return}
-            // color checks
-            if img[r][c] != oc {return}
+        for len(queue) != 0 {
+            dq := queue[0]
+            queue = queue[1:]
 
-            // logic
-            img[r][c] = nc
-            dfs(img, r+1,c,oc, nc)
-            dfs(img, r-1,c,oc, nc)
-            dfs(img, r,c+1,oc, nc)
-            dfs(img, r,c-1,oc, nc)    
+            for _, dir := range dirs {
+                r := dq[0] + dir[0]
+                c := dq[1] + dir[1]
+                if r >= 0 && r < m && c >= 0 && c < n && image[r][c] == oldColor {
+                    image[r][c] = newColor
+                    queue = append(queue, []int{r,c})
+                }
+            }
         }
-        dfs(image, sr, sc, oldColor, newColor)
     }
-        
+    
     return image
 }
+
+// func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
+//     if image == nil || len(image) == 0 {
+//         return image
+//     }
+
+//     oldColor := image[sr][sc]
+//     if oldColor != newColor {
+    
+    
+//         var dfs func(img [][]int, r, c, oc, nc int)
+//         dfs = func(img [][]int, r, c, oc, nc int){
+
+//             // base
+//             // boundary checks
+//             if img == nil || r < 0 || r >= len(img) || c < 0 || c >= len(img[0])  {return}
+//             // color checks
+//             if img[r][c] != oc {return}
+
+//             // logic
+//             img[r][c] = nc
+//             dfs(img, r+1,c,oc, nc)
+//             dfs(img, r-1,c,oc, nc)
+//             dfs(img, r,c+1,oc, nc)
+//             dfs(img, r,c-1,oc, nc)    
+//         }
+//         dfs(image, sr, sc, oldColor, newColor)
+//     }
+        
+//     return image
+// }
