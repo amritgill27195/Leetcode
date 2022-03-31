cb0d44f876458ab12c0a9c1b9f36164ded0cb59b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-03-31 11:59:45 -0400

    Time: 10 ms (41.29%), Space: 4.1 MB (80.25%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.go
@@ -0,0 +1,55 @@
+
+/*
+    Connected components  == BFS/DFS
+    
+    approach : using BFS
+    - We need ALL the starting independent nodes
+    - Luckily we are given a starting point ( sr, sc )
+    - Enqueue that starting
+    - Save the color we are changing ( so we can compare with its neighbors )
+    - Change the sr,sc to new color
+    - Start processing the queue
+    - As soon as we add an item to our queue, we will change the color right away to newColor
+    - So that we do not visit this node again
+    - if we are not allowed to modify the matrix ( save the visited poins in a set )
+    time: o(mn)
+    space: o(mn) - for the queue
+*/
+
+func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
+    
+    if image == nil || len(image) == 0 {
+        return image
+    }
+    newColor *= -1
+    queue := [][]int{ {sr,sc} } // [ [r,c] ]
+    oldColor := image[sr][sc]
+    dirs := [][]int{ {1,0},{-1,0},{0,1},{0,-1} }
+    image[sr][sc] = newColor
+    m := len(image)
+    n := len(image[0])
+    
+    for len(queue) != 0 {
+        dq := queue[0]
+        queue = queue[1:]
+        
+        for _, dir := range dirs {
+            r := dq[0] + dir[0]
+            c := dq[1] + dir[1]
+            if r >= 0 && r < m && c >= 0 && c < n && image[r][c] == oldColor {
+                image[r][c] = newColor
+                queue = append(queue, []int{r,c})
+            }
+        }   
+    }
+    
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if image[i][j] == newColor {
+                image[i][j] *= -1
+            }
+        }
+    }
+    return image
+    
+}
