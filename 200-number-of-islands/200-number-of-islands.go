7e3acd82f7f29b578cc664a29185313107addec7
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-01 15:52:36 -0400

    Time: 15 ms (20.72%), Space: 7 MB (14.99%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.go
+++ 200-number-of-islands/200-number-of-islands.go
@@ -10,23 +10,23 @@ func numIslands(grid [][]byte) int {
     dirs := [][]int{ {0,-1}, {0,1}, {-1, 0}, {1,0} }
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if grid[i][j] == 49 {
+            if grid[i][j] == '1' {
                 q = append(q, []int{i,j})    
+                grid[i][j] = '0' // mark this land visited
                 for len(q) != 0 {
                     dq := q[0]
                     q = q[1:]
                     for _, dir := range dirs {
                         r := dq[0] + dir[0]
                         c := dq[1] + dir[1]
-                        if r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 49 {
-                            grid[r][c] = 48
+                        if r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1' {
+                            grid[r][c] = '0'
                             q = append(q, []int{r,c})
                         }
                     }
                 }
                 count++
             }
-            grid[i][j] = 48
         }
     }
     return count 
