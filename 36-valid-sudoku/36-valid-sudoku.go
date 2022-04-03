adfef298393f72a0c72a2a1c3bc250a831e1f5d5
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-03 11:55:39 -0400

    Time: 0 ms (100.00%), Space: 2.9 MB (43.03%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.go
+++ 36-valid-sudoku/36-valid-sudoku.go
@@ -5,37 +5,38 @@ func isValidSudoku(board [][]byte) bool {
     boxSet := make([]map[byte]struct{}, m)
     for i := 0; i < m; i++ {
         rowSet := map[byte]struct{}{}
+        colSet := map[byte]struct{}{}
         for j := 0; j < n; j++ {
             
-            if board[i][j] == '.' {continue}
-            val := board[i][j]
-            
-            if _, ok := rowSet[val]; ok {
-                return false
+            if board[i][j] != '.' {
+                val := board[i][j]
+                if _, ok := rowSet[val]; ok {
+                    return false
+                }
+                rowSet[board[i][j]] = struct{}{}
+                
+                boxIdx := (i/3) * 3 + (j/3)
+                if _, ok := boxSet[boxIdx][val]; ok {
+                    return false
+                }
+                if boxSet[boxIdx] == nil {boxSet[boxIdx] = map[byte]struct{}{}}
+                boxSet[boxIdx][val] = struct{}{}
             }
-            rowSet[board[i][j]] = struct{}{}
             
             
-            boxIdx := (i/3) * 3 + (j/3)
-            if _, ok := boxSet[boxIdx][val]; ok {
-                return false
+            if string(board[j][i]) != "." {
+                if _, ok := colSet[board[j][i]]; ok {
+                    return false
+                }
+                colSet[board[j][i]] = struct{}{}
             }
-            if boxSet[boxIdx] == nil {boxSet[boxIdx] = map[byte]struct{}{}}
-            boxSet[boxIdx][val] = struct{}{}
+            
+            
+            
         }
     }
     
    
-    for i := 0; i < m; i++ {
-        colSet := map[byte]struct{}{}
-        for j := 0; j < n; j++ {
-            if string(board[j][i]) == "." {continue}
-            if _, ok := colSet[board[j][i]]; ok {
-                return false
-            }
-            colSet[board[j][i]] = struct{}{}
-        }
-    }
     
     return true
     
