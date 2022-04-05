5d5cac07437bb5e109e9ecf3c71e0e37822694ae
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-05 19:40:27 -0400

    Time: 13 ms (15.02%), Space: 6 MB (24.10%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.go
@@ -0,0 +1,28 @@
+func combinationSum(candidates []int, target int) [][]int {
+    
+    
+    var result [][]int
+    var helper func(c []int, target int, i int, paths []int)
+    helper = func(c []int, target int, i int, paths []int){
+        // base
+        if i >= len(c) || target < 0 {return}
+        
+        if target == 0 {
+            newPaths := make([]int, len(paths))
+            copy(newPaths, paths)
+            result = append(result, newPaths)
+            return
+        }
+        
+        // logic
+        // not choose
+        helper(c, target, i+1, paths)
+        
+        // choose
+        paths = append(paths, c[i])
+        helper(c, target - c[i], i, paths)
+    }
+    
+    helper(candidates, target, 0, []int{})
+    return result
+}
