313b28c852fde12a2708494ee720524070342ecf
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-05 21:32:11 -0400

    Time: 9 ms (27.51%), Space: 3.2 MB (67.51%) - LeetHub

--- 39-combination-sum/39-combination-sum.go
+++ 39-combination-sum/39-combination-sum.go
@@ -1,30 +1,61 @@
+// func combinationSum(candidates []int, target int) [][]int {
+    
+    
+//     var result [][]int
+//     var helper func(c []int, target int, i int, paths []int)
+//     helper = func(c []int, target int, i int, paths []int){
+//         // base
+//         if i >= len(c) || target < 0 {return}
+        
+//         if target == 0 {
+//             newPaths := make([]int, len(paths))
+//             copy(newPaths, paths)
+//             result = append(result, newPaths)
+//             return
+//         }
+        
+//         // logic
+//         // not choose
+//         helper(c, target, i+1, paths)
+        
+//         // choose
+//         paths = append(paths, c[i])
+//         helper(c, target - c[i], i, paths)
+        
+//         paths = paths[:len(paths)-1]
+//     }
+    
+//     helper(candidates, target, 0, []int{})
+//     return result
+// }
+
+
+
 func combinationSum(candidates []int, target int) [][]int {
     
     
     var result [][]int
-    var helper func(c []int, target int, i int, paths []int)
-    helper = func(c []int, target int, i int, paths []int){
-        // base
-        if i >= len(c) || target < 0 {return}
+    var helper func(c, paths []int, t, start int)
+    helper = func(c, paths []int, t, start int) {
         
-        if target == 0 {
-            newPaths := make([]int, len(paths))
-            copy(newPaths, paths)
-            result = append(result, newPaths)
+        // base
+        if t < 0 {return}
+        if t == 0 {
+            newL := make([]int, len(paths))
+            copy(newL, paths)
+            result = append(result, newL)
             return
         }
+    
         
         // logic
-        // not choose
-        helper(c, target, i+1, paths)
+        for i := start; i < len(c); i++ {
+            paths = append(paths, c[i])
+            helper(c, paths, t - c[i], i)
+            paths = paths[:len(paths)-1]
+        }
         
-        // choose
-        paths = append(paths, c[i])
-        helper(c, target - c[i], i, paths)
-        
-        paths = paths[:len(paths)-1]
     }
-    
-    helper(candidates, target, 0, []int{})
+    helper(candidates, []int{}, target, 0)
     return result
 }
