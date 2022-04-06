182781536e88d9a7018ccc8279b2bd464fc5b426
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-06 17:54:07 -0400

    Time: 0 ms (100.00%), Space: 1.9 MB (92.94%) - LeetHub

--- 394-decode-string/394-decode-string.go
+++ 394-decode-string/394-decode-string.go
@@ -8,16 +8,19 @@ func decodeString(s string) string {
     for i := 0; i < len(s); i++ {
         if s[i] >= '0' && s[i] <= '9' {
             num = num * 10 + int(s[i]-'0')
-        } else if string(s[i]) == "[" {
-            numSt = append(numSt, num); num=0
-            strSt = append(strSt, curr); curr = new(strings.Builder)
-        } else if string(s[i]) == "]" {
+        } else if s[i] == '[' {
+            numSt = append(numSt, num)
+            strSt = append(strSt, curr)
+            num=0
+            curr = new(strings.Builder)
+        } else if s[i] == ']' {
             decoded := new(strings.Builder)
             k := numSt[len(numSt)-1]; numSt = numSt[:len(numSt)-1]
             for i := 0; i < k ; i++ {
                 decoded.WriteString(curr.String())
             }
-            curr = strSt[len(strSt)-1]; strSt = strSt[:len(strSt)-1]
+            curr = strSt[len(strSt)-1]
+            strSt = strSt[:len(strSt)-1]
             curr.WriteString(decoded.String())
         } else {
             curr.WriteByte(s[i])
