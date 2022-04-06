15d4951b4a04fdb26d136901a1fc1f1578d1443d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-06 19:40:14 -0400

    Time: 4 ms (70.54%), Space: 3.2 MB (67.83%) - LeetHub

--- 39-combination-sum/39-combination-sum.go
+++ 39-combination-sum/39-combination-sum.go
@@ -1,26 +1,60 @@
+// func combinationSum(candidates []int, target int) [][]int {
+//     result := [][]int{}
+    
+//     var dfs func (c []int, i int, t int, p []int)
+//     dfs = func(c []int,i int, t int, p []int) {
+//         // base
+//         if t == 0 {
+//             newL := make([]int, len(p))
+//             copy(newL, p)
+//             result = append(result, newL)
+//             return
+//         }
+//         if i >= len(c) || t < 0 {return}
+        
+//         // logic
+//         // not choose
+//         dfs(c, i+1, t, p)
+        
+//         // choose
+//         p = append(p, c[i])
+//         dfs(c, i, t-c[i], p)
+//         p = p[:len(p)-1]
+//     }
+//     dfs(candidates, 0, target, nil)
+//     return result
+// }
+
+
 func combinationSum(candidates []int, target int) [][]int {
     result := [][]int{}
-    
-    var dfs func (c []int, i int, t int, p []int)
-    dfs = func(c []int,i int, t int, p []int) {
-        // base
+    var backtrack func(c []int, t int, start int, paths []int)
+    backtrack = func(c []int, t int, start int, paths []int) {
+        
+        // base 
         if t == 0 {
-            newL := make([]int, len(p))
-            copy(newL, p)
+            newL := make([]int, len(paths))
+            copy(newL, paths)
             result = append(result, newL)
             return
         }
-        if i >= len(c) || t < 0 {return}
+        if t < 0 || start >= len(c) {
+            return
+        }
         
         // logic
-        // not choose
-        dfs(c, i+1, t, p)
+        for i := start; i<len(c); i++ {
+            // action
+            paths = append(paths, c[i])
+            // recurse
+            backtrack(c, t - c[i], i, paths)
+            // backtrack
+            paths = paths[:len(paths)-1]
+        }
         
-        // choose
-        p = append(p, c[i])
-        dfs(c, i, t-c[i], p)
-        p = p[:len(p)-1]
     }
-    dfs(candidates, 0, target, nil)
+    
+    backtrack(candidates, target, 0, nil)
+    
     return result
 }
