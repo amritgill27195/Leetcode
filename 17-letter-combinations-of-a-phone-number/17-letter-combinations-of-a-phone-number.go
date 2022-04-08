f630ca0fdb0d51e1cdc1d38fbf0fbdd616e42d40
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-08 18:18:38 -0400

    Time: 3 ms (27.81%), Space: 2.1 MB (16.58%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.go
@@ -0,0 +1,35 @@
+func letterCombinations(digits string) []string {
+    if len(digits) == 0 {return nil}
+    digMap := map[string][]string{
+        "2": []string{"a","b","c"},
+        "3": []string{"d", "e", "f"},
+        "4": []string{"g", "h", "i"},
+        "5": []string{"j", "k", "l"},
+        "6": []string{"m","n","o"},
+        "7": []string{"p", "q", "r","s"},
+        "8": []string{"t","u","v"},
+        "9": []string{"w","x","y", "z"},
+    }
+    var result []string
+    var dfs func(path string, start int)
+    dfs = func(path string, start int) {
+        // base
+        if len(path) == len(digits) {
+            result = append(result, path)
+            return
+        }
+        
+        
+        // logic
+        for i := start; i < len(digits); i++ {
+            digChar := string(digits[i])
+            for _, char := range digMap[digChar] {
+                path += string(char)
+                dfs(path, i+1)
+                path = string(path[:len(path)-1])
+            }
+        }
+    }
+    dfs("",0)
+    return result
+}
