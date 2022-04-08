a85ce810d55dada300e8c0b336eeebfe3a13853a
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-07 23:52:13 -0400

    Time: 0 ms (100.00%), Space: 2.2 MB (80.34%) - LeetHub

--- 78-subsets/78-subsets.go
+++ 78-subsets/78-subsets.go
@@ -6,43 +6,73 @@
     - At leaves, we will append the running path
     - ( new path each recursion or backtrack existing )
     - time: exponential ( o(2^n) where n is the number of elements in nums array )
-    - space: exponential
+    - space: o(h) the max height of our decision tree which looks like it will be len(arr)
     
     approach 2: for loop based recursion
-    - instead of looking for an answer at a leaf node,
-    - each path at every node is answer
+    - instead of looking for an answer at a leaf node or when target == 0, we dont have such constraints here
+    - each path at every node is an answer
+    - time: exponential o(2^n)
+    - space: o(h) the max height of our decision tree which looks like it will be len(arr)
+    
+    approach 3: no recursion
+    - Start with [[]] list
+    - For each number in nums,
+        - For each list in result,
+            create a new list with curr num + eachList contents
 */
 
+
 func subsets(nums []int) [][]int {
+    if nums == nil {
+        return nil
+    }
     
-    var result [][]int
-    var dfs func(start int, paths []int)
-    dfs = func(start int, paths []int) {
+    result := [][]int{{}}
+    for i := 0; i < len(nums); i++ {
         
-        // base
-        
-        newL := make([]int, len(paths))
-        copy(newL, paths)
-        result = append(result, newL)
-        if start >= len(nums) {return}
-        
-        
-        // logic
-        for i := start; i < len(nums); i++ {
-            // action
-            paths = append(paths, nums[i])
-            // recurse
-            dfs(i+1, paths)
-            // backtrack
-            paths = paths[:len(paths)-1]
+        for _, res := range result {
+            newRes := make([]int, len(res))
+            copy(newRes, res)
+            newRes = append(newRes, nums[i])
+            result = append(result, newRes)
         }
         
     }
-    dfs(0, []int{})
     return result
-    
 }
+
+
+// for loop based recursion
+// func subsets(nums []int) [][]int {
     
+//     var result [][]int
+//     var dfs func(start int, paths []int)
+//     dfs = func(start int, paths []int) {
+        
+//         // base 
+//         newL := make([]int, len(paths))
+//         copy(newL, paths)
+//         result = append(result, newL)
+//         if start >= len(nums) {return}
+        
+        
+//         // logic
+//         for i := start; i < len(nums); i++ {
+//             // action
+//             paths = append(paths, nums[i])
+//             // recurse
+//             dfs(i+1, paths)
+//             // backtrack
+//             paths = paths[:len(paths)-1]
+//         }
+        
+//     }
+//     dfs(0, []int{})
+//     return result
+    
+// }
+
+// 0/1 based recursion
 // func subsets(nums []int) [][]int {
 //     var result [][]int
 //     var dfs func(i int, paths []int)
