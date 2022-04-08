e4213a305f9fe6d56ee335e34439497aba1cb7b1
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-07 23:26:59 -0400

    Time: 3 ms (36.97%), Space: 2.3 MB (80.34%) - LeetHub

--- 78-subsets/78-subsets.go
+++ 78-subsets/78-subsets.go
@@ -5,39 +5,74 @@
     - we can only use the 1 num ONCE
     - At leaves, we will append the running path
     - ( new path each recursion or backtrack existing )
-
+    - time: exponential ( o(2^n) where n is the number of elements in nums array )
+    - space: exponential
+    
+    approach 2: for loop based recursion
+    - instead of looking for an answer at a leaf node,
+    - each path at every node is answer
 */
 
 func subsets(nums []int) [][]int {
+    
     var result [][]int
-    var dfs func(i int, paths []int)
-    dfs = func(i int, paths []int) {
-        
+    var dfs func(start int, paths []int)
+    dfs = func(start int, paths []int) {
         
         // base
-        if i == len(nums) {
-            newL := make([]int, len(paths))
-            copy(newL, paths)
-            result = append(result, newL)
-            return
-        }
+        
+        newL := make([]int, len(paths))
+        copy(newL, paths)
+        result = append(result, newL)
+        if start >= len(nums) {return}
+        
         
         // logic
+        for i := start; i < len(nums); i++ {
+            // action
+            paths = append(paths, nums[i])
+            // recurse
+            dfs(i+1, paths)
+            // backtrack
+            paths = paths[:len(paths)-1]
+        }
         
-        // not choose
-        dfs(i+1, paths)
-        
-    
-        // choose
-        // action
-        paths = append(paths, nums[i])
-        // recurse
-        dfs(i+1, paths)
-        // backtrack
-        paths = paths[:len(paths)-1]
     }
-    
-    dfs(0, nil)
+    dfs(0, []int{})
     return result
     
 }
+    
+// func subsets(nums []int) [][]int {
+//     var result [][]int
+//     var dfs func(i int, paths []int)
+//     dfs = func(i int, paths []int) {
+        
+        
+//         // base
+//         if i == len(nums) {
+//             newL := make([]int, len(paths))
+//             copy(newL, paths)
+//             result = append(result, newL)
+//             return
+//         }
+        
+//         // logic
+        
+//         // not choose
+//         dfs(i+1, paths)
+        
+    
+//         // choose
+//         // action
+//         paths = append(paths, nums[i])
+//         // recurse
+//         dfs(i+1, paths)
+//         // backtrack
+//         paths = paths[:len(paths)-1]
+//     }
+    
+//     dfs(0, nil)
+//     return result
+    
+// }
