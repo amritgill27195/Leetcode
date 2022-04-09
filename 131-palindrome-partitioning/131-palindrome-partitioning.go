b34661428aa4d67ec2bbf516a314a792ebe4e98b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-09 10:50:54 -0400

    Time: 334 ms (68.06%), Space: 19 MB (95.37%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.go
@@ -0,0 +1,42 @@
+func partition(s string) [][]string {
+    
+    var result [][]string
+    
+    var dfs func(start int, paths []string)
+    dfs = func(start int, paths []string){
+        // base
+        if start >= len(s) {
+            newL := make([]string, len(paths))
+            copy(newL, paths)
+            result = append(result, newL)
+            return
+        }
+        
+        // logic
+        for i := start; i < len(s); i++ {
+            subStr := string(s[start:i+1])
+            if isPalindrome(subStr) {
+                // action
+                paths = append(paths, subStr)
+                // recurse
+                dfs(i+1, paths)
+                // backtrack
+                paths = paths[:len(paths)-1]
+            }
+        }   
+    }
+    
+    dfs(0, nil)
+    return result
+}
+
+func isPalindrome(s string) bool {
+    for i := 0; i < len(s) ; i++ {
+        curChar := string(s[i])
+        lastChar := string(s[len(s)-i-1])
+        if curChar != lastChar {
+            return false
+        }
+    }
+    return true
+}
