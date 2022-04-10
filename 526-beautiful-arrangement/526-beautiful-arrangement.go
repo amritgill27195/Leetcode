c53ba12b7bd92e1be45d87260770566101097a0e
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-10 15:06:42 -0400

    Time: 68 ms (77.78%), Space: 1.9 MB (97.78%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.go
+++ 526-beautiful-arrangement/526-beautiful-arrangement.go
@@ -1,33 +1,31 @@
 func countArrangement(n int) int {
-    count := 0
     nums := make([]int, n)
-    for i := 0; i < n; i++ {
-        nums[i] = i+1
-    }
+	for i := 0; i < len(nums); i++ {
+		nums[i] = i + 1
+	}
     
-    var dfs func(start int)
-    dfs = func(start int) {
-        // base
-        if start == len(nums) {
-            count++
-            return
-        }
-        
-        //logic
-        for i := start; i < len(nums); i++ {
-            // check
-            if nums[i] % (start+1) == 0 || (start+1) % nums[i] == 0 {
-                // action
-                nums[i], nums[start] = nums[start], nums[i]
-                // recurse
-                dfs(start+1)
-                // backtrack
-                nums[i], nums[start] = nums[start], nums[i]
-            }
-        }
-    }
-    
-    dfs(0)
-    
-    return count
+    count := 0
+	var dfs func(start int)
+	dfs = func(start int) {
+		// base
+		if start >= n {
+			// fmt.Println(nums)
+			count++
+		}
+
+		// logic
+		for i := start; i < len(nums); i++ {
+			// check
+			if (start+1)%nums[i] == 0 || nums[i]%(start+1) == 0 {
+				// action
+				nums[start], nums[i] = nums[i], nums[start]
+				// recurse
+				dfs(start + 1)
+				// backtrack
+				nums[start], nums[i] = nums[i], nums[start]
+			}
+		}
+	}
+	dfs(0)
+	return count
 }
