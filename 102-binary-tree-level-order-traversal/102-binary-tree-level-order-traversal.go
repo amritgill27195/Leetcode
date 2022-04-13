63e4dd55bf03c9bf4ca454ecc38e720738ecb1c5
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-13 13:58:12 -0400

    Time: 0 ms (100.00%), Space: 2.7 MB (99.89%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.go
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.go
@@ -10,29 +10,27 @@
 // iterative using BFS
 // time: o(n)
 // space: o(maxWidthOfTheTree)
-// func levelOrder(root *TreeNode) [][]int {
-//     if root == nil {
-//         return nil
-//     }
-//     out := [][]int{}
-//     queue := []*TreeNode{root}
-    
-//     for len(queue) != 0 {
-//         qSize := len(queue)
-//         cl := []int{}
-//         for qSize != 0 {
-//             dq := queue[0]
-//             queue = queue[1:]
-//             cl = append(cl, dq.Val)
-//             if dq.Left != nil {queue = append(queue, dq.Left)}
-//             if dq.Right != nil {queue = append(queue, dq.Right)}
-
-//             qSize--
-//         }
-//         out = append(out, cl)
-//     }
-//     return out
-// }
+func levelOrder(root *TreeNode) [][]int {
+    if root == nil {
+        return nil
+    }
+    var result [][]int
+    q := []*TreeNode{root}
+    for len(q) != 0 {
+        qSize := len(q)
+        level := []int{}
+        for qSize != 0 {
+            dq := q[0]
+            q = q[1:]
+            level = append(level, dq.Val)
+            if dq.Left != nil {q = append(q, dq.Left)}
+            if dq.Right != nil {q = append(q, dq.Right)}
+            qSize--
+        }
+        result = append(result, level)
+    }
+    return result
+}
 
 
 
@@ -48,31 +46,31 @@
 
 // tried passing result as a reference but golang complains that *result[idx] indexing on references are not supported
 // so then the class solution to narrow down the global scope of result to only 1 instance or else global pollution happens
-type sol struct {
-    result [][]int
-}
+// type sol struct {
+//     result [][]int
+// }
 
-func levelOrder(root *TreeNode) [][]int{
-    sol := new(sol)
-    sol.preorderDfs(root, 0)
-    return sol.result
-}
+// func levelOrder(root *TreeNode) [][]int{
+//     sol := new(sol)
+//     sol.preorderDfs(root, 0)
+//     return sol.result
+// }
 
 
-func (s *sol) preorderDfs(root *TreeNode, level int) {
+// func (s *sol) preorderDfs(root *TreeNode, level int) {
     
-    // base
-    if root == nil {
-        return
-    }
+//     // base
+//     if root == nil {
+//         return
+//     }
     
-    //logic
-    if len(s.result) == level {
-        s.result = append(s.result, []int{})
-    }
-    s.result[level] = append(s.result[level], root.Val)
-    level = level+1
+//     //logic
+//     if len(s.result) == level {
+//         s.result = append(s.result, []int{})
+//     }
+//     s.result[level] = append(s.result[level], root.Val)
+//     level = level+1
     
-    s.preorderDfs(root.Left, level)
-    s.preorderDfs(root.Right, level)
-}
+//     s.preorderDfs(root.Left, level)
+//     s.preorderDfs(root.Right, level)
+// }
