cfbe030a6d98d2cb40c1e129ee30c2ec8e0c52c8
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-12 21:48:36 -0400

    Time: 9 ms (63.38%), Space: 4.1 MB (23.05%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.go
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.go
@@ -113,20 +113,11 @@
 //-----------------------------------------------------------------------------------------------------------------------
 // container lib min heap implementation
 
-type MinHeap struct {
-	items []int
-}
-
-func (m *MinHeap) Len() int {return len(m.items)}
-func (m *MinHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
-func (m *MinHeap) Swap(i, j int) { m.items[i],m.items[j] = m.items[j], m.items[i]}
-func (m *MinHeap) Push(x interface{}) {m.items = append(m.items, x.(int))}
-func (m *MinHeap) Pop() interface{} {
-	i := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return i
-}
-
+// time: o(nlogk)
+// we loop over all numbers in nums o(n)
+// we store them in heap but our insert/extract only ever has to deal with k items in heap therefore logk
+// therefore total time: o(n) * o(logk) = o(nlogk)
+// space: o(k) - at worse we store k elements in our min heap
 func findKthLargest(nums []int, k int) int {
 	var out int
 	if nums == nil {
@@ -139,4 +130,19 @@ func findKthLargest(nums []int, k int) int {
 	}
 	out = mn.items[0]
 	return out
+}
+
+// implements container heap interface : https://pkg.go.dev/container/heap#Interface
+type MinHeap struct {
+	items []int
+}
+
+func (m *MinHeap) Len() int {return len(m.items)}
+func (m *MinHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
+func (m *MinHeap) Swap(i, j int) { m.items[i],m.items[j] = m.items[j], m.items[i]}
+func (m *MinHeap) Push(x interface{}) {m.items = append(m.items, x.(int))}
+func (m *MinHeap) Pop() interface{} {
+	i := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return i
 }
