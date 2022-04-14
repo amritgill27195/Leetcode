0a7001093aa5b0e537c862c43048a350d937b7d8
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-13 21:17:48 -0400

    Time: 61 ms (84.21%), Space: 18 MB (47.37%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.go
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.go
@@ -1,3 +1,21 @@
+// to get index of a character in golang
+// use unit8-lowerCaseRune ( if looking for idx for lowercase character , upper case otherwise )
+// for example
+// uint8=97 - 'a' -- gives us idx 0
+
+/*
+w := "amrit"
+for i := 0; i < len(w); i++ {
+    char := w[i]
+    fmt.Printf("%v: %T %v\n", string(char), char, char-'a')
+}
+a: uint8 0
+m: uint8 12
+r: uint8 17
+i: uint8 8
+t: uint8 19
+*/
+
 type TrieNode struct {
     isEnd bool
     childrens [26]*TrieNode
@@ -17,7 +35,8 @@ func Constructor() Trie {
     }
 }
 
-
+// time: o(k) where k is len of input str
+// space: o(k)
 func (this *Trie) Insert(word string)  {
     cur := this.root
     for i := 0; i < len(word); i++ {
@@ -31,7 +50,8 @@ func (this *Trie) Insert(word string)  {
     cur.isEnd = true
 }
 
-
+// time: o(k) where k is len of input str
+// space: o(1)
 func (this *Trie) Search(word string) bool {
     cur := this.root
     for i := 0; i < len(word); i++ {
@@ -43,7 +63,8 @@ func (this *Trie) Search(word string) bool {
 
 }
 
-
+// time: o(k) where k is len of input str
+// space: o(1)
 func (this *Trie) StartsWith(prefix string) bool {
     cur := this.root
     for i := 0; i < len(prefix); i++ {
