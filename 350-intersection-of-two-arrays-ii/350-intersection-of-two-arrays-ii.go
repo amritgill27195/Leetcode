6c285131f49ee835257073a6005e934e787f8353
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-14 09:58:41 -0400

    Time: 5 ms (43.70%), Space: 2.7 MB (99.30%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.go
@@ -36,25 +36,74 @@
 
 // approach sort + two pointers
 // time: 
-//      o(n1logn1) + o(n2logn2) +  o(max(nums1,nums2)) -- since we sort both
+//     if we count sorting: o(n1logn1) + o(n2logn2) + o(n1+n2)
+//      or else 2 ptrs on 2 arrays o(n1+n2)
 // space: o(1) if we do not count output array as part of space
+// func intersect(nums1 []int, nums2 []int) []int {
+//     sort.Ints(nums1)
+//     sort.Ints(nums2)
+//     out := []int{}
+//     n1 := 0
+//     n2 := 0
+    
+//     for n1 < len(nums1) && n2 < len(nums2) {
+//         if nums1[n1] == nums2[n2] {
+//             out = append(out, nums1[n1])
+//             n1++
+//             n2++
+//         } else if nums1[n1] < nums2[n2] {
+//             n1++
+//         } else {
+//             n2++
+//         }
+//     }
+//     return out
+// }
+
+
+
+// binary search
 func intersect(nums1 []int, nums2 []int) []int {
+    // to make sure nums1 is always bigger 
+    if len(nums2) > len(nums1) {
+        return intersect(nums2, nums1)
+    }
     sort.Ints(nums1)
     sort.Ints(nums2)
-    out := []int{}
-    n1 := 0
-    n2 := 0
     
-    for n1 < len(nums1) && n2 < len(nums2) {
-        if nums1[n1] == nums2[n2] {
-            out = append(out, nums1[n1])
-            n1++
-            n2++
-        } else if nums1[n1] < nums2[n2] {
-            n1++
-        } else {
-            n2++
+    // so now we can imply nums1 is always bigger
+    m := len(nums1)
+    out := []int{}
+    // we should do binary search on the larger array 
+    // so we can get smaller time complx
+    // for every n in smaller array, do binary search on bigger array
+    left := 0
+    right := m-1
+    for i := 0; i < len(nums2); i++ {
+        idx := binarySearch(left, right, nums2[i], nums1)
+        if idx != -1 {
+            out = append(out, nums2[i])
+            left = idx+1
         }
     }
     return out
 }
+
+func binarySearch(left, right, target int, nums []int) int {
+    for left <= right {
+        mid := left + ((right-left))/2
+        if nums[mid] == target {
+            if mid == left || nums[mid-1] != target {
+                return mid
+            } else {
+                right = mid-1
+            }
+        } else if nums[mid] > target {
+            right = mid-1
+        } else {
+            left = mid+1
+        }
+    }
+    return -1
+}
+
