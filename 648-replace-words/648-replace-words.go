665e80a640cb4442228bf3aef5dc1eb622cd600a
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-14 20:36:01 -0400

    Time: 15 ms (98.48%), Space: 23.7 MB (7.57%) - LeetHub

---
+++ 648-replace-words/648-replace-words.go
@@ -0,0 +1,76 @@
+
+type TrieNode struct {
+    isEnd bool
+    childrens [26]*TrieNode
+}
+
+
+
+// time: o(k) where k is len of input str
+// space: o(k)
+func (t *TrieNode) Insert(word string)  {
+    cur := t
+    for i := 0; i < len(word); i++ {
+        char := word[i]
+        if cur.childrens[char-'a'] == nil {
+            cur.childrens[char-'a'] = &TrieNode{
+                isEnd: false, childrens: [26]*TrieNode{}}
+        }
+        cur = cur.childrens[char-'a']
+    }
+    cur.isEnd = true
+}
+
+// time: o(k) where k is len of input str
+// space: o(1)
+func (t *TrieNode) Search(word string) (bool, string) {
+    cur := t
+    wordBldr := new(strings.Builder)
+    for i := 0; i < len(word); i++ {
+        char := word[i]
+        if cur.childrens[char-'a'] == nil {return false, ""}
+        cur = cur.childrens[char-'a']
+        wordBldr.WriteByte(char)
+        if cur.isEnd {break}
+    }
+    
+    return cur.isEnd, wordBldr.String()
+
+}
+
+
+/*
+    - toss the entire dictonary in a trie
+    - then split sentence into a list
+    - Loop over each word in split array
+    - Search for each character and form the replacement word
+    - If we successfully found a replacement, then replace append the replacement to a our resulting string builder
+    - Finally return the string from string builder
+*/
+func replaceWords(dictionary []string, sentence string) string {
+    root := &TrieNode{}
+    for _, dict := range dictionary {
+        root.Insert(dict)
+    }
+    
+    splitWords := strings.Split(sentence, " ")
+    outBldr := new(strings.Builder)
+    for idx, word := range splitWords {
+        if idx > 0 {
+            outBldr.WriteString(" ")
+        }
+        foundValidWord, replace := root.Search(word)
+        if foundValidWord {
+            splitWords[idx] = replace
+            outBldr.WriteString(replace)
+        } else {
+            outBldr.WriteString(word)
+        }
+    }
+    
+    return outBldr.String()
+}
+
+
+
+
