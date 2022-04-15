c72fa7fcf8277537ac7ea8b5592776e6210aa6bc
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-15 16:27:51 -0400

    Time: 44 ms (92.22%), Space: 7.1 MB (94.81%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.go
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.go
@@ -1,48 +1,44 @@
 func shortestPathBinaryMatrix(grid [][]int) int {
-    if grid == nil  || len(grid) == 0 || len(grid[0]) == 0 || grid[0][0] != 0 || grid[len(grid)-1][len(grid[0])-1] != 0 {
+    level := 0
+    m := len(grid)
+    n := len(grid[0])
+    destRow := m-1
+    destCol := n-1
+    if grid[destRow][destCol] != 0 || grid[0][0] != 0 {
         return -1
     }
-    
-    dirs := [][]int{  
-        // horizontal and vertical
+    dirs := [][]int{
         {1,0},
         {-1,0},
         {0,1},
         {0,-1},
-
-        // diagonal
-        {1,1},
-        {1,-1},
-        {-1,1},
         {-1,-1},
+        {-1,1},
+        {1,-1},
+        {1,1},
     }
-    m := len(grid)
-    n := len(grid[0])
-    q := [][]int{{0,0}}
     grid[0][0] = 1
-    for len(q) != 0{
-        
+    q := [][]int{{0,0}}
+    for len(q) != 0 {
         qSize := len(q)
-        // fmt.Println("Current queue: ", q, qSize)
         for qSize != 0 {
             dq := q[0]
             q = q[1:]
             
-            if dq[0] == m-1 && dq[1] == n-1 {
-                return grid[dq[0]][dq[1]]
+            if dq[0] == destRow && dq[1] == destCol {
+                return level+1
             }
-            
             for _, dir := range dirs {
-                r := dq[0] + dir[0]
-                c := dq[1] + dir[1]
+                r := dir[0] + dq[0]
+                c := dir[1] + dq[1]
                 if r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 0 {
-                    grid[r][c] = grid[dq[0]][dq[1]]+1
+                    grid[r][c] = 1
                     q = append(q, []int{r,c})
                 }
             }
             qSize--
         }
-        
+        level++
     }
     
     return -1
