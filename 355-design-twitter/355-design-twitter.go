e62b110a6ac53824c479213bc239cb5fc9b0b396
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-16 15:40:37 -0400

    Time: 1 ms (53.57%), Space: 2.1 MB (78.57%) - LeetHub

---
+++ 355-design-twitter/355-design-twitter.go
@@ -0,0 +1,107 @@
+
+type Twitter struct {
+    followMap map[int]*set
+    tweetMap map[int][]*tweet
+}
+type tweet struct {
+    id int
+    t time.Time
+}
+
+func Constructor() Twitter {
+    return Twitter{
+        followMap: map[int]*set{},
+        tweetMap: map[int][]*tweet{},
+    }
+}
+
+
+func (this *Twitter) PostTweet(userId int, tweetId int)  {
+    this.tweetMap[userId] = append(this.tweetMap[userId], &tweet{id: tweetId, t: time.Now()})
+}
+
+
+func (this *Twitter) GetNewsFeed(userId int) []int {
+    users := []int{userId}
+    if this.followMap[userId] != nil {
+        users = append(users, this.followMap[userId].list()...)
+    }
+    mh := &minHeap{tweets: []*tweet{}}
+    for _, uid := range users {
+        for _, tid := range this.tweetMap[uid] {
+            heap.Push(mh, tid)
+            if len(mh.tweets) > 10 {
+                heap.Pop(mh)
+            }
+        }
+    }
+    out := make([]int, len(mh.tweets))
+    for i := len(mh.tweets)-1; i>=0; i-- {
+        out[i] = heap.Pop(mh).(*tweet).id
+    }
+    return out
+}
+
+
+func (this *Twitter) Follow(followerId int, followeeId int)  {
+    if this.followMap[followerId] == nil {
+        this.followMap[followerId] = newSet()
+    }
+    this.followMap[followerId].add(followeeId)
+}
+
+
+func (this *Twitter) Unfollow(followerId int, followeeId int)  {
+    if this.followMap[followerId] == nil {
+        return
+    }
+    this.followMap[followerId].remove(followeeId)
+}
+
+
+
+// Heap -- implements container heap interface : https://pkg.go.dev/container/heap#Interface
+type minHeap struct {
+	tweets []*tweet
+}
+func (m *minHeap) Len() int {return len(m.tweets)}
+func (m *minHeap) Less(i, j int) bool {return m.tweets[i].t.Before(m.tweets[j].t)}
+func (m *minHeap) Swap(i, j int) { m.tweets[i],m.tweets[j] = m.tweets[j], m.tweets[i]}
+func (m *minHeap) Push(x interface{}) {m.tweets = append(m.tweets, x.(*tweet))}
+func (m *minHeap) Pop() interface{} {
+	i := m.tweets[len(m.tweets)-1]
+	m.tweets = m.tweets[:len(m.tweets)-1]
+	return i
+}
+
+
+// Poor mans set 
+type set struct {
+    items map[int]struct{}
+}
+func newSet() *set {
+    return &set{items:map[int]struct{}{}}
+}
+func (s *set) add(item int) {
+    s.items[item] = struct{}{}
+}
+func (s *set) remove(item int){
+    delete(s.items, item)
+}
+func (s *set) list() []int{
+    out := []int{}
+    for k, _ := range s.items{
+        out = append(out, k)
+    }
+    return out
+}
+
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.PostTweet(userId,tweetId);
+ * param_2 := obj.GetNewsFeed(userId);
+ * obj.Follow(followerId,followeeId);
+ * obj.Unfollow(followerId,followeeId);
+ */
