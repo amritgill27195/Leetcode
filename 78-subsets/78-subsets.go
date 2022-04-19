48d8f0a45d5dcc276f1f7681934e6fded99a7ace
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-19 13:49:14 -0400

    Time: 2 ms (50.09%), Space: 2.3 MB (27.62%) - LeetHub

--- 78-subsets/78-subsets.go
+++ 78-subsets/78-subsets.go
@@ -23,28 +23,56 @@
 */
 
 
-// 0/1 based recursion
+// for loop based recursion
 func subsets(nums []int) [][]int {
     var result [][]int
-    var dfs func(start int, paths []int)
-    dfs = func(start int, paths []int) {
+    var backtrack func(start int, paths []int)
+    backtrack = func(start int, paths []int) {
         // base
+        newL := make([]int, len(paths))
+        copy(newL, paths)
+        result = append(result, newL)
         if start == len(nums) {
-            newL := make([]int, len(paths))
-            copy(newL, paths)
-            result = append(result, newL)
             return
         }
         
         // logic
-        // not choose
-        dfs(start+1, paths)
-        
-        // choose
-        paths = append(paths, nums[start])
-        dfs(start+1, paths)
-        paths = paths[:len(paths)-1]
+        for i := start; i < len(nums); i++ {
+            // action
+            paths = append(paths, nums[i])
+            // recurse
+            backtrack(i+1, paths)
+            // backtrack
+            paths = paths[:len(paths)-1]
+        }
     }
-    dfs(0, nil)
+    backtrack(0,nil)
     return result
 }
+
+
+// 0/1 based recursion
+// func subsets(nums []int) [][]int {
+//     var result [][]int
+//     var dfs func(start int, paths []int)
+//     dfs = func(start int, paths []int) {
+//         // base
+//         if start == len(nums) {
+//             newL := make([]int, len(paths))
+//             copy(newL, paths)
+//             result = append(result, newL)
+//             return
+//         }
+        
+//         // logic
+//         // not choose
+//         dfs(start+1, paths)
+        
+//         // choose
+//         paths = append(paths, nums[start])
+//         dfs(start+1, paths)
+//         paths = paths[:len(paths)-1]
+//     }
+//     dfs(0, nil)
+//     return result
+// }
