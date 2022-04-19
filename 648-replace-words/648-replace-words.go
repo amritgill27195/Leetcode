eeff5865af4bd45bca2fa3a4d47894fee3eee210
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-18 21:23:38 -0400

    Time: 19 ms (83.56%), Space: 23.7 MB (9.59%) - LeetHub

--- 648-replace-words/648-replace-words.go
+++ 648-replace-words/648-replace-words.go
@@ -1,88 +1,48 @@
-/*
-    - toss the entire dictonary in a trie
-    - then split sentence into a list
-    - Loop over each word in split array
-    - Search for each character and form the replacement word
-    - If we successfully found a replacement, then replace append the replacement to a our resulting string builder
-    - Finally return the string from string builder
-    
 
-*/
-
-// dictionary size = m, avg word len = k
-// total words in sentence = n, avg word len = l
-func replaceWords(dictionary []string, sentence string) string {
-    root := &TrieNode{}
-    // time : o(m) * o(k) = o(mk)
-    for _, dict := range dictionary {
-        root.Insert(dict)
-    }
-    
-    splitWords := strings.Split(sentence, " ")
-    outBldr := new(strings.Builder)
-    // time : o(n) * o(l) = o(nl)
-    // o(l) where l is the avg len of each word in sentence and searching for a word in trie takes o(l) time
-    // time = o(nl)
-    for idx, word := range splitWords {
-        if idx != 0 {
-            outBldr.WriteString(" ")
-        }
-        foundValidWord, replace := root.Search(word)
-        if foundValidWord {
-            splitWords[idx] = replace
-            outBldr.WriteString(replace)
-        } else {
-            outBldr.WriteString(word)
-        }
-    }
-    
-    // total time = o(mk) + o(nl)
-    // space: o(mk) + o(nl)
-    
-    return outBldr.String()
-}
-
-
-
-type TrieNode struct {
+type trieNode struct {
     isEnd bool
-    childrens [26]*TrieNode
+    childrens [26]*trieNode
 }
 
-// time: o(k) where k is len of input str
-// space: o(k)
-func (t *TrieNode) Insert(word string)  {
-    cur := t
+func insert(word string, root *trieNode) {
+    curr := root
     for i := 0; i < len(word); i++ {
-        char := word[i]
-        if cur.childrens[char-'a'] == nil {
-            cur.childrens[char-'a'] = &TrieNode{
-                isEnd: false, childrens: [26]*TrieNode{}}
+        if curr.childrens[word[i]-'a'] == nil {
+            curr.childrens[word[i]-'a'] = new(trieNode)
         }
-        cur = cur.childrens[char-'a']
+        curr = curr.childrens[word[i]-'a']
     }
-    cur.isEnd = true
+    curr.isEnd = true
 }
 
-// time: o(k) where k is len of input str
-// space: o(k) for string builder - worse case we do not find any smaller valid word
-func (t *TrieNode) Search(word string) (bool, string) {
-    cur := t
-    wordBldr := new(strings.Builder)
+func search(word string, root *trieNode) (bool, string) {
+    curr := root
+    out := new(strings.Builder)
     for i := 0; i < len(word); i++ {
-        char := word[i]
-        if cur.childrens[char-'a'] == nil {return false, ""}
-        cur = cur.childrens[char-'a']
-        wordBldr.WriteByte(char)
-        if cur.isEnd {return true, wordBldr.String()}
+        if curr.childrens[word[i]-'a'] == nil { return false, word}
+        out.WriteByte(word[i])
+        curr = curr.childrens[word[i]-'a']
+        if curr.isEnd {return true, out.String()}
     }
-    // did not find a smaller word than the word itself 
     return false, word
-
 }
 
 
-
-
-
-
+func replaceWords(dictionary []string, sentence string) string {
+    
+    root := new(trieNode)
+    for _, word := range dictionary{
+        insert(word, root)
+    }
+    out := new(strings.Builder)
+    splitSent := strings.Split(sentence, " ")
+    for idx, word := range splitSent{
+        if idx != 0 {out.WriteString(" ")}
+        found, replace := search(word, root)
+        if found {
+           splitSent[idx] = replace 
+        }
+        out.WriteString(splitSent[idx])
+    }
+    return out.String()
+}
