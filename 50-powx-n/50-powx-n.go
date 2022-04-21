d2f0c6f366bfd5b7446cf6ad55e78728e8b70cf9
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-20 22:46:58 -0400

    Time: 0 ms (100.00%), Space: 2 MB (20.15%) - LeetHub

--- 50-powx-n/50-powx-n.go
+++ 50-powx-n/50-powx-n.go
@@ -39,14 +39,16 @@
         - if yes, return result * result
         - if not, return result * result * x (base)
             - why? 
-            - because even exponent(n) was perfectly divided in half, odd exponent miss multiplying the base by 1 therefore the extra  x $x(base)
+            - because even exponent(n) was perfectly divided in half, odd exponent missed multiplying the base by 1 more time, therefore the extra  * $x(base)
     - How is this optimizing the brute force????
-        - Example: 2^10
-        - Our brute force loop runs 10 times
-        - Whereas the binary search approach runs 5 times - therefore optimized
+        - Example: 0.2^20
+        - Our brute force loop runs 20 times
+        - Whereas the binary search approach runs 6 times - therefore optimized
+    
+    time: o(logn)
+    space: o(logn)
     
 */
-
 func myPow(x float64, n int) float64 {
     if n < 0 {
         x = 1/x
