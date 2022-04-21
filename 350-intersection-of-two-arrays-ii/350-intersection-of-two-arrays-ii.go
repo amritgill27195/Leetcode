c50dcddc3c998b0ff64331f30e07a910343a61fd
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-21 18:45:28 -0400

    Time: 0 ms (100.00%), Space: 2.7 MB (99.31%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.go
@@ -16,27 +16,86 @@
     space: o(n)
 */
 
+// func intersect(nums1 []int, nums2 []int) []int {
+    
+//     // to make sure nums1 is always the larger array
+//     if len(nums1) < len(nums2) {
+//         return intersect(nums2, nums1)
+//     }
+    
+    
+//     freqMap := map[int]int{}
+//     for _, num := range nums2 {
+//         freqMap[num]++
+//     }
+    
+//     result := []int{}
+//     for i := 0; i < len(nums1); i++ {
+//         val, ok := freqMap[nums1[i]]
+//         if ok && val != 0 { // no need to delete, we can just check its frequency
+//             result = append(result, nums1[i])
+//             freqMap[nums1[i]]--
+//         }
+//     }
+//     return result
+// }
+
+
+/*
+    We need to find the common elements between nums1 and nums2.
+    We can always sort both nums and deploy two pointers
+    
+    approach: 
+    - sort(nums1), sort(nums2)
+    - n1 and n2 ptrs starting at nums1 and nums2
+    - if n1 == n2 then add this value to result and increment both ptrs by 1
+    - if n1 > n2 - we need to move away from smaller number therefore increment n2
+        - why?
+        - because for n1 to become == n2, when n1 is larger than n2,
+        - moving n1 will only make n1 larger than n2 because arrays are sorted
+        - therefore moving n2 makes more sense as there is a possibility that if n2 is smaller than n1, then moving n2 may result into n2 == n1.
+    - otherwise increment n1
+    - what about out of bound check? when does our loop stop?
+    - What if we do while n1 < len(nums1) && n2 < len(nums2)
+    - whichever ptr goes out of bounds first, will stop our loop and help us avoid the out of bounds issue.
+    
+    m = larger array
+    n = smaller array
+    
+    excluding sort time from below because I am implying the array will be sorted
+    if not then add o(mlogm) + o(nlogn) to the below time 
+    time: o(m) + o(n)
+    space: o(1)
+*/
+
 func intersect(nums1 []int, nums2 []int) []int {
     
-    // to make sure nums1 is always the larger array
+    // make sure nums1 is larger 
     if len(nums1) < len(nums2) {
         return intersect(nums2, nums1)
     }
     
+    // sort ( implied )
+    sort.Ints(nums1)
+    sort.Ints(nums2)
     
-    freqMap := map[int]int{}
-    for _, num := range nums2 {
-        freqMap[num]++
-    }
     
+    n1 := 0
+    n2 := 0
     result := []int{}
-    for i := 0; i < len(nums1); i++ {
-        val, ok := freqMap[nums1[i]]
-        if ok && val != 0 { // no need to delete, we can just check its frequency
-            result = append(result, nums1[i])
-            freqMap[nums1[i]]--
+    for n1 < len(nums1) && n2 < len(nums2) {
+        n1Val := nums1[n1]
+        n2Val := nums2[n2]
+        if n1Val == n2Val {
+            // we are looking for common elements
+            result = append(result, n1Val)
+            n1++
+            n2++
+        } else if n1Val > n2Val {
+            n2++
+        } else {
+            n1++
         }
     }
     return result
-    
 }
