f6ed5f2027f31fd49a9234a8b3f0eec959872149
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-22 18:06:41 -0400

    Time: 29 ms (27.78%), Space: 6.8 MB (27.78%) - LeetHub

---
+++ 275-h-index-ii/275-h-index-ii.go
@@ -0,0 +1,16 @@
+func hIndex(citations []int) int {
+    left := 0
+    right := len(citations)-1
+    n := len(citations)
+    for left <= right {
+        mid := left+(right-left)/2
+        if n-mid == citations[mid] {
+            return n-mid
+        } else if citations[mid] > n-mid {
+            right = mid-1
+        } else {
+            left = mid+1
+        }
+    }
+    return n-left
+}
