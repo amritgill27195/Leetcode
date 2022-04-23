ac2bd2ffa72842cdfc5e26f3ab8c57ebe2e1483d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-22 20:20:25 -0400

    Time: 2 ms (74.30%), Space: 2 MB (88.01%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.go
+++ 28-implement-strstr/28-implement-strstr.go
@@ -4,13 +4,8 @@ func strStr(haystack string, needle string) int {
     }
     if len(needle) == 0 {return 0}
     for i := 0; i < len(haystack)+1-len(needle); i++ {
-        for j := 0; j < len(needle); j++ {
-            if haystack[i+j] != needle[j] {
-                break
-            }
-            if j == len(needle)-1 {
-                return i
-            }
+        if string(haystack[i:i+len(needle)]) == needle {
+            return i
         }
     }
     return -1
