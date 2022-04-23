2c00c679e0b11954982ea5f15be52dbc076ec3bb
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-23 13:31:00 -0400

    Time: 87 ms (72.46%), Space: 6.8 MB (76.81%) - LeetHub

--- 282-expression-add-operators/282-expression-add-operators.go
+++ 282-expression-add-operators/282-expression-add-operators.go
@@ -1,3 +1,22 @@
+/*
+    approach: for loop based recursion
+    - We need to output all possible expressions using ALL nums in num string that result into target
+    - The only operators we have to try are '+','-','*'
+    - Which also means we need make 3 recursive calls for EACH ith substr we have formed.
+    - How do we form all the expressions?
+        - For loop based recursion.
+        - We will have a start idx that will be passed into the initial recursive call
+        - And our for loop starts from that start idx until it reaches the end of num string
+        - If need to form '1', '12', '123' from the given '123' string,
+            - we can create substr using str[startIdx:i] -- i keeps moving and start stays where it is - so that works well in our favor
+        - But we need to form expressions...
+        - For each ith element, we need to form 3 expressions with '+','-','*' (i.e 3 recursive calls)
+    - What are all the parameters we need to pass down in the recursive call ?
+    - How do we maintain and enforce PEMDAS operator precedence?
+    - What evaluates whether this expression works or not?
+    - What do we need to backtrack? 
+
+*/
 func addOperators(num string, target int) []string {
     
     var result []string
