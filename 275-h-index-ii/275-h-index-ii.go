e8928374952e0a872170428fb1f540e7fe529792
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-24 16:00:33 -0400

    Time: 28 ms (27.78%), Space: 6.3 MB (72.22%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.go
+++ 275-h-index-ii/275-h-index-ii.go
@@ -1,19 +1,36 @@
 // worst problem desc ever...
 // time: o(logn)
 // space: o(1)
-func hIndex(citations []int) int {
-    left := 0
-    right := len(citations)-1
+// func hIndex(citations []int) int {
+//     left := 0
+//     right := len(citations)-1
+//     n := len(citations)
+//     for left <= right {
+//         mid := left+(right-left)/2
+//         if n-mid == citations[mid] {
+//             return n-mid
+//         } else if citations[mid] > n-mid {
+//             right = mid-1
+//         } else {
+//             left = mid+1
+//         }
+//     }
+//     return n-left
+// }
+
+
+func hIndex(citations []int) int { 
     n := len(citations)
-    for left <= right {
-        mid := left+(right-left)/2
-        if n-mid == citations[mid] {
-            return n-mid
-        } else if citations[mid] > n-mid {
-            right = mid-1
-        } else {
-            left = mid+1
+    for i := 0; i < len(citations); i++ {
+        diff := n-i
+        if diff <= citations[i] {
+            return diff
         }
     }
-    return n-left
+    return 0
+}
+
+func abs(n int) int {
+    if n < 0 {return n * -1}
+    return n
 }
