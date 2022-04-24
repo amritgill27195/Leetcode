31feea88953d77c326f2319a779ac269b76bdba7
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-24 14:20:25 -0400

    Time: 26 ms (40.79%), Space: 3.9 MB (12.28%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.go
@@ -0,0 +1,38 @@
+func minWindow(s string, t string) string {
+    tMap := map[string]int{}
+    for _, char := range t {
+        tMap[string(char)]++
+    }
+    count := 0
+    left := 0
+    startIdx := -1
+    endIdx := -1
+    smallestWinSize := len(s)+1
+    for right := 0; right < len(s); right++ {
+        rightChar := string(s[right])
+        if _, ok := tMap[rightChar]; ok {
+            tMap[rightChar]--
+            if val := tMap[rightChar]; val == 0 {
+                count++
+            }
+        }
+        for count == len(tMap) {
+            windowSize := right-left+1
+            if windowSize < smallestWinSize {
+                smallestWinSize = windowSize
+                startIdx = left
+                endIdx = right
+            }
+            leftChar := string(s[left])
+            if val, ok := tMap[leftChar]; ok {
+                tMap[leftChar]++
+                if val == 0 {
+                    count--
+                }
+            }
+            left++
+        }
+    }
+    if startIdx == -1 {return "" }
+    return string(s[startIdx:endIdx+1])
+}
