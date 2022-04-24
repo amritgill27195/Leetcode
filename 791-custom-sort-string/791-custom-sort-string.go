45c782d0b96338343b0868071c187c1418d48f86
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-24 17:08:11 -0400

    Time: 0 ms (100.00%), Space: 2 MB (49.61%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.go
+++ 791-custom-sort-string/791-custom-sort-string.go
@@ -10,26 +10,54 @@
     space: o(len(s))
 */
 
-func customSortString(order string, s string) string {
-    freqMap := map[string]int{}
+// func customSortString(order string, s string) string {
+//     freqMap := map[string]int{}
+//     for i := 0; i < len(s); i++ {
+//         freqMap[string(s[i])]++
+//     }
+//     out := new(strings.Builder)
+//     for i := 0; i < len(order); i++ {
+//         char := string(order[i])
+//         numTimes := freqMap[char] // if key is not found, the value will be 0 because default int value is 0
+//         for j := 0; j < numTimes; j++{
+//             out.WriteString(char)
+//         }
+//         delete(freqMap, char)
+//     }
+    
+//     // whatever is left over, append
+//     for key, val := range freqMap {
+//         for i := 0; i < val; i++ {
+//             out.WriteString(key)
+//         }
+//     }
+//     return out.String()
+// }
+
+
+// use char array
+func customSortString(order string, s string) string { 
+    charFreq := [26]int{}
     for i := 0; i < len(s); i++ {
-        freqMap[string(s[i])]++
+        charFreq[s[i]-'a']++
     }
     out := new(strings.Builder)
     for i := 0; i < len(order); i++ {
-        char := string(order[i])
-        numTimes := freqMap[char] // if key is not found, the value will be 0 because default int value is 0
-        for j := 0; j < numTimes; j++{
+        idxOfChar := order[i]-'a'
+        numTimes := charFreq[idxOfChar]
+        char := string('a'+idxOfChar)
+        for j := 0; j < numTimes;j++ {
+            out.WriteString(char)
+            charFreq[idxOfChar]--
+        }
+    }
+        
+    for i := 0; i < 26; i++ {
+        char := string('a'+i)
+        for j := 0; j < charFreq[i];j++ {
             out.WriteString(char)
         }
-        delete(freqMap, char)
     }
     
-    // whatever is left over
-    for key, val := range freqMap {
-        for i := 0; i < val; i++ {
-            out.WriteString(key)
-        }
-    }
     return out.String()
 }
