4da8d1e7ea2326db55f405c80f30431b88fe0878
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-25 12:31:58 -0400

    Time: 16 ms (46.75%), Space: 7.1 MB (95.56%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.go
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.go
@@ -15,58 +15,58 @@
     
     time: o(n)
         - worse case our recursion goes left first and p and q are located in the right subtree as leaf nodes
-        - plus we never exit early in 
     space: o(p+q)
-        - o(h) for recursion stack + o(p+q) the paths array for both p and q which would be larger compared to o(h)
+        - o(h) for recursion stack + o(p+q) the paths array for both p and q which would be larger compared to o(h) in worse case
 */
 
-func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-    var pPaths []*TreeNode
-	var qPaths []*TreeNode
+// func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+//     var pPaths []*TreeNode
+// 	var qPaths []*TreeNode
 
-	var backtrack func(r *TreeNode, paths []*TreeNode)
-	backtrack = func(r *TreeNode, paths []*TreeNode) {
-		// base
-        if r == nil{
-			return
-		}
+// 	var backtrack func(r *TreeNode, paths []*TreeNode)
+// 	backtrack = func(r *TreeNode, paths []*TreeNode) {
+// 		// base
+//         if r == nil{
+// 			return
+// 		}
         
-        // exit early
-        if len(pPaths) != 0 && len(qPaths) != 0 {return }
-		// logic
-		// action
-        paths = append(paths, r)
-        if r.Val == p.Val {
-            // new list since we backtrack paths arr so that we dont end up with empty list ( since we remove last element from paths )
-			newP := make([]*TreeNode, len(paths))
-			copy(newP, paths)
-			pPaths = newP
-		}
-		if r.Val == q.Val {
-			newQ := make([]*TreeNode, len(paths))
-			copy(newQ, paths)
-			qPaths = newQ
-		}
-		// recurse
-		backtrack(r.Left, paths)
-		backtrack(r.Right, paths)
-		// backtrack
-		paths = paths[:len(paths)-1]
-	}
-	backtrack(root, nil)
+//         // exit early
+//         if len(pPaths) != 0 && len(qPaths) != 0 {return}
+        
+// 		// logic
+// 		// action
+//         paths = append(paths, r)
+//         if r.Val == p.Val {
+//             // new list since we backtrack paths arr so that we dont end up with empty list ( since we remove last element from paths )
+// 			newP := make([]*TreeNode, len(paths))
+// 			copy(newP, paths)
+// 			pPaths = newP
+// 		}
+// 		if r.Val == q.Val {
+// 			newQ := make([]*TreeNode, len(paths))
+// 			copy(newQ, paths)
+// 			qPaths = newQ
+// 		}
+// 		// recurse
+// 		backtrack(r.Left, paths)
+// 		backtrack(r.Right, paths)
+// 		// backtrack
+// 		paths = paths[:len(paths)-1]
+// 	}
+// 	backtrack(root, nil)
 
-	var out *TreeNode
-	pPtr := 0
-	qPtr := 0
-	for pPtr < len(pPaths) && qPtr < len(qPaths) {
-        if pPaths[pPtr] == qPaths[qPtr] {
-			out = pPaths[pPtr]
-		}
-		pPtr++
-		qPtr++
-	}
-	return out
-}
+// 	var out *TreeNode
+// 	pPtr := 0
+// 	qPtr := 0
+// 	for pPtr < len(pPaths) && qPtr < len(qPaths) {
+//         if pPaths[pPtr] == qPaths[qPtr] {
+// 			out = pPaths[pPtr]
+// 		}
+// 		pPtr++
+// 		qPtr++
+// 	}
+// 	return out
+// }
 
 
 /*
@@ -74,6 +74,19 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
     time:
     space:
 */
-// func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-    
-// }
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    // base
+    if root == nil {
+        return nil
+    }
+    // logic
+    if root.Val == p.Val || root.Val == q.Val {
+        return root
+    }
+    left := lowestCommonAncestor(root.Left, p,q)
+    right := lowestCommonAncestor(root.Right, p,q)
+    if left == nil && right == nil {return nil}
+    if left != nil && right == nil {return left}
+    if left == nil && right != nil {return right}
+    return root
+}
