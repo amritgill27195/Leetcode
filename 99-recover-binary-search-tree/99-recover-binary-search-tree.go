1d71be37ca5a19044ba09d6566d6a7d4e0dbe871
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-25 16:25:26 -0400

    Time: 15 ms (51.36%), Space: 6.8 MB (6.29%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.go
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.go
@@ -6,32 +6,47 @@
  *     Right *TreeNode
  * }
  */
-func recoverTree(root *TreeNode)  {
-    var first *TreeNode
-    var second *TreeNode
-    var prev *TreeNode
-    var inorder func(r *TreeNode)
-    inorder = func(r *TreeNode) {
-        // base
-        if r == nil {
-            return    
-        }   
-        
-        // logic
-        inorder(r.Left)
-        
-        if prev != nil && prev.Val >= r.Val {
-            // invalid bst
-            if first == nil {
-                first = prev
-            }
-            second = r
 
+/*
+    BST == inorder gives us the sorted form ( as long as the tree is valid bst )
+    In this, we do have an invalid BST and we have to fix it by swapping **values**
+    
+    Everytime we have binaryTree/BST question, evaluate whether you have 2 different types of cases
+    where 2 could be part of the same subtree or 2 nodes could be spread out in different subtrees
+    
+    Here we are given that there will be EXACTLY 2 nodes who are in bad state;
+    which means;
+        1. The 2 nodes could be in the same subtree
+        2. The 2 nodes could be spread out into 2 different subtrees
+    
+    approach: brute force
+    - use inorder to generate a list of node values
+    - sort them
+    - use the sorted list to write back into the tree using inorder
+
+*/
+func recoverTree(root *TreeNode)  {
+    nodes := []int{}
+    var listGenerator func(r *TreeNode)
+    listGenerator = func(r *TreeNode) {
+        if r == nil {return}
+        listGenerator(r.Left)
+        nodes = append(nodes, r.Val)
+        listGenerator(r.Right)
+    }
+    listGenerator(root)
+    
+    sort.Ints(nodes)
+    ptr := 0
+    var inorderWriter func(r *TreeNode)
+    inorderWriter = func(r *TreeNode) {
+        if r == nil {
+            return
         }
-        prev = r
-        
-        inorder(r.Right)
-    } 
-    inorder(root)
-    first.Val, second.Val = second.Val, first.Val
+        inorderWriter(r.Left)
+        r.Val = nodes[ptr]
+        ptr++
+        inorderWriter(r.Right)
+    }
+    inorderWriter(root)
 }
