c69625cb56593223e654208e56ec0a29d5b74c65
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-24 22:08:18 -0400

    Time: 39 ms (50.94%), Space: 7 MB (66.55%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.go
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.go
@@ -25,13 +25,38 @@
 // square each number and store into a new array
 // sort the new array and return the new array
 // time: o(nlogn)
-// space: o(1)
+// space: o(1) IF we do not count the output array as part of space
+// func sortedSquares(nums []int) []int {
+//     out := []int{}
+//     for i := 0; i < len(nums); i++ {
+//         out = append(out, nums[i] * nums[i])
+//     }
+//     sort.Ints(out)
+//     return out
+// }
+
+// using two pointers in nums arrays ( start and end )
+// if start squared > end , then toss the start at the end of the output array and move start pointer and repeat
+// otherwise toss the end squared value at the back the output array
+// we will need a pointer pointing at the back of the array in output and this pointer will help us tell where to put the next element
 func sortedSquares(nums []int) []int {
-    out := []int{}
-    for i := 0; i < len(nums); i++ {
-        out = append(out, nums[i] * nums[i])
+    out := make([]int, len(nums))
+    left := 0
+    right := len(nums)-1
+    outPtr := len(nums)-1
+    
+    for outPtr >= 0 {
+        leftSq := nums[left] * nums[left]
+        rightSq := nums[right] * nums[right]
+        if leftSq > rightSq {
+            out[outPtr] = leftSq
+            left++
+        } else {
+            out[outPtr] = rightSq
+            right--
+        }
+        outPtr--
     }
-    sort.Ints(out)
     return out
 }
 
