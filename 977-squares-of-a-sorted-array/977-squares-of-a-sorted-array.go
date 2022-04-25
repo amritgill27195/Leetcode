5be604c8ab40620bdcdcfd31073a956f57d12b90
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-24 22:01:46 -0400

    Time: 47 ms (34.39%), Space: 7.5 MB (23.62%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.go
@@ -0,0 +1,32 @@
+ /*
+    approach 1
+    square them all into a new array
+    and sort the new arry
+    time: o(nlogn)
+    space: o(1) if we do not consider output array as part of space
+    
+    approach 2 : using two pointers
+    Have a left and right pointer ( 0, len(nums)-1)
+    Cretae an output array of same size
+    Have a pointer at len(output)-1
+    
+    We will sqaure each left and right pointer from nums
+    and whoever is bigger will be placed in the back of the output array.
+    Whoever was bigger, we will move that pointer
+    and we will also move the output ptr back 1
+    
+    
+    we will do this until leftPtr <= rightPtr
+    
+   
+    */
+
+// square them in place and sort them at the end
+func sortedSquares(nums []int) []int {
+    
+    for i := 0; i < len(nums); i++ {
+        nums[i] = nums[i] * nums[i]
+    }
+    sort.Ints(nums)
+    return nums
+}
