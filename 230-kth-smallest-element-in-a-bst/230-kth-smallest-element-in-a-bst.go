1d5a8e094cb7982791c813b8064e41ecb1c2a823
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-24 23:02:14 -0400

    Time: 13 ms (37.98%), Space: 6.7 MB (22.66%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.go
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.go
@@ -6,30 +6,53 @@
  *     Right *TreeNode
  * }
  */
+
+// dfs approach
+// func kthSmallest(root *TreeNode, k int) int {
+//     count := 0
+//     var inorder func(r *TreeNode, k int) int
+//     inorder = func(r *TreeNode, k int) int {
+//         // base
+//         if r == nil {
+//             return -1
+//         }
+        
+        
+//         // logic
+//         left := inorder(r.Left, k)
+//         if left != -1 {
+//             return left
+//         }
+//         count++
+//         if count == k {
+//             return r.Val
+//         }
+//         right := inorder(r.Right, k)
+//         if right != -1 {
+//             return right
+//         }
+//         return -1
+//     }
+//     return inorder(root, k)
+// }
+
+
+// iterative inorder approach
 func kthSmallest(root *TreeNode, k int) int {
+    s := []*TreeNode{}
     count := 0
-    var inorder func(r *TreeNode, k int) int
-    inorder = func(r *TreeNode, k int) int {
-        // base
-        if r == nil {
-            return -1
+    for root != nil || len(s) != 0 {
+        for root != nil {
+            s = append(s, root)
+            root = root.Left
         }
-        
-        
-        // logic
-        left := inorder(r.Left, k)
-        if left != -1 {
-            return left
-        }
+        root = s[len(s)-1]
+        s = s[:len(s)-1]
         count++
         if count == k {
-            return r.Val
+            return root.Val
         }
-        right := inorder(r.Right, k)
-        if right != -1 {
-            return right
-        }
-        return -1
+        root = root.Right
     }
-    return inorder(root, k)
+    return -1
 }
