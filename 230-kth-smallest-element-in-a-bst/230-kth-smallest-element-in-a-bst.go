1fdb24154b1e068ae19d9085fb3376723d073b7c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-25 10:59:11 -0400

    Time: 13 ms (37.98%), Space: 6.5 MB (34.22%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.go
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.go
@@ -7,36 +7,63 @@
  * }
  */
 
+/*
+    BST == running inorder gives us a sorted output  ( as long as its a valid bst )
+    approach: brute force
+    - run inorder
+    - at each node, store the node val in an array
+        - this array will be global and cannot be passed as arg because reference data structures combined with pass-by-value slice abstraction in golang will output incorrect answers
+    - finally, return the kth smallest item arry from inorder ( idx: k-1 )
+
+*/
+
+func kthSmallest(root *TreeNode, k int) int {
+    result := []int{}
+    var inorder func(r *TreeNode)
+    inorder = func(r *TreeNode) {
+        // base
+        if r == nil {
+            return
+        }
+        
+        // logic
+        inorder(r.Left)
+        result = append(result, r.Val)
+        inorder(r.Right)
+    }
+    inorder(root)
+    return result[k-1]
+}
 // dfs approach
 // iterative inorder approach
 // time: o(k)
 // space: o(h)
-func kthSmallest(root *TreeNode, k int) int {
-    var inorder func(r *TreeNode) int
-    inorder = func(r *TreeNode) int {
-        // base
-        if r == nil {
-            return -1
-        }
+// func kthSmallest(root *TreeNode, k int) int {
+//     var inorder func(r *TreeNode) int
+//     inorder = func(r *TreeNode) int {
+//         // base
+//         if r == nil {
+//             return -1
+//         }
         
         
-        // logic
-        left := inorder(r.Left)
-        if left != -1 {
-            return left
-        }
-        k--
-        if k == 0 {
-            return r.Val
-        }
-        right := inorder(r.Right)
-        if right != -1 {
-            return right
-        }
-        return -1
-    }
-    return inorder(root)
-}
+//         // logic
+//         left := inorder(r.Left)
+//         if left != -1 {
+//             return left
+//         }
+//         k--
+//         if k == 0 {
+//             return r.Val
+//         }
+//         right := inorder(r.Right)
+//         if right != -1 {
+//             return right
+//         }
+//         return -1
+//     }
+//     return inorder(root)
+// }
 
 
 // iterative inorder approach
