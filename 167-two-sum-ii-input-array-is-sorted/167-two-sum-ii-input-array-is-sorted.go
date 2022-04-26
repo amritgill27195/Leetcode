1bd11dc72bfe2da3af20f8d03258a84ec288260d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-26 12:30:00 -0400

    Time: 16 ms (49.35%), Space: 5.4 MB (49.57%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
@@ -38,21 +38,59 @@
 
 */
 
+// func twoSum(nums []int, target int) []int {
+//     for i := 0; i < len(nums); i++ {
+//         diff := target - nums[i]
+//         left := i+1
+//         right := len(nums)-1
+//         for left <= right {
+//             mid := left + (right-left)/2
+//             if nums[mid] == diff {
+//                 return []int{i+1, mid+1}
+//             } else if nums[mid] > diff {
+//                 right = mid-1
+//             } else {
+//                 left = mid+1
+//             }
+//         }
+//     }
+//     return []int{-1,-1}
+// }
+
+
+
+/*
+    Sorted array, then we should consider binary search and or two pointers
+    
+    approach: two pointers
+    - take a left and right pointer ( start and end of nums array )
+    - if their sum == target, return those indicies
+    - otherwise if sum > target 
+        - move right pointer back as moving this pointer back will result into smaller sum
+        - thereby increasing the chances of being equal to target
+    - else move left pointer forward
+        - because if their sum is smaller than the target
+        - moving right pointer back in a sorted array will only make the sum smaller
+        - moving left pointer forward will help in this case and increase chances of getting equal to target
+    
+    
+    time: o(n)
+    spaceL o(1)
+
+*/
+
 func twoSum(nums []int, target int) []int {
-    for i := 0; i < len(nums); i++ {
-        diff := target - nums[i]
-        left := i+1
-        right := len(nums)-1
-        for left <= right {
-            mid := left + (right-left)/2
-            if nums[mid] == diff {
-                return []int{i+1, mid+1}
-            } else if nums[mid] > diff {
-                right = mid-1
-            } else {
-                left = mid+1
-            }
+    left := 0
+    right := len(nums)-1
+    for left < right {
+        sum := nums[left] + nums[right]
+        if sum == target {
+            return []int{left+1, right+1}
+        } else if sum > target {
+            right--
+        } else {
+            left++
         }
     }
-    return []int{-1,-1}
+    return nil
 }
