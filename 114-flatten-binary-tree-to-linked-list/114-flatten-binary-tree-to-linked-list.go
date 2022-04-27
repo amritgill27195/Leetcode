deb547df4f23e68a9e0b21334d730ac045c6610e
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-26 22:42:47 -0400

    Time: 0 ms (100.00%), Space: 2.9 MB (24.52%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.go
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.go
@@ -20,9 +20,11 @@ func flatten(root *TreeNode)  {
         tmp := root.Right
         root.Right = root.Left
         root.Left = nil
+        // go right as much as possible to get to the tail end of the linkedlist
         for root.Right != nil {
             root = root.Right
         }
+        // once we have reached the tail end, set the tail.Right to tmp ( prev right )
         root.Right = tmp
     }
     
