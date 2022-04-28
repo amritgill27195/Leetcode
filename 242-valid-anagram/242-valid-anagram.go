1f1738faa2035ed030c691640b61ff6f279fb18d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-28 12:50:46 -0400

    Time: 22 ms (8.46%), Space: 6.4 MB (5.46%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.go
+++ 242-valid-anagram/242-valid-anagram.go
@@ -1,26 +1,43 @@
+import (
+    "sort"
+    "strings"
+)
 // map solution
 // time: o(s+t)
 // space: o(min(s,t))
+// func isAnagram(s string, t string) bool {
+//     // make sure s is the smaller string
+//     if len(t) < len(s) {
+//         return isAnagram(t,s)
+//     }
+    
+//     sMap := map[string]int{}
+//     for _, char := range s{
+//         sMap[string(char)]++
+//     }
+    
+//     for _, char := range t {
+//         tChar := string(char)
+//         val, ok := sMap[tChar]
+//         if !ok {
+//             return false
+//         } else if val == 0 {
+//             return false
+//         }
+//         sMap[tChar]--
+//     }
+//     return true
+// }
+
+
+
+// sort both strings and return equality check
 func isAnagram(s string, t string) bool {
-    // make sure s is the smaller string
-    if len(t) < len(s) {
-        return isAnagram(t,s)
-    }
-    
-    sMap := map[string]int{}
-    for _, char := range s{
-        sMap[string(char)]++
-    }
-    
-    for _, char := range t {
-        tChar := string(char)
-        val, ok := sMap[tChar]
-        if !ok {
-            return false
-        } else if val == 0 {
-            return false
-        }
-        sMap[tChar]--
-    }
-    return true
+    news := strings.Split(s, "")
+    sort.Strings(news)
+    s = strings.Join(news, "")
+    newt := strings.Split(t, "")
+    sort.Strings(newt)
+    t = strings.Join(newt, "")
+    return s == t
 }
