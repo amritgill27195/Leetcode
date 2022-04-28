16ce4685022f187ae59eb0cf4ee15254a3552ebe
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-28 12:38:49 -0400

    Time: 61 ms (94.61%), Space: 9.4 MB (29.42%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.go
+++ 217-contains-duplicate/217-contains-duplicate.go
@@ -1,25 +1,25 @@
-// func containsDuplicate(nums []int) bool {
-    
-    
-//     // hashmap
-//     set := map[int]struct{}{}
-//     for i := 0; i < len(nums); i++ {
-//         if _, seen := set[nums[i]]; seen {
-//             return true
-//         }
-//         set[nums[i]] =struct{}{}
-//     }
-    
-//     return false
-// }
-
-// sorting
 func containsDuplicate(nums []int) bool {
-    sort.Ints(nums)
-    for i := 1; i < len(nums); i++ {
-        if nums[i] == nums[i-1] {
+    
+    
+    // hashmap
+    set := map[int]struct{}{}
+    for i := 0; i < len(nums); i++ {
+        if _, seen := set[nums[i]]; seen {
             return true
         }
+        set[nums[i]] =struct{}{}
     }
+    
     return false
 }
+
+// sorting
+// func containsDuplicate(nums []int) bool {
+//     sort.Ints(nums)
+//     for i := 1; i < len(nums); i++ {
+//         if nums[i] == nums[i-1] {
+//             return true
+//         }
+//     }
+//     return false
+// }
