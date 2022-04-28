c7d083aae0152dd8bac43c48be0cb46fae614f71
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-27 21:03:23 -0400

    Time: 3 ms (25.36%), Space: 2.2 MB (62.32%) - LeetHub

---
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.go
@@ -0,0 +1,49 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (n NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (n NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (n *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (n NestedInteger) GetList() []*NestedInteger {}
+ */
+func depthSum(nestedList []*NestedInteger) int {
+    if nestedList == nil {
+        return 0
+    }
+    var dfs func(nl []*NestedInteger, depth int) int
+    dfs = func(nl []*NestedInteger, depth int) int {
+        // base
+        if nl == nil || len(nl) == 0 {
+            return 0
+        }
+        
+        // logic
+        total := 0
+        for i := 0; i < len(nl); i++ {
+            if nl[i].IsInteger() {
+                total += nl[i].GetInteger() * depth
+            } else {
+                total += dfs(nl[i].GetList(), depth+1)
+            }
+        }
+        return total
+    }
+    return dfs(nestedList, 1)
+}
