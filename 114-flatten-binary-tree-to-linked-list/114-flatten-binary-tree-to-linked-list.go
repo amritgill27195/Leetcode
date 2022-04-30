66af79cedd6efe9bc487322382f73d05c97c72ff
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-30 17:51:43 -0400

    Time: 4 ms (42.86%), Space: 2.8 MB (81.11%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.go
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.go
@@ -10,32 +10,49 @@
 // time: o(n)
 // space: o(h)
 // approach recursion
-func flatten(root *TreeNode)  {
+// func flatten(root *TreeNode)  {
     
-    // base
-    if root == nil {
-        return
-    }
+//     // base
+//     if root == nil {
+//         return
+//     }
     
-    //logic
-    flatten(root.Left)
-    if root.Left != nil {
-        tmp := root.Right
-        root.Right = root.Left
-        root.Left = nil
-        // go right as much as possible to get to the tail end of the linkedlist
-        for root.Right != nil {
-            root = root.Right
-        }
-        // once we have reached the tail end, set the tail.Right to tmp ( prev right )
-        root.Right = tmp
-    }
-    flatten(root.Right)    
-}
+//     //logic
+//     flatten(root.Left)
+//     if root.Left != nil {
+//         tmp := root.Right
+//         root.Right = root.Left
+//         root.Left = nil
+//         // go right as much as possible to get to the tail end of the linkedlist
+//         for root.Right != nil {
+//             root = root.Right
+//         }
+//         // once we have reached the tail end, set the tail.Right to tmp ( prev right )
+//         root.Right = tmp
+//     }
+//     flatten(root.Right)    
+// }
 
 // iterative
-// func flatten(root *TreeNode)  {
-//     var stack []*TreeNode{}
-    
+func flatten(root *TreeNode)  {
+    var stack []*TreeNode
+    for root != nil || len(stack) != 0 {
+        for root != nil {
+            stack = append(stack, root)
+            root = root.Left
+        }
+        root = stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        if root.Left != nil {
+            tmp := root.Right
+            root.Right = root.Left
+            root.Left = nil
+            for root.Right != nil {
+                root = root.Right
+            }
+            root.Right = tmp
+        }
+        root = root.Right
+    }
 
-// }
+}
