6ad700f145d92c37f34bd9e2b4127eb82ef8a360
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-30 19:23:06 -0400

    Time: 59 ms (41.03%), Space: 7.8 MB (43.59%) - LeetHub

--- 490-the-maze/490-the-maze.go
+++ 490-the-maze/490-the-maze.go
@@ -1,35 +1,34 @@
 func hasPath(maze [][]int, start []int, destination []int) bool {
     m := len(maze)
     n := len(maze[0])
-    
     sr := start[0]
     sc := start[1]
     dr := destination[0]
     dc := destination[1]
-
-    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
+    maze[sr][sc] = -1
     
-    maze[sr][sc] = 2
+    dirs := [][]int{{-1,0},{1,0},{0,-1},{0,1}}
+    
     q := [][]int{{sr,sc}}
-    
     for len(q) != 0 {
         dq := q[0]
         q = q[1:]
-        if dq[0] == dr && dq[1] == dc {
-            return true
-        }
+        
+        cr := dq[0]
+        cc := dq[1]
+        if cr == dr && cc == dc {return true}
         
         for _, dir := range dirs {
-            r := dq[0] + dir[0]
-            c := dq[1] + dir[1]
+            r := cr + dir[0]
+            c := cc + dir[1]
             for r >= 0 && r < m && c >= 0 && c < n && maze[r][c] != 1 {
                 r += dir[0]
                 c += dir[1]
             }
             r -= dir[0]
             c -= dir[1]
-            if r >= 0 && r < m && c >= 0 && c < n && maze[r][c] != 2 {
-                maze[r][c] = 2
+            if maze[r][c] == 0 {
+                maze[r][c] = -1 // mark this visited
                 q = append(q, []int{r,c})
             }
         }
