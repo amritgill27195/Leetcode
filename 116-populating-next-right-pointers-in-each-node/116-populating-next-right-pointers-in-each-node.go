2578e557842c192d880c9bb8d533f12d01879354
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-01 10:31:57 -0400

    Time: 10 ms (34.59%), Space: 6.8 MB (40.49%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.go
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.go
@@ -10,25 +10,48 @@
 
 
 // classis bfs level order using a queue
+// func connect(root *Node) *Node {
+//     if root == nil {return root}	
+//     q := []*Node{root}
+//     for len(q) != 0 {
+//         qSize := len(q)
+//         for i := 0; i < qSize; i++ {
+//             dq := q[0]
+//             q = q[1:]
+//             if i != qSize-1 {
+//                 dq.Next = q[0]
+//             }
+//             if dq.Left != nil {
+//                 q = append(q, dq.Left)
+//             }
+//             if dq.Right != nil {
+//                 q = append(q, dq.Right)
+//             }
+//         }
+//     }
+//     return root 
+// }
+
+// DFS
 func connect(root *Node) *Node {
-    if root == nil {return root}	
-    q := []*Node{root}
-    for len(q) != 0 {
-        qSize := len(q)
-        for i := 0; i < qSize; i++ {
-            dq := q[0]
-            q = q[1:]
-            if i != qSize-1 {
-                dq.Next = q[0]
-            }
-            if dq.Left != nil {
-                q = append(q, dq.Left)
-            }
-            if dq.Right != nil {
-                q = append(q, dq.Right)
-            }
+    var dfs func(r *Node)
+    dfs = func(r *Node) {
+        // base
+        if r == nil {
+            return
         }
-    }
-    return root
+        // logic
+        if r.Left != nil {
+            r.Left.Next = r.Right
+        }
+        
+        if r.Next != nil && r.Right != nil {
+            r.Right.Next = r.Next.Left
+        }
     
+        dfs(r.Left)
+        dfs(r.Right)
+    }
+    dfs(root)
+    return root
 }
