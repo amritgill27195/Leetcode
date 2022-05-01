4f34e8f7ba69e9efe74ce75f54aa8751daa80a4a
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-30 22:24:50 -0400

    Time: 17 ms (40.73%), Space: 5 MB (67.69%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.go
+++ 695-max-area-of-island/695-max-area-of-island.go
@@ -1,51 +1,50 @@
 
 /*
-    connected components
-    BFS
-    We will mark a land visited by changing the land number to 0 so its not recounted by some other child
+    We are not looking for shortest path, simply count areas of an island
     
-    need to figure out how to do dfs
+
+    approach: DFS
+    - For each 1 we run into
+    - deploy dfs from that r,c
+        - DFS will mark each node visited by changing a land(1) to water(0)
+        - This way the same cell is not re-added by a neighboring land
+    - The dfs will return the area of the island formed using the r,c from above
+    - Compare and save the returned area with max area
 */
 
 func maxAreaOfIsland(grid [][]int) int {
-    
-    
-    if grid == nil {
-        return 0
-    }
-    dirs := [][]int{ {1,0}, {-1,0}, {0,1}, {0,-1} }
+    max := 0
     m := len(grid)
     n := len(grid[0])
+    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
     
-    q := [][]int{}
-    maxArea := 0
+    var dfs func(r, c int) int
+    dfs = func(r, c int) int {
+        // base
+        if r < 0 || r == m || c < 0 || c == n || grid[r][c] == 0 {
+            return 0
+        }
+        
+        // logic
+        count := 1
+        grid[r][c] = 0
+        for _, dir := range dirs {
+            count += dfs(r+dir[0], c+dir[1])
+        }
+        return count
+    }
+    
+    
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            
             if grid[i][j] == 1 {
-                grid[i][j] = 0
-                q = append(q, []int{i,j})
-                area := 0
-                for len(q) != 0 {
-                    area++
-                    dq := q[0]
-                    q = q[1:]
-                    for _, dir := range dirs {
-                        r := dq[0] + dir[0]
-                        c := dq[1] + dir[1]
-                        if r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1 {
-                            grid[r][c] = 0
-                            q = append(q, []int{r,c})
-                        }
-                    }
+                area := dfs(i,j)
+                if area > max {
+                    max = area
                 }
-                if area > maxArea {
-                    maxArea = area
-                }
-                
             }
         }
     }
     
-    return maxArea
+    return max
 }
