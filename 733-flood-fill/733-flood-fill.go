a4a2f6e37270b5fcd7f57a7f65d4242ca9f85f9d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-30 22:11:22 -0400

    Time: 3 ms (97.01%), Space: 4.2 MB (73.74%) - LeetHub

--- 733-flood-fill/733-flood-fill.go
+++ 733-flood-fill/733-flood-fill.go
@@ -24,66 +24,30 @@
 */
 
 
-
-
-// dfs
-// func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
-    
-//     if image == nil {return image}
-//     oldCol := image[sr][sc]
-//     m := len(image)
-//     n := len(image[0])
-            
-//     var dfs func(r,c,nc,oc int)
-//     dfs = func(r,c,nc,oc int) {        
-//         // base
-//         if r < 0 || r >= m || c < 0 || c >= n || image[r][c] == nc || image[r][c] != oc {
-//             return
-//         }
-        
-//         // logic
-//         image[r][c] = nc
-//         dfs(r+1, c, nc,oc)
-//         dfs(r-1, c, nc,oc)
-//         dfs(r, c-1, nc,oc)
-//         dfs(r, c+1, nc,oc)
-//     }
-    
-//     dfs(sr, sc, newColor, oldCol)
-//     return image
-// }
-
-
-
-// using level order with BFS
+// DFS ( recursive )
 func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
-    
-    if image == nil || len(image) == 0 {
+    oldColor := image[sr][sc]
+    if newColor == oldColor {
         return image
     }
-    oldColor := image[sr][sc]
+    m := len(image)
+    n := len(image[0])
     
-    if oldColor != newColor {
-        queue := [][]int{ {sr,sc} } // [ [r,c] ]
-        dirs := [][]int{ {1,0},{-1,0},{0,1},{0,-1} }
-        image[sr][sc] = newColor
-        m := len(image)
-        n := len(image[0])
-
-        for len(queue) != 0 {
-            dq := queue[0]
-            queue = queue[1:]
-
-            for _, dir := range dirs {
-                r := dq[0] + dir[0]
-                c := dq[1] + dir[1]
-                if r >= 0 && r < m && c >= 0 && c < n && image[r][c] == oldColor {
-                    image[r][c] = newColor
-                    queue = append(queue, []int{r,c})
-                }
-            }
+    dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
+    var dfs func(r, c int) 
+    dfs = func(r, c int) {
+        // base
+        if r < 0 || r == m || c < 0 || c == n || image[r][c] == newColor || image[r][c] != oldColor {
+            return
+        }
+        
+        // logic
+        image[r][c] = newColor
+        for _, dir := range dirs {
+            dfs(r+dir[0],c+dir[1])
         }
     }
-    
+    dfs(sr, sc)
     return image
 }
+
