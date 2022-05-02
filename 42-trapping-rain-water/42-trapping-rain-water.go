146cd7bbeb2fce3fb7b09935ec68444ab343681c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-02 08:47:51 -0400

    Time: 12 ms (54.03%), Space: 4.6 MB (77.79%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.go
@@ -0,0 +1,35 @@
+func trap(height []int) int {
+    
+    slow := 0
+    fast := 1
+    n := len(height)
+    trap := 0
+    result := 0
+    
+    for fast < n {
+        if height[fast] < height[slow] {
+            trap += height[slow]-height[fast]
+        } else {
+            result += trap
+            trap = 0
+            slow = fast
+        }
+        fast++
+    }
+    peak := slow
+    slow = n-1
+    fast = slow-1
+    trap = 0
+    for fast >= peak {
+        if height[fast] < height[slow] {
+            trap += height[slow]-height[fast]
+        } else {
+            result += trap
+            trap = 0
+            slow = fast
+        }
+        fast--
+    }
+    return result
+    
+}
