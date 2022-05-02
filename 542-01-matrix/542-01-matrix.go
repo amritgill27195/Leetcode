3aea46f7240c3f941818a48159cb94dbfb312b30
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-02 14:39:58 -0400

    Time: 104 ms (40.66%), Space: 8.1 MB (40.11%) - LeetHub

--- 542-01-matrix/542-01-matrix.go
+++ 542-01-matrix/542-01-matrix.go
@@ -1,56 +1,38 @@
 
-
-/*
-    connected components!
-    A dist in a cell (that already reached 0) can tell us a dist for a cell that cannot reach 0 directly
-    
-    approach: BFS
-    - We need starting INDEPENDANT nodes.
-    - 0s do not depend on anything, we are not looking for distance of each 0 to another 0
-    - So 0s are independant
-    - Enqueue all 0 positions
-    - While we are all looking for all 0's , we will mark all the 1's = -1
-        - Why? Well this is to distinguish between 1 as a distance vs visited/not-visited.
-    - So when we are processing all 0's from our queue
-    - We will look for all -1's ( unvisited nodes around this 0 )
-    - When we find it, we will enqueue all neighbouring -1's ( but change the value of them to currentNode val + 1)
-    - Then we will have cells with 1's in our queue
-    - Keep looking for all -1's around each item from our queue, keep adding current val + 1 to new all -1's we find.
-    - Instead of keeping the distance maintained at each level, the cell being processed itself will tell us how far away this cell is from a 0.
-    - We can take that info and add + 1 for its neighbors that are -1.
-    - And then repeat until full matrix is processed
-    
-    Time: o(mn)
-    space: o(mn)
-*/
-
+// BFS with extra space ( using another matrix to keep track of visited {r:c} )
 func updateMatrix(mat [][]int) [][]int {
-    
     q := [][]int{}
     m := len(mat)
     n := len(mat[0])
+    visited := make([][]bool, m)
+    for idx, _ := range visited {
+        visited[idx] = make([]bool, n)
+    }
+    
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             if mat[i][j] == 0 {
                 q = append(q, []int{i,j})
-            } else {
-                mat[i][j] = -1
+                visited[i][j] = true
             }
         }
     }
-    
-    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
+    dirs := [][]int{{1,0},{-1,0}, {0,-1},{0,1}}
     for len(q) != 0 {
         dq := q[0]
         q = q[1:]
+        cr := dq[0]
+        cc := dq[1]
+        cv := mat[cr][cc]
         for _, dir := range dirs {
-            r := dq[0] + dir[0]
-            c := dq[1] + dir[1]
-            if r >= 0 && r < m && c >=0 && c < n && mat[r][c] == -1 {
-                mat[r][c] = mat[dq[0]][dq[1]] + 1
+            r := cr+dir[0]
+            c := cc+dir[1]
+            if r >= 0 && r < m && c >= 0 && c < n && mat[r][c] == 1 && !visited[r][c] {
+                visited[r][c] = true
+                mat[r][c] = cv + 1
                 q = append(q, []int{r,c})
             }
         }
-    } 
+    }
     return mat
 }
