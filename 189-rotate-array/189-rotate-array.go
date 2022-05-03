c3703896a30573541df06e36620d6fda572c7f8a
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-03 19:17:27 -0400

    Time: 30 ms (80.66%), Space: 8.4 MB (58.37%) - LeetHub

--- 189-rotate-array/189-rotate-array.go
+++ 189-rotate-array/189-rotate-array.go
@@ -1,22 +1,23 @@
-// approach: 
-// reverse full
-// reverse 0 : k-1 indicies
-// reverse k : end
-// MAKE SURE K is relative to array size: i.e k = k % len(arr)
-
 func rotate(nums []int, k int)  {
     k %= len(nums)
-	reverse(nums, 0, len(nums)-1)
-	reverse(nums, 0, k-1)
-    reverse(nums, k, len(nums) - 1);
-
+    
+    // reverse whole arr
+    nums = reverse(0, len(nums)-1, nums)
+    
+    // reverse 0:k-1 elements
+    nums = reverse(0, k-1, nums)
+    
+    // reverse k:end elements
+    nums = reverse(k, len(nums)-1, nums)
+    
 }
 
 
-func reverse(nums []int, low, high int) {
-	for low < high {
-		nums[low], nums[high] = nums[high], nums[low]
-		low++
-		high--
-	}
+func reverse(left, right int, nums []int) []int {
+    for left < right {
+        nums[left], nums[right] = nums[right],nums[left]
+        left++
+        right--
+    }
+    return nums
 }
