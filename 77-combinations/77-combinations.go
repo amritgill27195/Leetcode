ec5021acc3e85bc7c1d11da3eab095d81065d8fd
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-04 15:53:49 -0400

    Time: 14 ms (65.65%), Space: 7.7 MB (59.74%) - LeetHub

--- 77-combinations/77-combinations.go
+++ 77-combinations/77-combinations.go
@@ -1,10 +1,8 @@
 func combine(n int, k int) [][]int {
-    
     var result [][]int
     
-    var dfs func(paths []int, start int)
+    var dfs func(paths []int, start int) 
     dfs = func(paths []int, start int) {
-        
         // base
         if len(paths) == k {
             newL := make([]int, len(paths))
@@ -12,10 +10,12 @@ func combine(n int, k int) [][]int {
             result = append(result, newL)
             return
         }
-        
+        if start > n {
+            return
+        }
         
         // logic
-        for i := start; i <= n; i++ {
+        for i := start;i <= n; i++{
             // action
             paths = append(paths, i)
             // recurse
@@ -23,10 +23,7 @@ func combine(n int, k int) [][]int {
             // backtrack
             paths = paths[:len(paths)-1]
         }
-        
     }
-    
     dfs(nil, 1)
     return result
-    
 }
