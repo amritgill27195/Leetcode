de4413e27520e3bb0e6adb5eabeb08df78eb4a30
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-04 19:00:13 -0400

    Time: 9 ms (69.23%), Space: 6.5 MB (46.75%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.go
+++ 784-letter-case-permutation/784-letter-case-permutation.go
@@ -1,8 +1,8 @@
 
 func letterCasePermutation(s string) []string {
     var result []string
-    var helper func(i int, path string)
-    helper = func(i int, path string)  {
+    var backtrack func(i int, path string)
+    backtrack = func(i int, path string)  {
         // base
         if i == len(s) {
             result = append(result, path)
@@ -14,12 +14,12 @@ func letterCasePermutation(s string) []string {
         char := s[i]
         strChar := string(char)
         if char >= '0' && char <= '9' {
-            helper(i+1, path+strChar)
+            backtrack(i+1, path+strChar)
         } else {
-            helper(i+1, path+strings.ToUpper(strChar))
-            helper(i+1, path+strings.ToLower(strChar))
+            backtrack(i+1, path+strings.ToUpper(strChar))
+            backtrack(i+1, path+strings.ToLower(strChar))
         }
     }
-    helper(0, "")
+    backtrack(0, "")
     return result
 }
