e12d238beb1c9733adc14c8705d5c37377bd64a9
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-04 18:59:34 -0400

    Time: 24 ms (14.20%), Space: 6.3 MB (93.49%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.go
+++ 784-letter-case-permutation/784-letter-case-permutation.go
@@ -1,27 +1,25 @@
 
 // PUKE.... horrible horrible -- TODO: revisit this dumbass
 func letterCasePermutation(s string) []string {
-    seen := map[string]bool{}
     var result []string
     var helper func(i int, path string)
     helper = func(i int, path string)  {
         // base
         if i == len(s) {
-            if _, ok := seen[path]; !ok {
-                seen[path] = true
-                result = append(result, path)
-            }
+            result = append(result, path)
             return
         }
         
         
         // logic
-        strChar := string(s[i])
-        upper := strings.ToUpper(strChar)
-        lower := strings.ToLower(strChar)
-        
-        helper(i+1, path+upper)
-        helper(i+1, path+lower)
+        char := s[i]
+        strChar := string(char)
+        if char >= '0' && char <= '9' {
+            helper(i+1, path+strChar)
+        } else {
+            helper(i+1, path+strings.ToUpper(strChar))
+            helper(i+1, path+strings.ToLower(strChar))
+        }
     }
     helper(0, "")
     return result
