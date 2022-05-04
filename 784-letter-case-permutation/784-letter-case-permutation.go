633360647caa701daa681ee401da7bae3c2cd562
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-04 18:39:26 -0400

    Time: 31 ms (8.88%), Space: 6.9 MB (12.43%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.go
@@ -0,0 +1,25 @@
+func letterCasePermutation(s string) []string {
+    res := map[string]bool{}
+    var result []string
+    var helper func(i int, path string)
+    helper = func(i int, path string)  {
+        // base
+        if len(path) == len(s) {
+            res[path] = true
+            return
+        }
+        if i == len(s) {return}
+        
+        
+        // logic
+        upper := strings.ToUpper(string(s[i]))
+        lower := strings.ToLower(string(s[i]))
+        helper(i+1, path+upper)
+        helper(i+1, path+lower)
+    }
+    helper(0, "")
+    for k , _ := range res {
+        result = append(result, k)
+    }
+    return result
+}
