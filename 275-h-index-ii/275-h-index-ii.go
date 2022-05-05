19f291906aeaaac47a872b2929c59e34ef69777e
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-04 23:48:16 -0400

    Time: 19 ms (77.42%), Space: 6.3 MB (87.10%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.go
+++ 275-h-index-ii/275-h-index-ii.go
@@ -20,8 +20,11 @@
     - if n-idx <= citations[idx]
         - in brute force ( linear traverse, we returned right away because its the first element from left side )
         - but in binary search, we will save it as it may NOT be the first element and so we will save it and search left
+            HOWEVER, if diff is the value at that idx, just return, we wont find a better ans
     - otherwise
         - search right
+    
+    - Also this sounds like find either idx whose value == n-idx || if closest to n-idx
     
 time: o(logn)
 space: o(1)
@@ -35,6 +38,7 @@ func hIndex(citations []int) int {
     for left <= right {
         mid := left + (right-left)/2
         if n-mid <= citations[mid] {
+            // if diff is the value at that idx, just return, we wont find a better ans
             if n-mid == citations[mid] {
                 return n-mid
             }
