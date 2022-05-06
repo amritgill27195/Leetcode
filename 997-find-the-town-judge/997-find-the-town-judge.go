4dfcbdda955e2492a737f9904805b0f80783c687
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-06 14:57:01 -0400

    Time: 144 ms (35.42%), Space: 7.9 MB (41.15%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.go
+++ 997-find-the-town-judge/997-find-the-town-judge.go
@@ -1,13 +1,36 @@
+/*
+    approach: 
+    
+    indegrees array to track number of incoming edge at a node,
+    then whichever node has exactly n-1 value, thats the node with the most amount
+    of incoming egdes 
+    
+    if we do not find a n-1 value, then that means there is no trusted person, return -1
+    
+    remember indegrees array counts the number of incoming edges to $this node ( i.e this translates to idx which is a node )
+    
+    in this case, we will track number of trusts given to a node 
+    then look for a node with exactly n-1 value
+    
+    time: o(n)
+    space: o(n) -- for indegrees array
+*/
+
 func findJudge(n int, trust [][]int) int {
-    indegree := make([]int, n)
-    for i := 0; i < len(trust); i++ {
-        person := trust[i][0]
-        trustedPerson := trust[i][1]
-        indegree[person-1]--
-        indegree[trustedPerson-1]++
+    indegrees := make([]int, n)
+    
+    for _, ele := range trust {
+        person := ele[0]
+        trusts := ele[1]
+        
+        indegrees[trusts-1]++
+        indegrees[person-1]--
     }
-    for i := 0; i < len(indegree); i++ {
-        if indegree[i] == n-1{return i+1}
+    
+    for i := 0; i < len(indegrees); i++ {
+        if indegrees[i] == n-1 {
+            return i+1
+        }
     }
     return -1
 }
