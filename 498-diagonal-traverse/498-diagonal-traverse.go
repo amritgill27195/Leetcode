ad3ea60e19b2b8c24f5369741cc7f4cb44717d23
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-08 19:39:44 -0400

    Time: 26 ms (72.39%), Space: 7.8 MB (21.64%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.go
+++ 498-diagonal-traverse/498-diagonal-traverse.go
@@ -1,17 +1,58 @@
+/*
+matrix traversals are all about watching for boundaries!
+
+We are going in either or directions
+1. up
+2. down
+
+when going up, what boundaries do we hit when we are going out of bounds?
+
+    We will hit
+        top row or
+     _ _ _ _ _ 
+    |         |
+    |         |
+    |         |  right col
+    |         |
+    |_ _ _ _ _|
+
+So if going up ever reaches row == 0 ( top row )
+then go to next col ( c++ ) and change direction and go down
+if going up every ever hits right col, then row++ and change direction to go down
+
+
+when going down, what boundaries do we hit when we are going out of bounds?
+         _ _ _ _ _ 
+        |         |
+        |         |
+left col|         |
+        |         |
+        |_ _ _ _ _|
+            bottom
+            row
+            
+
+if going down we ever hit col 0, then row++ and change direction to go up
+if going down we ever hit bottom last row ( m-1 ), then c++ and change direction
+
+
+*/
+
 func findDiagonalOrder(mat [][]int) []int {
-    m := len(mat)
-    n := len(mat[0])
-    
+    if mat == nil {
+        return nil
+    }
+    up := true
     r := 0
     c := 0
+    m := len(mat)
+    n := len(mat[0])
     idx := 0
-    up := true
+    out := []int{}
     
-    var out []int
     for idx < m*n {
         idx++
         out = append(out, mat[r][c])
-        
         if up {
             if c == n-1 {
                 up = false
@@ -25,8 +66,8 @@ func findDiagonalOrder(mat [][]int) []int {
             }
         } else {
             if r == m-1 {
-                up = true
                 c++
+                up = true
             } else if c == 0 {
                 up = true
                 r++
@@ -38,3 +79,12 @@ func findDiagonalOrder(mat [][]int) []int {
     }
     return out
 }
+
+
+
+
+
+
+
+
+
