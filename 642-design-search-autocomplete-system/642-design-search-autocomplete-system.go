efbad569f1db4c0d3c71fc62e5cfc8a2905d7217
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-08 11:53:27 -0400

    Time: 358 ms (51.47%), Space: 8.9 MB (85.29%) - LeetHub

--- 642-design-search-autocomplete-system/642-design-search-autocomplete-system.go
+++ 642-design-search-autocomplete-system/642-design-search-autocomplete-system.go
@@ -1,5 +1,10 @@
+
+/*
+    approach: brute force
+    - using a freqMap
+*/
 type AutocompleteSystem struct {
-    input string
+    input *strings.Builder
     m map[string]int
 }
 
@@ -13,29 +18,38 @@ func Constructor(sentences []string, times []int) AutocompleteSystem {
     }
     return AutocompleteSystem{
         m: m,
-        input: "",
+        input: new(strings.Builder),
     }
 }
 
 
+// time: o(nk) + o(nlogn)
+// avg time: o(nlogn)
+// space: o(n) 
 func (this *AutocompleteSystem) Input(c byte) []string {
     if c == '#' {
-        this.m[this.input]++
-        this.input = ""
+        this.m[this.input.String()]++
+        this.input = new(strings.Builder)
         return nil
     }
-    this.input += string(c)
+    this.input.WriteByte(c)
+    
     type pair struct {
         s string
         t int
     }
+    
     tmp := []*pair{}
+    // time: o(n) -- we loop thru entire map AND
     for key, val := range this.m {
-        if strings.HasPrefix(key, this.input) {
+        // time: o(k) - where k is the length of this.input...
+        if strings.HasPrefix(key, this.input.String()) {
             tmp = append(tmp, &pair{s: key, t: val})
         }
     }
+    // so above time is o(nk) - k will likely be a smaller term on avg so o(n) on avg
     
+    // sorting the entire tmp result -- o(nlogn)
     sort.Slice(tmp, func(i,j int)bool{
         if tmp[i].t == tmp[j].t {
             return tmp[i].s < tmp[j].s
@@ -43,6 +57,7 @@ func (this *AutocompleteSystem) Input(c byte) []string {
         return tmp[i].t > tmp[j].t
     })
     
+    // time: o(1) -- its constant -- 3 -- we break as soon as we have 3 results...
     result := []string{}
     for j := 0; j < len(tmp); j++ {
         result = append(result, tmp[j].s)
