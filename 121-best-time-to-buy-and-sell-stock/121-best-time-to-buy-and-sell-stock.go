c59f1abe1f6119cefa35377552382f0810f58716
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-08 20:45:52 -0400

    Time: 205 ms (20.74%), Space: 12.1 MB (5.24%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
@@ -1,3 +1,24 @@
+
+/*
+    since we have to return the largest possible profit,
+    We want to buy it on the cheapest day possible and sell it on the day that gives us most profit
+    
+    We will start by implying that cheapest day is at idx 0
+    so we will buy at price[0]
+    
+    Then loop over prices from 1, and check the profit if we were to sell on for ith price.
+    (price[i]-buy) -- if this profit > max - then update max to the new number.
+    
+    However, because we implied that at 0th idx, the price is the cheapest, we need to also keep on checking
+    if the current ith price is cheaper/smaller than the price we originally bought it for.
+    If it is, save this as our new buy price and compare with rest of the selling prices down the line.
+    
+    Return the max at the end
+    
+    time: o(n)
+    space: o(1)
+
+*/
 func maxProfit(prices []int) int {
     max := 0
     buy := prices[0]
