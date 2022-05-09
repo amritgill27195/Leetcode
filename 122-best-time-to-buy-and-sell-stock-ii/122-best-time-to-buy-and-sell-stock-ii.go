b1d34cdb937d96f0cb6dd7ef2b0889cf7fe78968
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-08 20:48:40 -0400

    Time: 8 ms (28.72%), Space: 3 MB (29.24%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.go
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.go
@@ -1,3 +1,16 @@
+
+/*
+    We can do as many transactions as possible...
+    This is when we will compare buy on i and sell on i+1 price
+    if there is any profit, add to total profit..
+    
+    Question becomes, if you sold it on a day, can you buy it again after selling it?
+    The answer is not very clear but it is true, you can buy the stock after selling it
+
+    time: o(n)
+    space: o(1)
+
+*/
 func maxProfit(prices []int) int {
     totalProfit := 0
     for i := 1; i < len(prices); i++ {
