06aaa4de0da1d4fdaf7e45d6ae327e0b0cd22ad9
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-11 22:37:37 -0400

    Time: 866 ms (5.27%), Space: 6.6 MB (13.92%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.go
@@ -0,0 +1,31 @@
+// BFS
+// time: exponential
+// space: o(n) for the queue
+func jump(nums []int) int {
+    visitedSet := map[int]bool{}
+    
+    q := []int{0}
+    visitedSet[0] = true
+    jumps := 0
+    for len(q) != 0 {
+        qSize := len(q)
+        for qSize != 0 {
+            dq := q[0]
+            q = q[1:]
+            if dq >= len(nums)-1 {
+                return jumps
+            }
+            for i := 1; i <= nums[dq]; i++ {
+                newIdx := dq+i
+                if newIdx >= len(nums)-1 {return jumps+1}
+                if _, ok := visitedSet[newIdx]; !ok {
+                    visitedSet[newIdx] = true
+                    q = append(q, newIdx)
+                }
+            }
+            qSize--
+        }
+        jumps++
+    }
+    return jumps
+}
