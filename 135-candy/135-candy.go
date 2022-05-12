f77e153d80054831ff00da8c91803057086592d7
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-11 21:19:22 -0400

    Time: 34 ms (31.94%), Space: 6.9 MB (16.67%) - LeetHub

---
+++ 135-candy/135-candy.go
@@ -0,0 +1,29 @@
+func candy(ratings []int) int {
+    
+    candies := make([]int, len(ratings))
+    for i := 0; i < len(candies); i++ {
+        candies[i] = 1
+    }
+    
+    // left pass - if element is greater than its
+    // immediate left element, then leftElementVal + 1 is the value for this position
+    for i := 1; i < len(ratings); i++ {
+        if ratings[i] > ratings[i-1] {
+            candies[i] = candies[i-1]+1
+        }
+    }
+    
+    // right pass 
+    // if ith rating is greater than right element
+    // and the ith value for this rating is NOT greater than
+    // i+1 val, then rightVal + 1
+    total := candies[len(candies)-1]
+    for i := len(ratings)-2; i >= 0; i-- {
+        if ratings[i] > ratings[i+1] && candies[i] < candies[i+1]+1 {
+            candies[i] = candies[i+1]+1
+        }
+        total += candies[i]
+    }
+    return total
+    
+}
