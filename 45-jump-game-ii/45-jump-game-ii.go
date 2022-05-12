bfa2cacc539ccddcddafbac7659dbf32cda885a4
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-11 22:38:44 -0400

    Time: 714 ms (5.91%), Space: 6.9 MB (6.12%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.go
+++ 45-jump-game-ii/45-jump-game-ii.go
@@ -15,6 +15,9 @@ func jump(nums []int) int {
             if dq >= len(nums)-1 {
                 return jumps
             }
+            // explore all the childs
+            // here childs are the next indicies
+            // each child is discovered by jumping from current idx + valAtThatIdx
             for i := 1; i <= nums[dq]; i++ {
                 newIdx := dq+i
                 if newIdx >= len(nums)-1 {return jumps+1}
