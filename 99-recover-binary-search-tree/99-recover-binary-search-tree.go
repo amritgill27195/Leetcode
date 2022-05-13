d9e76124aa24b3e0635f5a21d9ca193a74572621
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-12 21:06:07 -0400

    Time: 11 ms (77.60%), Space: 5.7 MB (100.00%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.go
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.go
@@ -7,39 +7,30 @@
  * }
  */
 func recoverTree(root *TreeNode)  {
+    var first *TreeNode
+    var second *TreeNode
+    var prev *TreeNode
     
-    list := []int{}
     var inorder func(r *TreeNode)
     inorder = func(r *TreeNode) {
         // base
         if r == nil {
             return
-        }
-        
+        }    
         // logic
         inorder(r.Left)
-        list = append(list, r.Val)
+        if prev != nil {
+            if prev.Val >= r.Val {
+                if second == nil {
+                    second = prev
+                }
+                first = r
+            }
+        }
+        prev = r
         inorder(r.Right)
     }
-    
     inorder(root)
-    sort.Ints(list)
-    
-    lPtr := 0
-    var inorderW func(r *TreeNode)
-    inorderW = func(r *TreeNode) {
-        // base
-        if r == nil {
-            return
-        }
-        
-        // logic
-        inorderW(r.Left)
-        r.Val = list[lPtr]
-        lPtr++
-        inorderW(r.Right)
-    }
-    
-    inorderW(root)
+    first.Val, second.Val = second.Val, first.Val
     
 }
