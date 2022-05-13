727b9d32fdd30fb7a9dc304d24752ef43ec21f81
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-12 20:41:11 -0400

    Time: 8 ms (85.40%), Space: 4.5 MB (93.01%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.go
+++ 42-trapping-rain-water/42-trapping-rain-water.go
@@ -20,67 +20,67 @@
     space: o(1)
 */
 
-// func trap(height []int) int {
-//     left := 0
-//     right := len(height)-1
-//     leftMax := height[left]
-//     rightMax := height[right]
-//     totalTrapped := 0
-//     for left < right {
-//         if height[left] < height[right] {
-//             left++
-//             if height[left] > leftMax {
-//                 leftMax = height[left]
-//             }
-//             totalTrapped += leftMax - height[left]
-//         } else {
-//             right--
-//             if height[right] > rightMax {
-//                 rightMax = height[right]
-//             }
-//             totalTrapped += rightMax - height[right]
-//         }
-//     }
-//     return totalTrapped
-// }
+func trap(height []int) int {
+    left := 0
+    right := len(height)-1
+    leftMax := height[left]
+    rightMax := height[right]
+    totalTrapped := 0
+    for left < right {
+        if height[left] < height[right] {
+            left++
+            if height[left] > leftMax {
+                leftMax = height[left]
+            }
+            totalTrapped += leftMax - height[left]
+        } else {
+            right--
+            if height[right] > rightMax {
+                rightMax = height[right]
+            }
+            totalTrapped += rightMax - height[right]
+        }
+    }
+    return totalTrapped
+}
 
 
 /*
     approach: slow and fast pointers : 2 pass
     
 */
-func trap(height []int) int {
+// func trap(height []int) int {
     
-    slow := 0
-    fast := 1
-    n := len(height)
-    trap := 0
-    result := 0
+//     slow := 0
+//     fast := 1
+//     n := len(height)
+//     trap := 0
+//     result := 0
     
-    for fast < n {
-        if height[fast] < height[slow] {
-            trap += height[slow]-height[fast]
-        } else {
-            result += trap
-            trap = 0
-            slow = fast
-        }
-        fast++
-    }
-    peak := slow
-    slow = n-1
-    fast = slow-1
-    trap = 0
-    for fast >= peak {
-        if height[fast] < height[slow] {
-            trap += height[slow]-height[fast]
-        } else {
-            result += trap
-            trap = 0
-            slow = fast
-        }
-        fast--
-    }
-    return result
+//     for fast < n {
+//         if height[fast] < height[slow] {
+//             trap += height[slow]-height[fast]
+//         } else {
+//             result += trap
+//             trap = 0
+//             slow = fast
+//         }
+//         fast++
+//     }
+//     peak := slow
+//     slow = n-1
+//     fast = slow-1
+//     trap = 0
+//     for fast >= peak {
+//         if height[fast] < height[slow] {
+//             trap += height[slow]-height[fast]
+//         } else {
+//             result += trap
+//             trap = 0
+//             slow = fast
+//         }
+//         fast--
+//     }
+//     return result
     
-}
+// }
