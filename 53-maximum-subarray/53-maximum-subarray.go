0b7d8aeef0444771f49434ccd17f117711b3bff2
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-12 20:26:35 -0400

    Time: 107 ms (67.67%), Space: 8.8 MB (84.07%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.go
+++ 53-maximum-subarray/53-maximum-subarray.go
@@ -1,56 +1,10 @@
-
-/*
-    approach: brute force
-    - nested loop
-    - keep track of running sum
-    - save running into max whenever needed
-    
-    time: o(n^2)
-    space: o(1)
-    
-
 func maxSubArray(nums []int) int {
-    max := math.MinInt64
-    for i := 0; i < len(nums); i++ {
-        running := 0
-        for j := i; j < len(nums); j++ {
-            running += nums[j]
-            if running > max {
-                max = running
-            }
-        }
+    rs := nums[0]
+    max := rs
+    
+    for i := 1; i < len(nums); i++ {
+        rs = int(math.Max(float64(rs+nums[i]), float64(nums[i])))
+        max = int(math.Max(float64(rs), float64(max)))
     }
     return max
-}
-*/
-
-
-/*
-    approach: linear running sum
-    - we do what we used to with runningSum pattern
-    - but instead of continuing adding nums as we loop
-    - we will still add current num to runningSum
-    - but since the inputs can be negative integers
-    - the current number on its own may be bigger than currentRunningSum
-    - so, in that case , if current number > runningSum = then runningSum becomes currentNumber
-    - Save the max between max,runningSum
-    
-    time: o(n)
-    space: o(1)
-*/
-
-func maxSubArray(nums []int) int {
-    running := 0
-    max := math.MinInt64
-    for i := 0; i < len(nums); i++ {
-        running += nums[i]
-        running = getMax(running, nums[i])
-        max = getMax(running, max)
-    }
-    return max
-}
-
-func getMax(n,m int) int {
-    if n > m {return n}
-    return m
 }
