a4b79256deeb6aadd9507507c3a610fc35be46b4
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-13 20:28:35 -0400

    Time: 307 ms (53.42%), Space: 7.9 MB (97.26%) - LeetHub

--- 642-design-search-autocomplete-system/642-design-search-autocomplete-system.go
+++ 642-design-search-autocomplete-system/642-design-search-autocomplete-system.go
@@ -1,80 +1,70 @@
-/*
-    approach: brute force
-    - We need a way to make sentences and times array query-able
-    - as in how many times a word repeats 
-    - so to correlate a word with numberOfTimes searched, we will use a map
-    - in our constructor, we will turn the input args into a freqMap
-    - We also need a way to keep on appending the input string to input char
-    - so we will have a strings.builder() thats appending characters of input until we the input char is #
-    - when the input char is #, then we will take the string from stringBuilder and add it back to freqMap
-    - Now to searching...
-    - we will loop thru our freqMap and check if each word starts with our input stringBuilder
-    - if it does we will add it to a list as a pair [word, numTimes]
-    - Then we will sort the list by numTimes ( descending order - large number first )
-        - if the numTimes for i,j are the same, then we will sort by alphabetically
-        - whichever alphabet comes first will be first
-    
-    time: o(nk) -- to populate ans list --  + o(nlogn) -- to sort ans list --- + o(1) to populate a 3 len output list
-    space: o(n) 
-          
-*/
 
 type AutocompleteSystem struct {
-    freqMap map[string]int
-    input *strings.Builder
+	freqMap map[string]int
+	input   *strings.Builder
+	result  []string
 }
-
 
 func Constructor(sentences []string, times []int) AutocompleteSystem {
-    m := map[string]int{}
-    for i := 0; i < len(sentences); i++ {
-        m[sentences[i]] += times[i] 
-    }
-    return AutocompleteSystem{freqMap: m,input: new(strings.Builder)} 
+	m := map[string]int{}
+	for i := 0; i < len(sentences); i++ {
+		m[sentences[i]] += times[i]
+	}
+	return AutocompleteSystem{freqMap: m, input: new(strings.Builder), result: []string{}}
 }
-
-
-
 
 func (this *AutocompleteSystem) Input(c byte) []string {
-    if c == '#' {
-        this.freqMap[this.input.String()]++
-        this.input = new(strings.Builder)
-        return nil
-    }
-    this.input.WriteByte(c)
-    type ansPair struct{
-        word string
-        times int
-    }
-    ans := []*ansPair{}
-    for k, v := range this.freqMap {
-        in := this.input.String()
-        if strings.HasPrefix(k, in) {
-            ans = append(ans, &ansPair{word: k, times: v})
-        }
-    }
-    
-    sort.Slice(ans, func(i, j int)bool {
-        if ans[i].times == ans[j].times {
-            return ans[i].word < ans[j].word
-        }
-        return ans[i].times > ans[j].times
-    })
-   
-    out := []string{}
-    for i := 0; i < len(ans); i++ {
-        out = append(out, ans[i].word)
-        if len(out) == 3 {
-            break
-        }
-    }
-    return out
+	if c == '#' {
+		this.freqMap[this.input.String()]++
+		this.input = new(strings.Builder)
+		return nil
+	}
+	this.input.WriteByte(c)
+	for k, _ := range this.freqMap {
+		in := this.input.String()
+		if strings.HasPrefix(k, in) {
+			heap.Push(this, k)
+			if len(this.result) > 3 {
+				heap.Pop(this)
+			}
+		}
+	}
+	out := []string{}
+	for len(this.result) != 0 {
+		out = append(out, heap.Pop(this).(string))
+	}
+	for i := 0; i < len(out)/2; i++ {
+		out[i], out[len(out)-1-i] = out[len(out)-1-i], out[i]
+	}
+	return out
 }
-
 
 /**
  * Your AutocompleteSystem object will be instantiated and called as such:
  * obj := Constructor(sentences, times);
  * param_1 := obj.Input(c);
  */
+
+// Pop : remove and return element Len() - 1.a
+func (acs *AutocompleteSystem) Pop() interface{} {
+	out := acs.result[len(acs.result)-1]
+	acs.result = acs.result[:len(acs.result)-1]
+	return out
+}
+func (acs *AutocompleteSystem) Swap(i, j int) {
+	acs.result[i], acs.result[j] = acs.result[j], acs.result[i]
+}
+func (acs *AutocompleteSystem) Len() int           { return len(acs.result) }
+func (acs *AutocompleteSystem) Push(x interface{}) { acs.result = append(acs.result, x.(string)) }
+func (acs *AutocompleteSystem) Less(i, j int) bool {
+	iWord := acs.result[i]
+	iWordWeight := acs.freqMap[iWord]
+	jWord := acs.result[j]
+	jWordWeight := acs.freqMap[jWord]
+
+	if iWordWeight == jWordWeight {
+		return strings.Compare(jWord, iWord) == -1
+	}
+	return iWordWeight < jWordWeight
+
+}
