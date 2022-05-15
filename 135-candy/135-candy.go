36fbe7f01d48e5c84dba7d5ae9d8ade6ba6cb81c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-15 18:24:13 -0400

    Time: 19 ms (80.00%), Space: 6.5 MB (44.29%) - LeetHub

--- 135-candy/135-candy.go
+++ 135-candy/135-candy.go
@@ -1,10 +1,7 @@
 func candy(ratings []int) int {
-    
     candies := make([]int, len(ratings))
     candies[0] = 1
     
-    // left pass - if element is greater than its
-    // immediate left element, then leftElementVal + 1 is the value for this position
     for i := 1; i < len(ratings); i++ {
         if ratings[i] > ratings[i-1] {
             candies[i] = candies[i-1]+1
@@ -13,11 +10,6 @@ func candy(ratings []int) int {
         }
     }
     
-    // right pass 
-    // if ith rating is greater than right element
-    // and the ith value for this rating is NOT greater than
-    // i+1 val, then rightVal + 1
-    // ( while looping back, might as well start adding to total instead of looping 3rd time to see the same numbers again.. )
     total := candies[len(candies)-1]
     for i := len(ratings)-2; i >= 0; i-- {
         if ratings[i] > ratings[i+1] && candies[i] < candies[i+1]+1 {
@@ -25,6 +17,11 @@ func candy(ratings []int) int {
         }
         total += candies[i]
     }
-    return total
     
+    
+    
+    
+    
+    
+    return total
 }
