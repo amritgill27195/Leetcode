90d6aca95ae97f6f7a97e1b27ca3184e599ada8c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-15 14:48:02 -0400

    Time: 44 ms (34.31%), Space: 7.5 MB (41.61%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.go
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.go
@@ -1,8 +1,26 @@
+/*
+    approach: brute force
+    - flatten matrix into a new 1d array
+    - sort the array
+    - return k-1 idx element
+    time: o(mn) + o(mnlogmn)
+    space: o(mn)
+    
+    
+    approach: max heap
+    - loop over matrix
+    - add each item to heap
+    - if heap size > k, pop the top of the heap ( i.e keep heap size <= k )
+    - then finally return the 0th element from heap ( peek )
+    time: o(mn) x o(klogk)
+    space: o(k)
+*/
+
 func kthSmallest(matrix [][]int, k int) int {
     mx := &maxHeap{items: []int{}}
     m := len(matrix)
     n := len(matrix[0])
-    
+
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             heap.Push(mx, matrix[i][j])
@@ -19,7 +37,6 @@ func kthSmallest(matrix [][]int, k int) int {
 type maxHeap struct {
 	items []int
 }
-
 
 func (mx *maxHeap) Len() int {return len(mx.items)}
 func (mx *maxHeap) Swap(i, j int) {mx.items[i],mx.items[j]= mx.items[j],mx.items[i]}
