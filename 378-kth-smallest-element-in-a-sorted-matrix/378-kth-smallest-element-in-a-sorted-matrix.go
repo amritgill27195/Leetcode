409adf7044383c9336a2153ef6ec7f3b07501fd3
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-15 13:39:42 -0400

    Time: 41 ms (42.34%), Space: 7.9 MB (28.47%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.go
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.go
@@ -1,53 +1,15 @@
-
-/*
-    approach: brute force
-    - Flatten the entire matrix in a 1D array
-    - Sort it
-    - Return the k-1 idx element
-    Time: o(mn) + o(nlogn)
-    Space: o(mn)
-*/
-
-
-// brute force
-// func kthSmallest(matrix [][]int, k int) int {
-//     flat := []int{}
-//     m := len(matrix)
-//     n := len(matrix[0])
-//     for i := 0; i < m; i++ {
-//         for j := 0; j < n; j++ {
-//             flat = append(flat, matrix[i][j])
-//         }
-//     }
-//     sort.Ints(flat)
-//     return flat[k-1]
-// }
-
-
-
-
-/*
-    approach: max heap of size k
-    - For each element, store in maxHeap
-    - If maxHeap size > k, pop to keep size <= k
-    - Return the first element from maxHeap
-    time : o(mnlogk)
-    space: o(k)
-*/
-
-
-// max heap
 func kthSmallest(matrix [][]int, k int) int {
     mx := &maxHeap{items: []int{}}
     m := len(matrix)
     n := len(matrix[0])
+    
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             heap.Push(mx, matrix[i][j])
-            if mx.Len() > k {
+            if len(mx.items) > k {
                 heap.Pop(mx)
             }
-         }
+        }
     }
     return mx.items[0]
 }
@@ -57,12 +19,14 @@ func kthSmallest(matrix [][]int, k int) int {
 type maxHeap struct {
 	items []int
 }
-func (m *maxHeap) Swap(i, j int) { m.items[i],m.items[j] = m.items[j], m.items[i]}
-func (m *maxHeap) Less(i, j int) bool {return m.items[i] > m.items[j]}
-func (m *maxHeap) Len() int {return len(m.items)}
-func (m *maxHeap) Push(x interface{}) {m.items = append(m.items, x.(int))}
-func (m *maxHeap) Pop()interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
+
+
+func (mx *maxHeap) Len() int {return len(mx.items)}
+func (mx *maxHeap) Swap(i, j int) {mx.items[i],mx.items[j]= mx.items[j],mx.items[i]}
+func (mx *maxHeap) Less(i, j int) bool {return mx.items[i] > mx.items[j]}
+func (mx *maxHeap) Pop() interface{} {
+	out := mx.items[len(mx.items)-1]
+	mx.items = mx.items[:len(mx.items)-1]
 	return out
 }
+func (mx *maxHeap) Push(x interface{}) {mx.items = append(mx.items, x.(int))}
