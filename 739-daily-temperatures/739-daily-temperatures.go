75299589524cfb0002515f052b81199fac5fd3ee
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-14 20:42:06 -0400

    Time: 197 ms (44.24%), Space: 9.8 MB (36.65%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.go
+++ 739-daily-temperatures/739-daily-temperatures.go
@@ -1,36 +1,13 @@
-/*
-    approach: brute force
-    - nested for loops
-    - check if jth element is greater than ith element
-    - if it is, grab the distance (j-i) and save it in out[i]
-    time: o(n^2)
-    space: o(1)
-    
-    ( also this is essentially, find the next greater element on right but instead of value, save the distance between two points )
-    approach: monotonic increasing stack
-    - Loop over the list
-    - if the stack is not empty, and this item resolves previous its immediate previous neighbour ( top of the stack )
-        - resolve in this context means, its warmer than previous day
-        - Then pop the top of the stack 
-        - Calculate distance between top and current item ( currentIdx - idxPoppedFromTopOfStack )
-        - Save the distance in the output array at idxPoppedFromTopOfStack
-    - Otherwise save the currentIdx in the stack
-    
-    time: o(n)
-    space: o(n)
-
-*/
-
 func dailyTemperatures(temperatures []int) []int {
     out := make([]int, len(temperatures))
-    stack := []int{}
+    st := []int{}
     for i := 0; i < len(temperatures); i++ {
-        for len(stack) != 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
-            top := stack[len(stack)-1]
-            stack = stack[:len(stack)-1]
-            out[top] = i-top
+        for len(st) != 0 && temperatures[i] > temperatures[st[len(st)-1]] {
+            top := st[len(st)-1]
+            st = st[:len(st)-1]
+            out[top] = i - top
         }
-        stack = append(stack, i)
+        st = append(st, i)
     }
     return out
 }
