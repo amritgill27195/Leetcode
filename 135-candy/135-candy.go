81c6f00da827d157c49b08dacb8b097a0c549e8b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-15 19:05:50 -0400

    Time: 27 ms (48.57%), Space: 6.4 MB (51.43%) - LeetHub

--- 135-candy/135-candy.go
+++ 135-candy/135-candy.go
@@ -1,27 +1,22 @@
 func candy(ratings []int) int {
     candies := make([]int, len(ratings))
-    candies[0] = 1
+    for idx, _ := range candies {
+        candies[idx] = 1
+    }
     
     for i := 1; i < len(ratings); i++ {
         if ratings[i] > ratings[i-1] {
             candies[i] = candies[i-1]+1
-        } else {
-            candies[i] = 1
         }
     }
     
     total := candies[len(candies)-1]
-    for i := len(ratings)-2; i >= 0; i-- {
-        if ratings[i] > ratings[i+1] && candies[i] < candies[i+1]+1 {
+    for i := len(candies)-2; i>=0; i-- {
+        if ratings[i] > ratings[i+1] && candies[i] <= candies[i+1] {
             candies[i] = candies[i+1]+1
         }
         total += candies[i]
     }
-    
-    
-    
-    
-    
-    
     return total
+       
 }
