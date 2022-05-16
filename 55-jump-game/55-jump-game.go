d02620f72ad960bfc06271330e04c01a94215aec
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-15 22:03:31 -0400

    Time: 1221 ms (9.03%), Space: 9 MB (12.48%) - LeetHub

--- 55-jump-game/55-jump-game.go
+++ 55-jump-game/55-jump-game.go
@@ -1,20 +1,30 @@
-// DFS + Memo -- Result still TLE
+/**
+    Each value in the array list represents the jump distance we can jump ( 1 thru N where N is the ith val in array list)
+    
+    approach: DFS, explore all paths
+    - We can start from idx 0
+    - suppose the value at idx 0 is N
+    - Then we explore whether jumping 1..N is possible
+    - if any one returns true, we will return true and stop exploring
+    
+    time: exponential ( n^N - i think, there are n elements each have their own distince N value )
+    space: exponential - i think
+    Result: TLE
+    
+**/
 // func canJump(nums []int) bool {
-//     memo := map[int]bool{}
-//     var dfs func(idx int) bool
-//     dfs = func(idx int) bool {
+//     var dfs func(startIdx int) bool
+//     dfs = func(startIdx int) bool {
 //         // base
-//         if idx >= len(nums)-1 {return true}
+//         if startIdx >= len(nums)-1 {
+//             return true
+//         }
         
 //         // logic
-//         for i := nums[idx]; i >= 1; i-- {
-//             newIdx := idx+i
-//             if ok, val := memo[newIdx]; ok && val == true{
+//         for i := nums[startIdx]; i >= 1; i-- {
+//             nextIdx := i + startIdx
+//             if dfs(nextIdx) {
 //                 return true
-//             } else {
-//                 res := dfs(newIdx)
-//                 if res {return true}
-//                 memo[newIdx] = res
 //             }
 //         }
 //         return false
@@ -22,13 +32,49 @@
 //     return dfs(0)
 // }
 
-func canJump(nums []int) bool { 
-    destIdx := len(nums)-1
-    for i := len(nums)-2; i >= 0; i-- {
-        numJumpsAvail := nums[i]
-        if i+numJumpsAvail >= destIdx {
-            destIdx = i
+
+
+/**
+    Each value in the array list represents the jump distance we can jump ( 1 thru N where N is the ith val in array list)
+    
+    approach: DFS, explore all paths with Memoization
+    - We can try caching in a map[idx]bool
+        - why not an array ?
+        - well we cannot safetly assume array size because a jump value could be 1k but array size is only 5.. and then it we go out of bounds..
+        - therefore map
+        
+    - We can start from idx 0
+    - suppose the value at idx 0 is N, then jump (0+N..1)
+    - if this new idx position that we will jump to is already explored ( cached ) and if its true, then return true
+    - otherwise recursively call the dfs function again and cache its value - if the value is true, return and exit early, otherwise continue exploring...
+    
+    time: exponential ( n^N - i think, there are n elements each have their own distince N value )
+    space: exponential - i think
+    Result: Still TLE
+    
+**/
+func canJump(nums []int) bool {
+    var dfs func(startIdx int) bool
+    memo := map[int]bool{}
+
+    dfs = func(startIdx int) bool {
+        // base
+        if startIdx >= len(nums)-1 {
+            return true
         }
+        
+        // logic
+        for i := nums[startIdx]; i >= 1; i-- {
+            nextIdx := i + startIdx
+            if res, ok := memo[nextIdx]; ok {
+                if res {return true}
+            }else {
+                res := dfs(nextIdx)
+                memo[nextIdx] = res
+                if res { return true }
+            }
+        }
+        return false
     }
-    return destIdx == 0
+    return dfs(0)
 }
