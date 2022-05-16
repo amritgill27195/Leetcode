f48c238330e590dd851113cbca0af51db9fceaa0
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-16 15:14:43 -0400

    Time: 3 ms (90.69%), Space: 4.3 MB (31.37%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.go
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.go
@@ -6,29 +6,63 @@
  *     Right *TreeNode
  * }
  */
+// // brute force , slicing post and inorder into smaller calls and recursively finding root and returning root from the lowest level to left and right
+// func buildTree(inorder []int, postorder []int) *TreeNode {
+//     if len(inorder) != len(postorder) || len(inorder) == 0 && len(postorder) == 0{
+//         return nil
+//     }
+    
+//     root := &TreeNode{Val: postorder[len(postorder)-1]}
+//     mid := -1
+    
+//     for i := 0; i < len(inorder); i++ {
+//         if inorder[i] == root.Val {
+//             mid = i
+//             break
+//         }
+//     }
+    
+//     root.Left = buildTree(inorder[0:mid], postorder[0:mid])
+//     root.Right = buildTree(inorder[mid+1:], postorder[mid:len(postorder)-1])
+//     return root
+// }
+
+
+type btree struct {
+    inordermap map[int]int
+    postOrderIdx int
+}
+
 func buildTree(inorder []int, postorder []int) *TreeNode {
-    postOrderIdx := len(postorder)-1
     
-    inorderMap := map[int]int{}
-    for idx, ele := range inorder {
-        inorderMap[ele] = idx
+    if len(inorder) != len(postorder) {
+        return nil
     }
     
-    var dfs func(start, end int) *TreeNode
-    dfs = func(start, end int) *TreeNode{
-        // base
-        if start > end {
-            return nil
-        }
-        
-        // logic
-        root := &TreeNode{Val: postorder[postOrderIdx]}
-        postOrderIdx--
-        rootIdxInInorder := inorderMap[root.Val]
-        root.Right = dfs(rootIdxInInorder+1, end)
-        root.Left = dfs(start, rootIdxInInorder-1)
-        return root
+    b := &btree{
+        postOrderIdx: len(postorder)-1,
+        inordermap: map[int]int{},
     }
     
-    return dfs(0, len(postorder)-1)
+    for i := 0; i < len(inorder); i++ {
+        b.inordermap[inorder[i]] = i
+    }
+    
+    return b.helper(postorder, 0, len(inorder)-1)
+}
+
+
+func (b *btree) helper(post []int, start, end int) *TreeNode{
+    if start > end {
+        return nil
+    }
+    rootVal := post[b.postOrderIdx]
+    b.postOrderIdx--
+    rootIdx := b.inordermap[rootVal]
+    root := &TreeNode{Val: rootVal}
+    
+    root.Right = b.helper(post, rootIdx+1, end)
+    root.Left = b.helper(post, start, rootIdx-1)
+    
+    return root
 }
