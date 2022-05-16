c2bf3f04e2f58db282f8231d8cfb5d07ea7dfd7b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-15 23:28:58 -0400

    Time: 652 ms (7.38%), Space: 6.8 MB (7.58%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.go
+++ 45-jump-game-ii/45-jump-game-ii.go
@@ -1,30 +1,21 @@
-// BFS
-// time: exponential
-// space: o(n) for the queue
 func jump(nums []int) int {
-    visitedSet := map[int]bool{}
-    
     q := []int{0}
-    visitedSet[0] = true
+    visited := map[int]struct{}{}
     jumps := 0
     for len(q) != 0 {
         qSize := len(q)
-        for qSize != 0 {
+        for qSize != 0 {            
             dq := q[0]
             q = q[1:]
             if dq >= len(nums)-1 {
                 return jumps
             }
             // explore all the childs
-            // here childs are the next indicies
-            // each child is discovered by jumping from current idx + valAtThatIdx
-            for i := 1; i <= nums[dq]; i++ {
-                newIdx := dq+i
-                if newIdx >= len(nums)-1 {return jumps+1}
-                if _, ok := visitedSet[newIdx]; !ok {
-                    visitedSet[newIdx] = true
-                    q = append(q, newIdx)
-                }
+            for i := nums[dq]; i >= 1; i-- {
+                nextIdx := i+dq
+                if _, ok := visited[nextIdx]; ok {continue}
+                visited[nextIdx] = struct{}{}
+                q = append(q, nextIdx)
             }
             qSize--
         }
