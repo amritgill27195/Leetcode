c6047032a48c597e6eac36a28a6984b4f9afec3e
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-18 12:10:58 -0400

    Time: 69 ms (71.04%), Space: 7.2 MB (64.11%) - LeetHub

--- 55-jump-game/55-jump-game.go
+++ 55-jump-game/55-jump-game.go
@@ -1,22 +1,9 @@
 func canJump(nums []int) bool {
-    memo := map[int]bool{}
-    var dfs func(idx int) bool
-    dfs = func(idx int) bool {
-        // base
-        if idx >= len(nums)-1 {return true}
-        
-        // logic
-        for i := nums[idx]; i >= 1; i-- {
-            nextIdx := idx + i
-            if val, ok := memo[nextIdx]; ok {
-                if val == true{return true}
-            } else {
-                res := dfs(nextIdx)
-                memo[nextIdx] = res
-                if res {return true}
-            }
+    destIdx := len(nums)-1
+    for i := len(nums)-2; i >= 0 ; i-- {
+        if i+nums[i] >= destIdx {
+            destIdx = i
         }
-        return false
     }
-    return dfs(0)
+    return destIdx==0
 }
