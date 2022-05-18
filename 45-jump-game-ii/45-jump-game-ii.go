cdba5036ffea6762bb42b9b8b0b1d3a782e9231b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-18 14:29:49 -0400

    Time: 15 ms (77.41%), Space: 5.9 MB (81.34%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.go
+++ 45-jump-game-ii/45-jump-game-ii.go
@@ -1,29 +1,45 @@
-func jump(nums []int) int {
-    visited := map[int]struct{}{
-        0: struct{}{},
-    }
-    q := []int{0}
-    jumps := 0
+// func jump(nums []int) int {
+//     visited := map[int]struct{}{
+//         0: struct{}{},
+//     }
+//     q := []int{0}
+//     jumps := 0
     
-    for len(q) != 0 {
-        qSize := len(q)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            if dq >= len(nums)-1 {return jumps}
+//     for len(q) != 0 {
+//         qSize := len(q)
+//         for qSize != 0 {
+//             dq := q[0]
+//             q = q[1:]
+//             if dq >= len(nums)-1 {return jumps}
 
-            for i := nums[dq]; i >= 1; i-- {
-                nextIdx := dq + i
-                if nextIdx >= len(nums)-1 {return jumps+1}
-                _, ok := visited[nextIdx]
-                if !ok {
-                    visited[nextIdx] = struct{}{}
-                    q = append(q, nextIdx)
-                }
-            }
-            qSize--
+//             for i := nums[dq]; i >= 1; i-- {
+//                 nextIdx := dq + i
+//                 if nextIdx >= len(nums)-1 {return jumps+1}
+//                 _, ok := visited[nextIdx]
+//                 if !ok {
+//                     visited[nextIdx] = struct{}{}
+//                     q = append(q, nextIdx)
+//                 }
+//             }
+//             qSize--
+//         }
+//         jumps++
+//     }
+//     return jumps
+// } 
+
+func jump(nums []int) int {
+    currInt := nums[0]
+    nextInt := nums[0]
+    jumps := 1
+    if len(nums) < 2 {return 0}
+
+    for i := 1; i < len(nums); i++ {
+        nextInt = int(math.Max(float64(nextInt), float64(i+nums[i])))
+        if i == currInt && i != len(nums)-1 {
+            jumps++
+            currInt = nextInt
         }
-        jumps++
     }
     return jumps
-} 
+}
