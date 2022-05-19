c7429757fdadf1a885dd215e80ae7592d38e64d5
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-19 18:54:12 -0400

    Time: 20 ms (65.56%), Space: 6.5 MB (67.78%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.go
+++ 621-task-scheduler/621-task-scheduler.go
@@ -3,24 +3,19 @@ func leastInterval(tasks []byte, n int) int {
     maxCount := 0 // total number times maxFreq was seen
     maxFreq := 0
     m := map[byte]int{}
-    
     for i := 0; i < len(tasks); i++ {
         m[tasks[i]]++
         if m[tasks[i]] > maxFreq {
             maxFreq = m[tasks[i]]
         }
     }
-    
     for _, val := range m {
         if val == maxFreq {maxCount++}
     }
-    
     totalTasks := len(tasks)
     partitions := maxFreq-1
     avail := partitions * (n - (maxCount-1))
     pending := totalTasks - (maxCount*maxFreq)
     idle := int(math.Max(0.0, float64(avail)-float64(pending)))
-    
     return totalTasks+idle
-    
 }
