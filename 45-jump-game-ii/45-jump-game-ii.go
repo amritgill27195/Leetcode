e2dbec0c6b3089933a98d40dc02cee7565d6701d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-18 21:36:13 -0400

    Time: 687 ms (6.93%), Space: 6.7 MB (10.02%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.go
+++ 45-jump-game-ii/45-jump-game-ii.go
@@ -24,35 +24,35 @@
                 - or mark the number negative and have the adding of indicies to queue, filter our negative values at a idx we are evaluating
 
 */
-// func jump(nums []int) int {
-//     visited := map[int]struct{}{
-//         0: struct{}{},
-//     }
-//     q := []int{0}
-//     jumps := 0
+func jump(nums []int) int {
+    visited := map[int]struct{}{
+        0: struct{}{},
+    }
+    q := []int{0}
+    jumps := 0
     
-//     for len(q) != 0 {
-//         qSize := len(q)
-//         for qSize != 0 {
-//             dq := q[0]
-//             q = q[1:]
-//             if dq >= len(nums)-1 {return jumps}
+    for len(q) != 0 {
+        qSize := len(q)
+        for qSize != 0 {
+            dq := q[0]
+            q = q[1:]
+            if dq >= len(nums)-1 {return jumps}
 
-//             for i := nums[dq]; i >= 1; i-- {
-//                 nextIdx := dq + i
-//                 if nextIdx >= len(nums)-1 {return jumps+1}
-//                 _, ok := visited[nextIdx]
-//                 if !ok {
-//                     visited[nextIdx] = struct{}{}
-//                     q = append(q, nextIdx)
-//                 }
-//             }
-//             qSize--
-//         }
-//         jumps++
-//     }
-//     return jumps
-// } 
+            for i := nums[dq]; i >= 1; i-- {
+                nextIdx := dq + i
+                if nextIdx >= len(nums)-1 {return jumps+1}
+                _, ok := visited[nextIdx]
+                if !ok {
+                    visited[nextIdx] = struct{}{}
+                    q = append(q, nextIdx)
+                }
+            }
+            qSize--
+        }
+        jumps++
+    }
+    return jumps
+} 
 
 /*
     approach: greedy
@@ -71,18 +71,18 @@
     space: 0(1)
 
 */
-func jump(nums []int) int {
-    currInt := nums[0]
-    nextInt := nums[0]
-    jumps := 1
-    if len(nums) < 2 {return 0}
+// func jump(nums []int) int {
+//     currInt := nums[0]
+//     nextInt := nums[0]
+//     jumps := 1
+//     if len(nums) < 2 {return 0}
 
-    for i := 1; i < len(nums); i++ {
-        nextInt = int(math.Max(float64(nextInt), float64(i+nums[i])))
-        if i == currInt && i != len(nums)-1 {
-            jumps++
-            currInt = nextInt
-        }
-    }
-    return jumps
-}
+//     for i := 1; i < len(nums); i++ {
+//         nextInt = int(math.Max(float64(nextInt), float64(i+nums[i])))
+//         if i == currInt && i != len(nums)-1 {
+//             jumps++
+//             currInt = nextInt
+//         }
+//     }
+//     return jumps
+// }
