963f81ad31b9dceb27ab0c092143f3b8e5c5ce81
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-18 21:13:41 -0400

    Time: 44 ms (75.89%), Space: 7 MB (93.75%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.go
+++ 1306-jump-game-iii/1306-jump-game-iii.go
@@ -1,3 +1,14 @@
+/*
+    approach: BFS
+    - we have a start point and we have multiple destinations ( there could be n 0's)
+    - enqueue start position and look for childs
+        - Each item in this question only has 2 childs (you can jump to i + arr[i] or i - arr[i])
+        - so ith val + i and ith val - 1
+        - enqueue them both ( mark their inidices visited so we do not re-add them, also do boundary checks )
+        - while processing an item we will check if this idx val == 0, return true
+    - If our bfs exits without returning true, we will simply return false as in there was no way to get to an index with value 0
+        
+*/
 func canReach(arr []int, start int) bool {
     visited := map[int]struct{}{start: struct{}{}}
     q := []int{start}
@@ -34,9 +45,4 @@ func canReach(arr []int, start int) bool {
     
     return false
     
-}
-
-func abs(n int) int {
-    if n < 0 {return n*-1}
-    return n
 }
