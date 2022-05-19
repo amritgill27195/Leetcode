1588b8adb71e6dccb6c55a8368ca2d4c640fccf8
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-19 12:08:20 -0400

    Time: 18 ms (57.63%), Space: 6.6 MB (51.41%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.go
@@ -0,0 +1,24 @@
+/*
+    This is a classic stair case search
+    - Outline the staircase that holds all negatives
+    
+*/
+func countNegatives(grid [][]int) int {   
+    
+    m := len(grid)
+    n := len(grid[0])
+    r := m-1
+    c := 0
+    totalNegatives := 0
+    
+    for r >= 0 && c < n {
+        if grid[r][c] < 0 {
+            totalNegatives += n-c
+            r--
+        } else {
+            c++
+        }
+    }
+    return totalNegatives
+    
+}
