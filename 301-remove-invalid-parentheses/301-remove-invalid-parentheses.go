46999edd84c780ebec0c2a98fcbaa6a7f3ad5058
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-20 15:16:26 -0400

    Time: 69 ms (40.71%), Space: 7.8 MB (15.93%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.go
@@ -0,0 +1,46 @@
+func removeInvalidParentheses(s string) []string {
+    
+    visited := map[string]bool{s: true}
+    q := []string{s}
+    result := []string{}
+    
+    for len(q) != 0 {
+        qSize := len(q)
+        for qSize != 0 {
+            dq := q[0]
+            q = q[1:]
+            if yes := isValid(dq); yes{
+                result = append(result, dq)
+            }
+            
+            // enqueue all the childs
+            for i := 0; i < len(dq); i++ {
+                if dq[i] == '(' || dq[i] == ')' {
+                    child := string(dq[0:i]) + string(dq[i+1:])
+                    if _, ok := visited[child]; !ok {
+                        visited[child] = true
+                        q = append(q, child)
+                    }
+                }
+            }
+            qSize--
+        }
+        if len(result) != 0 {
+            break
+        }
+    }
+    return result
+}
+
+func isValid(s string) bool {
+    count := 0
+    for _, ele := range s {
+        if ele == '(' {
+            count++
+        } else if ele == ')' {
+            count--
+        }
+        if count < 0 {return false}
+    }
+    return count==0
+}
