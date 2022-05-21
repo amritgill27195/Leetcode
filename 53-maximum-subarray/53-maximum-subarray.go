6bd517a521777d29fd743e3efcfaece88216acdc
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-20 22:45:09 -0400

    Time: 109 ms (65.32%), Space: 9.6 MB (49.58%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.go
+++ 53-maximum-subarray/53-maximum-subarray.go
@@ -1,10 +1,10 @@
 func maxSubArray(nums []int) int {
+    max := nums[0]
     rs := nums[0]
-    max := rs
     
     for i := 1; i < len(nums); i++ {
-        rs = int(math.Max(float64(rs+nums[i]), float64(nums[i])))
-        max = int(math.Max(float64(rs), float64(max)))
+        rs = int(math.Max(float64(nums[i]), float64(rs+nums[i])))
+        max = int(math.Max(float64(max),float64(rs))) 
     }
     return max
 }
