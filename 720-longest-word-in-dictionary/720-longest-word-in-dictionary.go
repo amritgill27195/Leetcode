6e4ccac0fed6a149f2cb82c0f044e8069e4bc4a7
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-22 18:19:48 -0400

    Time: 14 ms (72.73%), Space: 7.6 MB (15.91%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.go
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.go
@@ -1,52 +1,70 @@
-type trieNode struct {
-    childrens [26]*trieNode
+/*
+    approach: trie + DFS
+    - Store all the words in a trie
+    - Then loop thru all childrens of trie
+        - 26 childrens because trie nodes contains characters ( for now )
+        - and there are 26 characters ( lower case )
+    - if a children is initialized and isEnd == true
+    - add this children to our path ( the output string we are building to return )
+    - Then recursively call the dfs on this child as the new root to explore other childs this child has initialized
+    - In our base case, if len(path) > maxLen - then update the len and save the current word as a potential result
+    - Finally once all the trie nodes are traversed, return the saved word
+    
+    time:
+    - let n be the number of words
+    - let k be the len of each word in words
+    - o(nk) - to insert each word in a trie
+    - 
+*/
+
+type trieNode struct{
     isEnd bool
+    childrens [26]*trieNode
 }
 
 func insert(root *trieNode, word string) {
-    if root == nil {return}
-    curr := root
-    for i := 0; i < len(word); i++ {
-        charIdx := word[i]-'a'
-        if curr.childrens[charIdx] == nil {
-            curr.childrens[charIdx] = &trieNode{childrens: [26]*trieNode{}}
+    current := root
+    for _, char := range word {
+        if current.childrens[char-'a'] == nil {
+            current.childrens[char-'a'] = &trieNode{childrens: [26]*trieNode{}}
         }
-        curr = curr.childrens[charIdx]
+        current = current.childrens[char-'a']
     }
-    curr.isEnd = true
+    current.isEnd = true
 }
 
+func search(root *trieNode, word string) {
+    
+}
 
 func longestWord(words []string) string {
-    
     root := &trieNode{childrens: [26]*trieNode{}}
     for _, word := range words {
         insert(root, word)
     }
-    
-    result := ""
-    var backtrack func(path string, r *trieNode)
-    backtrack = func(path string, r *trieNode) {
+    var result string
+    var backtrack func(r *trieNode, path string)
+    backtrack = func(r *trieNode, path string) {
         // base
         if len(path) > len(result) {
             result = path
         }
-        if r == nil {return}
+        if r == nil {
+            return
+        }
         
         // logic
-        curr := r
-        for i := 0; i < 26; i++ {
-            if curr.childrens[i] != nil && curr.childrens[i].isEnd {
+        for i := 0 ; i < 26; i++ {
+            if r.childrens[i] != nil && r.childrens[i].isEnd{
                 // action
-                path += string(i+'a')
+                path += string('a'+i)
                 // recurse
-                backtrack(path,curr.childrens[i])
+                backtrack(r.childrens[i], path)
                 // backtrack
-                path = string(path[:len(path)-1])
+                path = path[:len(path)-1]
             }
         }
     }
-    backtrack("", root)
+    backtrack(root, "")
     return result
-    
 }
