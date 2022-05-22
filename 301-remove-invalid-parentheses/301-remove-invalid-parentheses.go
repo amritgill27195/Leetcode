3ea27daa0b602ee4efb1572cd00f8ac881d13342
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-22 12:35:08 -0400

    Time: 77 ms (34.48%), Space: 7.2 MB (30.17%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.go
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.go
@@ -1,33 +1,68 @@
-func removeInvalidParentheses(s string) []string {
+// func removeInvalidParentheses(s string) []string {
     
+//     result := []string{}
+//     visited := map[string]struct{}{}
+//     q := []string{s}
+//     visited[s] = struct{}{}
+    
+//     for len(q) != 0 {
+//         qSize := len(q)
+//         for qSize != 0 {
+//             dq := q[0]
+//             q = q[1:]
+//             if isValid(dq) {
+//                 result = append(result, dq)
+//             }
+//             for i := 0; i < len(dq); i++ {
+//                 if dq[i] == '(' || dq[i] == ')' {
+//                     child := string(dq[0:i]) + string(dq[i+1:])
+//                     if _, ok := visited[child]; !ok {
+//                         q = append(q, child)
+//                         visited[child] = struct{}{}
+//                     }
+//                 }
+//             }
+//             qSize--
+//         }
+//         if len(result) > 0 {
+//             break
+//         }
+//     }
+//     return result
+// }
+
+
+
+func removeInvalidParentheses(str string) []string {
     result := []string{}
+    max := 0
     visited := map[string]struct{}{}
-    q := []string{s}
-    visited[s] = struct{}{}
-    
-    for len(q) != 0 {
-        qSize := len(q)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            if isValid(dq) {
-                result = append(result, dq)
+    var dfs func(s string)
+    dfs = func(s string) {
+        // base
+        if len(s) < max {return}
+        if _, ok := visited[s]; ok {return}
+        if isValid(s) {
+            if len(s) > max {
+                result = []string{s}
+                max = len(s)
+            } else if len(s) == max {
+                result = append(result, s)
             }
-            for i := 0; i < len(dq); i++ {
-                if dq[i] == '(' || dq[i] == ')' {
-                    child := string(dq[0:i]) + string(dq[i+1:])
-                    if _, ok := visited[child]; !ok {
-                        q = append(q, child)
-                        visited[child] = struct{}{}
-                    }
-                }
-            }
-            qSize--
+            visited[s] = struct{}{}
+            return
         }
-        if len(result) > 0 {
-            break
+        
+        // logic
+        visited[s] = struct{}{}
+        for i := 0; i < len(s); i++ {
+            if s[i] == '(' || s[i] == ')' {
+                child := string(s[0:i]) + string(s[i+1:])
+                dfs(child)
+            }
         }
     }
+    dfs(str)
     return result
 }
 
