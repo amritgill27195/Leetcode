601e2c4d4a6f0cc55ab91e0ce1415f93a58ccbe4
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-22 18:01:06 -0400

    Time: 93 ms (34.90%), Space: 17.2 MB (55.36%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.go
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.go
@@ -1,73 +1,54 @@
-type TrieNode struct {
-    // to denote that this word ends here ( full valid word )
+type trieNode struct {
+    childrens [26]*trieNode
     isEnd bool
-    // optional if we want to search for words that begin with this trie node as prefix
-    words []string
-    
-    // why 26 ? because
-    // 1. trie nodes are used to store characters of a string ( 1 char == 1 trie node )
-    // 2. And we have 26 total characters in english ( 26 lower case )
-    // if we are supposed to handle lower and upper case, than the size would 26(lower-case)+26(upper-case)
-    // if we are supposed to handle lower, upper and all other ascii values, then 256 would be children array length
-    childrens [26]*TrieNode
 }
 
 type Trie struct {
-    root *TrieNode
+    root *trieNode
 }
 
 
 func Constructor() Trie {
     return Trie{
-        root: &TrieNode{childrens: [26]*TrieNode{}},
+        root: &trieNode{childrens: [26]*trieNode{}},
     }
 }
 
-// time: o(k) where k is the length of word
-// space: o(k)
+
 func (this *Trie) Insert(word string)  {
     curr := this.root
-    for _, char := range word {
-        if curr.childrens[char-'a'] == nil {
-            curr.childrens[char-'a'] = &TrieNode{isEnd: false, childrens: [26]*TrieNode{}}
+    for i := 0; i < len(word); i++ {
+        charIdx := word[i]-'a'
+        if curr.childrens[charIdx] == nil {
+            curr.childrens[charIdx] = &trieNode{childrens: [26]*trieNode{}}
         }
-        // optionally if we are told to find all words with $somePrefix
-        curr.words = append(curr.words, word)
-        curr = curr.childrens[char-'a']
+        curr = curr.childrens[charIdx]
     }
-    // once we have inserted all characters of word, our curr ptr will be pointing to the last TrieNode of this word
-    // we need to denote that this is the end of this word by setting isEnd=true at the last TrieNode of $word ( which is where curr will be pointing to)
     curr.isEnd = true
 }
 
-// time: o(k) where k is the length of word
-// space: o(1)
+
 func (this *Trie) Search(word string) bool {
-    // we are looking for FULL VALID WORD ( not prefix )
     curr := this.root
-    for _, char := range word {
-        // get the idx of this char and if that idx is nil - this means the char does not exist which also means the word does not exist
-        if curr.childrens[char-'a'] == nil {return false}
-        // or else move current ptr to that node, and continue searching for the next character in $word
-        curr = curr.childrens[char-'a']
+    for i := 0; i < len(word); i++ {
+        charIdx := word[i] - 'a'
+        if curr.childrens[charIdx] == nil {
+            return false
+        }
+        curr = curr.childrens[charIdx]
     }
-    
-    // once we get here, it means we were will able to find ALL characters of $word
-    // but that does not mean that this is a valid word ( it could be a prefix to some other longer word )
-    // if this is a valid word that exists in our trie, then curr ( which points to the last trieNode char of $word ), its .isEnd property should be true 
     return curr.isEnd
 }
 
-// time: o(k) where k is the length of word
-// space: o(1)
+
 func (this *Trie) StartsWith(prefix string) bool {
-    // here we are searching for whether we have a prefix 
-    // existance of a prefix and not all words that began with this $prefix
-    // so move curr ptr thru all chars of $prefix - if successful, it means we do have a prefix
     curr := this.root
-    for _, char := range prefix {
-        if curr.childrens[char-'a'] == nil {return false}
-        curr = curr.childrens[char-'a']
+    for i := 0; i < len(prefix); i++ {
+        charIdx :=prefix[i]-'a'
+        if curr.childrens[charIdx] == nil {
+            return false
+        }
+        curr = curr.childrens[charIdx]
     }
     return true
 }
