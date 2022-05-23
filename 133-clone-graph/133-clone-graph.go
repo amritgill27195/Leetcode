eb7d3e13879a01cd642ddb26bbb63108744b2e14
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-23 12:05:59 -0400

    Time: 4 ms (42.02%), Space: 2.8 MB (76.06%) - LeetHub

--- 133-clone-graph/133-clone-graph.go
+++ 133-clone-graph/133-clone-graph.go
@@ -5,52 +5,26 @@
  *     Neighbors []*Node
  * }
  */
-/*
-    approach: BFS
-    - Similar type of approach as Copy LL with random pointers ( approach that used extra space )
-    time: o(v+e) where v is the number of verticies and e is number of edges.
-    Space: o(v) - we only ever store verticies in our {$srcNode:$clonedCopy} map
-*/
-// func cloneGraph(node *Node) *Node {
-//     if node == nil {return nil}
-//     clone := &Node{Val: node.Val}
-//     q := []*Node{node}
-//     visited := map[*Node]*Node{node: clone}    
-//     for len(q) != 0 {
-//         dq := q[0]
-//         q = q[1:]
-//         cloneOfDq := visited[dq]
-//         for _, n := range dq.Neighbors {
-//             if visited[n] == nil {
-//                 clone := &Node{Val: n.Val}
-//                 visited[n] = clone
-//                 q = append(q, n)
-//             }
-//             cloneOfDq.Neighbors = append(cloneOfDq.Neighbors, visited[n])
-//         }
-//     }    
-//     return clone
-// }
-
-
-
+// BFS
 func cloneGraph(node *Node) *Node {
+    if node == nil {return nil}
     visited := map[*Node]*Node{}
-    var dfs func(n *Node) *Node
-    dfs = func(n *Node) *Node {
-        // base
-        if n == nil {return nil}
-        if _, ok := visited[n]; ok {return nil}
+    clone := &Node{Val: node.Val}
+    visited[node] = clone
+    q := []*Node{node}
+    
+    for len(q) != 0 {
+        dq := q[0]
+        q = q[1:]
         
-        // logic
-        copyOfN := &Node{Val: n.Val}
-        visited[n] = copyOfN
-        for _, ne := range n.Neighbors {
-            dfs(ne)
-            copyOfN.Neighbors = append(copyOfN.Neighbors, visited[ne])
+        for _, nh := range dq.Neighbors {
+            _, ok := visited[nh]
+            if !ok {
+                visited[nh] = &Node{Val: nh.Val}
+                q = append(q, nh)
+            }
+            visited[dq].Neighbors = append(visited[dq].Neighbors, visited[nh])
         }
-        return copyOfN
     }
-    clone := dfs(node)
     return clone
 }
