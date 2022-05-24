4c90954f10c40278f1711350e1869073a1a67783
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-23 22:16:31 -0400

    Time: 2 ms (87.69%), Space: 3 MB (20.00%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.go
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.go
@@ -1,20 +1,10 @@
-/*
-    approach: slow and fast pointers
-    - slow and fast pointer both starting at idx 1
-    - count goes up by 1 if fast == fast-1
-    - count resets to 1 if fast != fast-1
-    - if count <= k, this tells slow pointer that it needs to move and that slow pointer has not seen k dupes to be allowed yet
-        - replace value at slow with fast and slow++
-    
-    time: o(n)
-    space: o(1)
-*/
 func removeDuplicates(nums []int) int {
+    count := 1
     slow := 1
     fast := 1
-    count := 1
-    k := 2
     n := len(nums)
+    k := 2
+    
     for fast < n {
         if nums[fast] == nums[fast-1] {
             count++
