5dfdace8a4c42f25b1c58f8784ed1d824cbc7fca
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-24 14:47:01 -0400

    Time: 50 ms (63.61%), Space: 8.3 MB (25.00%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.go
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.go
@@ -1,33 +1,15 @@
-/*
-    approach: brute force
-    - using nested for loop, form every single subarray and check its sum
-    - if sum == target, count++
-    
-    time: o(n^2)
-    space: o(1)
-    
-    
-    approach: running sum + complement search
-    - to do complement search, we need some lookup data structure
-        - map would be a good fit for this
-    - We will have a running sum as we are looping over the nums array
-    - at each runningSum, we will search for a number that will be needed to add to this runningSum to make it == target
-    - in other words, complement search
-    - what number do I need to add to this runningSum to make it == target
-    - target-runningSum - will give you the complement number 
-    - search for this complement in our map, if found, take its value and add to count
-    - if not found, then save this runningSum and increment its count by 1 in the map
-*/
-
 func subarraySum(nums []int, k int) int {
-    rsCount := map[int]int{0:1}
+    m := map[int]int{0:1}
+    rs := 0
     count := 0
-    sum := 0
     for i := 0; i < len(nums); i++ {
-        sum += nums[i]
-        complement := sum - k
-        count += rsCount[complement]
-        rsCount[sum]++
+        rs += nums[i]
+        complement := rs-k
+        numTimes, ok := m[complement]
+        if ok {
+            count += numTimes
+        }
+        m[rs]++
     }
     return count
 }
