d42b5af0b279c03c142497f0372211aad4c52a2d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-25 14:42:06 -0400

    Time: 18 ms (57.89%), Space: 5.4 MB (73.68%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.go
+++ 909-snakes-and-ladders/909-snakes-and-ladders.go
@@ -1,19 +1,15 @@
-/*
-    time: o(n^2)
-    space: o(n^2)
-*/
 func snakesAndLadders(board [][]int) int {
     n := len(board)
-    moves := make([]int, n*n)
+    flatBoard := make([]int, n*n)
+    idx := 0
     r := n-1
     c := 0
-    idx := 0
     even := 0
     for idx < n*n {
-        if board[r][c] != -1 {
-            moves[idx] = board[r][c]-1
+        if board[r][c] == -1 {
+            flatBoard[idx]=-1
         } else {
-            moves[idx] = board[r][c]
+            flatBoard[idx] = board[r][c]-1
         }
         idx++
         if even % 2 == 0 {
@@ -33,33 +29,32 @@ func snakesAndLadders(board [][]int) int {
         }
     }
     
-    level := 0
     q := []int{0}
-    moves[0] = -2 //mark this node visited
+    flatBoard[0] = -2
+    count := 0
     for len(q) != 0 {
         qSize := len(q)
         for qSize != 0 {
             dq := q[0]
             q = q[1:]
             if dq == n*n-1 {
-                return level
+                return count
             }
             for i := 1; i <= 6; i++ {
-                child := dq + i
-                if child < n*n { // out of bound is not an option
-                    if moves[child] != -2 { // if not visited ( i.e not already in the queue )
-                        if moves[child] == -1 { // if this cell does not have a ladder
-                            q = append(q, child) // then simply add that cell
-                        } else { // other wise if the cell has a ladder, take the ladder and add that resulting cell of ladder in queue
-                            q = append(q, moves[child])
-                        }
-                        moves[child] = -2 // finally mark this child visited
+                childIdx := dq+i
+                if childIdx < n*n {
+                    if flatBoard[childIdx] == -2 {continue}
+                    if flatBoard[childIdx] == -1 {
+                        q = append(q, childIdx)
+                    } else {
+                        q = append(q, flatBoard[childIdx])
                     }
+                    flatBoard[childIdx] = -2
                 }
             }
             qSize--
         }
-        level++
-    } 
+        count++
+    }
     return -1
 }
