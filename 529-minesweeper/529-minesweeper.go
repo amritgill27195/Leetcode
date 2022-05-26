2078d1fa22a0fa0ea20156fe595e9f304b43f2eb
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-25 21:10:50 -0400

    Time: 42 ms (17.86%), Space: 7.4 MB (25.00%) - LeetHub

--- 529-minesweeper/529-minesweeper.go
+++ 529-minesweeper/529-minesweeper.go
@@ -17,6 +17,76 @@
     - We will enqueue a child and then process that child and then this child will add its other 8 childs depending on the matrix if possible
     - so somewhere at somepoint the space of the queue is proportional to the input matrix size
 */
+// func updateBoard(board [][]byte, click []int) [][]byte {
+    
+//     sr := click[0]
+//     sc := click[1]
+//     if board[sr][sc] == 'M' {
+//         board[sr][sc] = 'X'
+//         return board
+//     }
+//     dirs := [][]int{
+//         {1,0},
+//         {-1,0},
+//         {0,1},
+//         {0,-1},
+//         {-1,-1},
+//         {-1,1},
+//         {1,-1},
+//         {1,1},
+//     }
+    
+//     q := [][]int{{sr,sc}}
+    
+    
+//     for len(q) != 0 {
+//         dq := q[0]
+//         q = q[1:]
+//         cr := dq[0]
+//         cc := dq[1]
+//         board[cr][cc] = 'B'
+//         numMines, childs := countNumMinesAround(cr, cc, board, dirs)
+//         if numMines > 0 {
+//             board[cr][cc] = byte(numMines + '0')
+//         } else {
+//             for _, child := range childs {
+//                 board[child[0]][child[1]] = 'B'
+//                 q = append(q, child)
+//             }
+//         }
+//     }
+//     return board
+    
+// }
+
+
+
+// func countNumMinesAround(r, c int, board [][]byte, dirs [][]int) (int, [][]int) {
+//     m := len(board)
+//     n := len(board[0])
+//     count := 0
+//     childs := [][]int{}
+    
+//     for _, dir := range dirs {
+//         nr := r+dir[0]
+//         nc := c+dir[1]
+//         if nr >= 0 && nr < m && nc >=0 && nc < n {
+            
+//             if board[nr][nc] == 'M' {
+//                 count++
+//             }
+            
+//             if board[nr][nc] == 'E' {
+//                 childs = append(childs, []int{nr,nc})
+//             }
+//         }
+//     }
+//     return count, childs
+// }
+
+
+
+// DFS
 func updateBoard(board [][]byte, click []int) [][]byte {
     
     sr := click[0]
@@ -35,51 +105,44 @@ func updateBoard(board [][]byte, click []int) [][]byte {
         {1,-1},
         {1,1},
     }
-    
-    q := [][]int{{sr,sc}}
-    
-    
-    for len(q) != 0 {
-        dq := q[0]
-        q = q[1:]
-        cr := dq[0]
-        cc := dq[1]
-        board[cr][cc] = 'B'
-        numMines, childs := countNumMinesAround(cr, cc, board, dirs)
+    m := len(board)
+    n := len(board[0])
+    var dfs func(r, c int)
+    dfs = func(r, c int) {
+        // base
+        if r < 0 || r == m || c < 0 || c == n || board[r][c] == 'B' {return}
+        
+        // logic
+        board[r][c] = 'B'
+        numMines := countNumMinesAround(r,c, board, dirs)
         if numMines > 0 {
-            board[cr][cc] = byte(numMines + '0')
+            board[r][c] = byte(numMines+'0')
         } else {
-            for _, child := range childs {
-                board[child[0]][child[1]] = 'B'
-                q = append(q, child)
+            for _, dir := range dirs {
+                dfs(r+dir[0], c+dir[1])
             }
         }
+        
     }
+    dfs(sr,sc)
+    
     return board
     
 }
 
 
 
-func countNumMinesAround(r, c int, board [][]byte, dirs [][]int) (int, [][]int) {
+func countNumMinesAround(r, c int, board [][]byte, dirs [][]int) int {
     m := len(board)
     n := len(board[0])
     count := 0
-    childs := [][]int{}
     
     for _, dir := range dirs {
         nr := r+dir[0]
         nc := c+dir[1]
-        if nr >= 0 && nr < m && nc >=0 && nc < n {
-            
-            if board[nr][nc] == 'M' {
-                count++
-            }
-            
-            if board[nr][nc] == 'E' {
-                childs = append(childs, []int{nr,nc})
-            }
+        if nr >= 0 && nr < m && nc >=0 && nc < n && board[nr][nc] == 'M'  {
+            count++ 
         }
     }
-    return count, childs
+    return count
 }
