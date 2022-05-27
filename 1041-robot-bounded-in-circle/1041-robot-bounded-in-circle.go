9edd1058ea114cbac3371f68f8afcc2e5e85f92f
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-26 20:35:12 -0400

    Time: 0 ms (100.00%), Space: 1.9 MB (47.92%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.go
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.go
@@ -1,43 +1,21 @@
 func isRobotBounded(instructions string) bool {
+    //                U     R      D     L
+    dirs := [][]int{{0,1},{1,0},{0,-1},{-1,0}}
     x := 0
     y := 0
-    dir := "u"
     idx := 0
-    
-    for idx < len(instructions) {
-        inst := string(instructions[idx])
-        if inst == "G" {
-            if dir == "u" {
-                y++
-            } else if dir == "d" {
-                y--
-            } else if dir == "l" {
-                x--
-            } else if dir == "r" {
-                x++
-            }
-        } else if inst == "L" {
-            if dir == "u" {
-                dir = "l"
-            } else if dir == "l" {
-                dir = "d"
-            } else if dir == "d" {
-                dir = "r"
-            } else if dir == "r" {
-                dir = "u"
-            }
-        } else if inst == "R" {
-            if dir == "u" {
-                dir = "r"
-            } else if dir == "l" {
-                dir = "u"
-            } else if dir == "d" {
-                dir = "l"
-            } else if dir == "r" {
-                dir = "d"
-            }
+    for i := 0 ; i < len(instructions); i++ {
+        inst := instructions[i]
+        if inst == 'R' {
+            idx = (idx + 1)% 4
+        } else if inst == 'L' {
+            idx = (idx + 3) % 4
+        } else if inst == 'G'{
+            x += dirs[idx][0]
+            y += dirs[idx][1]
         }
-        idx++
+        
     }
-    return dir != "u" || (x == 0 && y == 0)
+    
+    return idx != 0 || (x == 0 && y == 0)
 }
