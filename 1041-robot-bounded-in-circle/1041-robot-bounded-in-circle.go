fe4b6710997fdd034fd210fc3f35b9c06af93244
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-26 20:37:15 -0400

    Time: 0 ms (100.00%), Space: 1.9 MB (47.92%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.go
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.go
@@ -1,3 +1,5 @@
+// time: o(n)
+// space: o(1)
 func isRobotBounded(instructions string) bool {
     //                U     R      D     L
     dirs := [][]int{{0,1},{1,0},{0,-1},{-1,0}}
@@ -19,3 +21,54 @@ func isRobotBounded(instructions string) bool {
     
     return idx != 0 || (x == 0 && y == 0)
 }
+
+
+/*
+    initial naive code after understanding the explanation
+    time: o(n)
+    space: o(1)
+*/
+
+// func isRobotBounded(instructions string) bool {
+//     x := 0
+//     y := 0
+//     dir := "u"
+//     idx := 0
+    
+//     for idx < len(instructions) {
+//         inst := string(instructions[idx])
+//         if inst == "G" {
+//             if dir == "u" {
+//                 y++
+//             } else if dir == "d" {
+//                 y--
+//             } else if dir == "l" {
+//                 x--
+//             } else if dir == "r" {
+//                 x++
+//             }
+//         } else if inst == "L" {
+//             if dir == "u" {
+//                 dir = "l"
+//             } else if dir == "l" {
+//                 dir = "d"
+//             } else if dir == "d" {
+//                 dir = "r"
+//             } else if dir == "r" {
+//                 dir = "u"
+//             }
+//         } else if inst == "R" {
+//             if dir == "u" {
+//                 dir = "r"
+//             } else if dir == "l" {
+//                 dir = "u"
+//             } else if dir == "d" {
+//                 dir = "l"
+//             } else if dir == "r" {
+//                 dir = "d"
+//             }
+//         }
+//         idx++
+//     }
+//     return dir != "u" || (x == 0 && y == 0)
+// }
