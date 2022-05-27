e917d8a6344a60840fe0f85ae24cd6558d5a785f
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-26 21:18:26 -0400

    Time: 561 ms (25.46%), Space: 9.4 MB (47.83%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.go
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.go
@@ -1,3 +1,22 @@
+/*
+    approach: try to build out all sequences with numbers that we can start a sequence from
+    - First we need to make our elements in nums array searchable fast, therefore we will put them in a hashmap
+    - Then while looping over the nums array, we will;
+        - Check if the current number is a start of a sequence
+        - current number would be start of a sequence IF there is not a number that comes before this number
+            - check if our map contains currentNumber-1 
+            - if it does, this means that this currentNumber is not the start of a sequence..
+            - REMEMBER FOR EACH ELEMENT WE ARE CHECKING IF THIS NUMBER COULD BE THE START OF A SEQUENCE
+            - IF IT IS, WE KEEP CHECKING ITS NEXT NUMBER AND SAVING LENGTH EACH TIME
+            - if there is no prev number in our nums array of our currentNumber
+            - this means this currentNumber is definetely start of a sequence
+            - Now, from this number maintain a count (starting at 1 to include the start sequence number as part of count)
+            - Keep adding 1 to this currentNumber and as long as our nums map has the next number , keep incrementing the count
+            - Once we no longer have a next immediate number, our loop will break out and we will need to save our count ( max = math.max(max, count))
+    
+    Time: o(n)
+    Space: o(1)
+*/
 func longestConsecutive(nums []int) int {
     set := map[int]struct{}{}
     for i := 0; i < len(nums); i++ {
