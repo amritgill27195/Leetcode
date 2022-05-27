73a54e111b3a321f023814a8436e85b7a6e13851
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-27 16:35:39 -0400

    Time: 46 ms (23.23%), Space: 9.5 MB (5.27%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.go
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -1,19 +1,23 @@
 func productExceptSelf(nums []int) []int {
-    if nums == nil {
-        return nil
-    }
-    left := []int{1}
+    lProd := make([]int, len(nums))
+    lProd[0] = 1
     for i := 1; i < len(nums); i++ {
-        left = append(left, left[i-1] * nums[i-1])
+        prev := nums[i-1]
+        prevProd := lProd[i-1]
+        lProd[i] = prev * prevProd
     }
-    right := make([]int, len(nums))
-    right[len(right)-1] = 1
-    for i := len(right)-2; i >= 0; i-- {
-        right[i] = right[i+1] * nums[i+1]
+    
+    rProd := make([]int, len(nums))
+    rProd[len(nums)-1] = 1
+    for i := len(nums)-2; i >= 0; i-- {
+        prev := nums[i+1]
+        prevProd := rProd[i+1]
+        rProd[i] = prev*prevProd
     }
-    out := make([]int, len(nums))
-    for i := 0; i < len(nums); i++ {
-        out[i] = left[i] * right[i]
+    
+    out := []int{}
+    for i := 0; i < len(lProd); i++ {
+        out = append(out, lProd[i] * rProd[i])
     }
     return out
 }
