5ea91c983d5ab113463ca808228be3fb7b3c39f0
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-29 11:13:02 -0400

    Time: 2 ms (62.90%), Space: 2.9 MB (41.66%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.go
+++ 22-generate-parentheses/22-generate-parentheses.go
@@ -1,4 +1,28 @@
-
+/*
+    approach: dfs with backtracking
+    - Since we have to generate every single valid combination of parans, dfs + backtracking makes sense
+    - We are given n - which represents the number of parans pairs we are given
+    - for example: n = 3 - we have 3 open and 3 close parans to work with
+    - for example: n = 1 - we have 1 open and 1 close paran to work with
+    
+    - inorder to generate any valid parans... 
+        - we have to start with an open paran right?
+        - so we will keep track of number of opens we have used so far 
+        - while openUsed < n , append an open paran to our current path ( ACTION )
+        - we can potentially have more open parans, even tho we just used an open paran - so recurse ( RECURSE )
+        - when our openUsed is no longer less than n, then we can no longer add more open parans - this means we have used up all our open parans allowed..
+        - Now inorder to make path valid, we have to start closing our parans...
+        - So as long as closeUsed < openUsed , add, close paran ( ACTION )
+        - we may have more close parans to use so recurse ( RECURSE )
+        - When we have used all open and all closed, our base case will add the path to a result array and return
+        - Then once the call goes back to the close parans block, we will undo our action ( BACKTRACK )
+        - Which will then end as there is nothing to do after backtracking our closUsed action
+        - So recursion goes back to a previous parent call and at somepoint goes back to openUsed block where we have not used all open parans but goes to closeUsed call to generate other combinations with closed parans.
+    
+    Time: exponential - 2^n
+    Space: ???
+    
+*/
 func generateParenthesis(n int) []string {
     result := []string{}
     var backtrack func(openN, closeN int, path string)
@@ -11,13 +35,19 @@ func generateParenthesis(n int) []string {
         
         // logic
         if openN < n {
+            // action
             path += "("
+            // recurse
             backtrack(openN+1, closeN, path)
+            // backtrack
             path = path[:len(path)-1]
         }
         if closeN < openN {
+            // action
             path += ")"
+            // recurse
             backtrack(openN, closeN+1, path)
+            // backtrack
             path = path[:len(path)-1]
         }
     }
