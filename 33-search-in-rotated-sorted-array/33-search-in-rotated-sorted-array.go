479f00884f051f6b48b5ee68ed119e35107f88b2
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-01 19:39:44 -0400

    Time: 6 ms (22.40%), Space: 2.6 MB (62.90%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.go
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.go
@@ -1,19 +1,19 @@
 func search(nums []int, target int) int {
+    n := len(nums)
     left := 0
-    right := len(nums)-1
+    right := n-1
     
     for left <= right {
         mid := left + (right-left)/2
         if nums[mid] == target {
             return mid
         } else if nums[mid] >= nums[left] {
-            // left sorted
+            // left sorted 
             if target >= nums[left] && target < nums[mid] {
                 right = mid-1
             } else {
                 left = mid+1
             }
-            
         } else {
             // right sorted
             if target <= nums[right] && target > nums[mid] {
