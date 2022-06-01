ad2602c44b5270a7d1cb024abd0b7af47a81528b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-05-31 23:25:31 -0400

    Time: 35 ms (14.93%), Space: 5.3 MB (34.72%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.go
+++ 42-trapping-rain-water/42-trapping-rain-water.go
@@ -1,3 +1,22 @@
+/*
+    approach: 2 pass using 2 pointers ( slow and fast ptrs )
+    - We want to position our 2 pointers such that we know we can trap some water
+    - fast pointer will always move
+    - if height at fast pointer is smaller than height at left
+    - this means we can trap some water between left and right - so we will take whatever we can trap here
+        - trap += height[left]-height[fast]
+    - when height at fast is not less than height at slow, this means we are done trapping as much water as we can , so add our trapped value to final result
+    - move slow to fast and reset trap value
+    - always move fast pointer regardless of which path we take
+    - but this does not work in a single pass everytime... because we could have a slow pointer pointing the tallest bar in the middle of the array
+    - and all bars to right are always less than slow height ( i.e height[fast] is always less than height[slow] because slow is at the tallest point )
+    - Therefore another pass from the back is needed to check if we can trap water the other until this peek ( if there is a peek value slow paused at )
+    - so loop back until peek idx and repeat the same checks
+   
+   time: o(2n) - but 2 is constant
+   space: o(1)
+   
+*/ 
 func trap(height []int) int {
     n := len(height)
     result := 0
