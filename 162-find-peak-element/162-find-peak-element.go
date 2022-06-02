b60aaac635745c60940ae60b2cba154f9e7e241c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-01 21:35:22 -0400

    Time: 8 ms (17.96%), Space: 2.8 MB (63.59%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.go
+++ 162-find-peak-element/162-find-peak-element.go
@@ -1,10 +1,22 @@
+/*
+    peak element is an element greater than its left and right neighbors
+    
+    approach: Binary search
+    - if mid > mid-1 and mid+1
+    - then return mid
+    - otherwise binary search on the greater element side
+    
+    time: o(logn)
+    space: o(1)
+*/
+
 func findPeakElement(nums []int) int {
     left := 0
     right := len(nums)-1
     
-    for left <= right {
+    for left <= right{
         mid := left + (right-left)/2
-        if (mid==0 || nums[mid] > nums[mid-1]) && (mid == len(nums)-1 || nums[mid] > nums[mid+1]) {
+        if (mid == 0 || nums[mid] > nums[mid-1]) && (mid == len(nums)-1 || nums[mid] > nums[mid+1]) {
             return mid
         } else if nums[mid+1] > nums[mid] {
             left = mid+1
@@ -12,5 +24,6 @@ func findPeakElement(nums []int) int {
             right = mid-1
         }
     }
+    
     return -1
 }
