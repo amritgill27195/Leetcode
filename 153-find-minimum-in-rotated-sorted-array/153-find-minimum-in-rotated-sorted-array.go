91377e36d06f89313a7ffdd60fdf5a2b4cc22109
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-01 21:24:03 -0400

    Time: 4 ms (47.46%), Space: 2.4 MB (69.92%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.go
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.go
@@ -1,18 +1,27 @@
+// min is always on the unsorted half
+// unsorted half is half of array that is  unsorted from mid's perspective
+// min is an element that is smallest among its immediate neighbors ( left and right )
+// so to find mid, find sorted side from mid's perspective - take the search on unsorted half
+// at some point, the search on unsorted half will be the smallest sorted half, when this happens return left
+
 func findMin(nums []int) int {
-    n := len(nums)
     left := 0
-    right := n-1
-    
+    right := len(nums)-1
+    if len(nums) == 1 {
+        return nums[0]
+    }
     for left <= right {
+        mid := left + (right-left)/2
         if nums[left] <= nums[right] {
             return nums[left]
         }
-        mid := left + (right-left)/2
-        if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == n-1 || nums[mid] < nums[mid+1]) {
+        if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == len(nums)-1 || nums[mid] < nums[mid+1]) {
             return nums[mid]
-        } else if nums[mid] >= nums[left] {
+        } else if nums[left] <= nums[mid] {
+            // left sorted
             left = mid+1
         } else {
+            // right sorted
             right = mid-1
         }
     }
