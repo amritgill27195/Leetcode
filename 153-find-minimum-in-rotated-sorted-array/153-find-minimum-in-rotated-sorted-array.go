24c38add23ea59f004b0771f46b5f38c6a18f4e9
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-01 21:23:46 -0400

    Time: 7 ms (14.71%), Space: 2.5 MB (69.92%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.go
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.go
@@ -1,17 +1,16 @@
 func findMin(nums []int) int {
+    n := len(nums)
     left := 0
-    right := len(nums)-1
+    right := n-1
     
     for left <= right {
-        mid := left + (right-left)/2
-        
         if nums[left] <= nums[right] {
             return nums[left]
         }
-        
-        if (mid==0 || nums[mid] < nums[mid-1]) && (mid==len(nums)-1 || nums[mid] < nums[mid+1]) {
+        mid := left + (right-left)/2
+        if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == n-1 || nums[mid] < nums[mid+1]) {
             return nums[mid]
-        } else if nums[left] <= nums[mid] {
+        } else if nums[mid] >= nums[left] {
             left = mid+1
         } else {
             right = mid-1
