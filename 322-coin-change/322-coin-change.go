850beaa32ba3ca6e73086e58dc92f8c2e636bdb0
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-04 19:16:18 -0400

    Time: 41 ms (29.22%), Space: 7.2 MB (30.93%) - LeetHub

--- 322-coin-change/322-coin-change.go
+++ 322-coin-change/322-coin-change.go
@@ -26,33 +26,55 @@
 //     return min
 // }
 
+// func coinChange(coins []int, amount int) int {
+//     m := len(coins)
+//     n := amount
+    
+//     dp := make([][]int, m+1)
+//     for idx, _ := range dp {
+//         dp[idx] = make([]int, n+1)
+//     }
+    
+//     for j := 1; j < len(dp[0]); j++ {
+//         dp[0][j] = 10001 // max 
+//     }
+    
+//     for i := 1; i < len(dp); i++ { // coin value
+//         for j := 1; j < len(dp[0]); j++ { // amount
+//             coinVal := coins[i-1]
+//             amount := j
+//             if coinVal > amount {
+//                 dp[i][j] = dp[i-1][j]
+//             } else {
+//                 dp[i][j] = int(math.Min(float64(dp[i][j-coinVal]+1),float64(dp[i-1][j])))
+//             }
+//         }
+//     }
+//     result := dp[len(dp)-1][len(dp[0])-1]
+//     if result == 10001 {
+//         return -1
+//     }
+//     return result
+// }
+
 func coinChange(coins []int, amount int) int {
     m := len(coins)
     n := amount
     
-    dp := make([][]int, m+1)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n+1)
+    dp := make([]int, n+1)
+    for i := 1; i < len(dp); i++ {
+        dp[i] = amount+1 
     }
     
-    for j := 1; j < len(dp[0]); j++ {
-        dp[0][j] = 10001
-    }
-    
-    for i := 1; i < len(dp); i++ { // coin value
-        for j := 1; j < len(dp[0]); j++ { // amount
+    for i := 1; i < m+1; i++ {
+        for j := 1; j < len(dp); j++ {
             coinVal := coins[i-1]
             amount := j
-            if coinVal > amount {
-                dp[i][j] = dp[i-1][j]
-            } else {
-                dp[i][j] = int(math.Min(float64(dp[i][j-coinVal]+1),float64(dp[i-1][j])))
-            }
+            if coinVal > amount {continue}
+            dp[j] = int(math.Min(float64(dp[j]),float64(dp[j-coinVal]+1)))
         }
     }
-    result := dp[len(dp)-1][len(dp[0])-1]
-    if result == 10001 {
-        return -1
-    }
-    return result
+    
+    if dp[len(dp)-1] > amount {return -1}
+    return dp[len(dp)-1]
 }
