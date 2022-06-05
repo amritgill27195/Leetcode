fdffc11830572d941c95d4162d5d44e633a2317e
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-05 15:13:26 -0400

    Time: 0 ms (100.00%), Space: 2.3 MB (44.62%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.go
+++ 518-coin-change-2/518-coin-change-2.go
@@ -19,31 +19,51 @@
 //     return total
 // }
 
+// func change(amount int, coins []int) int {
+//     m := len(coins)
+//     n := amount
+    
+//     dp := make([][]int, m+1)
+//     for idx, _ := range dp {
+//         dp[idx] = make([]int, n+1)
+//     }
+    
+//     for i := 0; i < len(dp); i++ {
+//         dp[i][0] = 1
+//     }
+    
+//     for i := 1; i < len(dp); i++ {
+//         for j := 1; j < len(dp[0]); j++ {
+//             coin := coins[i-1]
+//             amount := j
+//             if coin > amount {
+//                 dp[i][j] = dp[i-1][j]
+//             } else {
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-coin]
+//             }
+//         }
+//     }
+    
+//     return dp[len(dp)-1][len(dp[0])-1] 
+// }
+
+
 func change(amount int, coins []int) int {
     m := len(coins)
     n := amount
     
-    dp := make([][]int, m+1)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n+1)
-    }
+    dp := make([]int, n+1)
+    dp[0] = 1
     
-    for i := 0; i < len(dp); i++ {
-        dp[i][0] = 1
-    }
     
-    for i := 1; i < len(dp); i++ {
-        for j := 1; j < len(dp[0]); j++ {
+    for i := 1; i < m+1; i++ {
+        for j := 1; j < n+1; j++ {
             coin := coins[i-1]
             amount := j
-            if coin > amount {
-                dp[i][j] = dp[i-1][j]
-            } else {
-                dp[i][j] = dp[i-1][j] + dp[i][j-coin]
-            }
+            if coin > amount { continue }
+            dp[j] = dp[j] + dp[j-coin]
         }
     }
     
-    return dp[len(dp)-1][len(dp[0])-1]
-    
+    return dp[len(dp)-1] 
 }
