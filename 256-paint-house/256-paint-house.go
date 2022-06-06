18013a35ccc15d422fcb4a9a90e608758120c58d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-05 21:35:16 -0400

    Time: 8 ms (36.84%), Space: 3 MB (50.00%) - LeetHub

--- 256-paint-house/256-paint-house.go
+++ 256-paint-house/256-paint-house.go
@@ -49,28 +49,55 @@
 //     return min
 // }
 
+// bottom up dp
+// solving from bottom to top solves repeated subproblems that we saw in our decision tree
+// time: o(m) - where m is the number of rows in costs array -- since number of cols is constant, i.e: 3
+// space: o(m) - where m is the number of cols in costs array -- since number of cols is constant, i.e: 3
+// func minCost(costs [][]int) int {        
+//     m := len(costs)
+//     n := len(costs[0])
+//     dp := make([][]int, m)
+//     for idx, _ := range dp {
+//         dp[idx] = make([]int, n)
+//     }
+//     for j := 0; j < n; j++ {
+//         dp[m-1][j] = costs[m-1][j]
+//     }
+//     for i := m-2; i >= 0; i-- {
+//         for j := 0; j < n; j++ {
+            
+//             // out of the 3 choices - get the min of other 2 values while excluding current j idx
+//             jVal := 0
+//             if j == 0 {jVal = int(math.Min(float64(dp[i+1][1]),float64(dp[i+1][2])))}
+//             if j == 1 {jVal = int(math.Min(float64(dp[i+1][0]),float64(dp[i+1][2])))}
+//             if j == 2 {jVal = int(math.Min(float64(dp[i+1][0]),float64(dp[i+1][1])))}
+//             dp[i][j] = jVal + costs[i][j]
+//         }
+//     }
+//     min := dp[0][0]
+//     if dp[0][1] < min {min = dp[0][1]}
+//     if dp[0][2] < min {min = dp[0][2]}
+//     return min
+// }
 
+// Bottom up DP - mutating existing costs array
 func minCost(costs [][]int) int {        
     m := len(costs)
     n := len(costs[0])
-    dp := make([][]int, m)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n)
-    }
-    for j := 0; j < n; j++ {
-        dp[m-1][j] = costs[m-1][j]
-    }
+
     for i := m-2; i >= 0; i-- {
         for j := 0; j < n; j++ {
+            
+            // out of the 3 choices - get the min of other 2 values while excluding current j idx
             jVal := 0
-            if j == 0 {jVal = int(math.Min(float64(dp[i+1][1]),float64(dp[i+1][2])))}
-            if j == 1 {jVal = int(math.Min(float64(dp[i+1][0]),float64(dp[i+1][2])))}
-            if j == 2 {jVal = int(math.Min(float64(dp[i+1][0]),float64(dp[i+1][1])))}
-            dp[i][j] = jVal + costs[i][j]
+            if j == 0 {jVal = int(math.Min(float64(costs[i+1][1]),float64(costs[i+1][2])))}
+            if j == 1 {jVal = int(math.Min(float64(costs[i+1][0]),float64(costs[i+1][2])))}
+            if j == 2 {jVal = int(math.Min(float64(costs[i+1][0]),float64(costs[i+1][1])))}
+            costs[i][j] = jVal + costs[i][j]
         }
     }
-    min := dp[0][0]
-    if dp[0][1] < min {min = dp[0][1]}
-    if dp[0][2] < min {min = dp[0][2]}
+    min := costs[0][0]
+    if costs[0][1] < min {min = costs[0][1]}
+    if costs[0][2] < min {min = costs[0][2]}
     return min
 }
