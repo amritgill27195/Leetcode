26e1f3ac8d2ca58db33dbb49ed462aecfb71941e
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-06 19:44:38 -0400

    Time: 4 ms (73.47%), Space: 3.6 MB (61.22%) - LeetHub

--- 1087-brace-expansion/1087-brace-expansion.go
+++ 1087-brace-expansion/1087-brace-expansion.go
@@ -1,6 +1,21 @@
+/*
+    approach: backtrack
+    - what are we backtracking?
+    - the path we form from input string
+    - We will first convert our input string to be parsable list of strings
+    - This way we wont have to complicate our logic with what to when we run into different types of characters
+    - Our main logic will simply work with a for loop
+    - We will have a 2D nested array of characters
+    - So our backtrack func will be hold the pointer to outter element
+    - While inside each for backtrack call, a for loop will loop over each char within a nested array
+    
+    Time: exponential
+    space: o(n) to expand input string into a nested array
+*/  
 func expand(s string) []string {
     blocks := [][]string{}
     i := 0
+    // time:  o(n) to loop the string
     for i < len(s) {
         block := []string{}
         if s[i] == '{' {
