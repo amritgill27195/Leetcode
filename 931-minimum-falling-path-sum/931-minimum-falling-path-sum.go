de1f43fc7151631360d482c2d5377162bdca9a31
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-07 20:42:42 -0400

    Time: 11 ms (86.18%), Space: 5.3 MB (84.55%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.go
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.go
@@ -1,51 +1,52 @@
-// func minFallingPathSum(matrix [][]int) int {
-//     m := len(matrix)
-//     n := len(matrix[0])
-    
-//     dirs := [][]int{{1,-1},{1,0},{1,1}}
-//     for i := m-2; i >= 0; i-- {
-//         for j := 0; j < n; j++ {
-//             min := math.MaxInt64
-//             // this for loop is constant -- its 3 loops for each r,c
-//             for _, dir := range dirs {
-//                 nr := i+dir[0]
-//                 nc := j+dir[1]
-//                 if nr < m && nc < n && nr >= 0 && nc >= 0 {
-//                     if matrix[nr][nc] < min {
-//                         min = matrix[nr][nc]
-//                     }
-//                 }
-//             }
-//             matrix[i][j] += min
-//         }
-//     }
-//     minVal := matrix[0][0]
-//     for j := 1; j < n; j++ {
-//         if matrix[0][j] < minVal {
-//             minVal = matrix[0][j]
-//         }
-//     }
-//     return minVal
-// }
 func minFallingPathSum(matrix [][]int) int {
     m := len(matrix)
     n := len(matrix[0])
+    
+    dirs := [][]int{{1,-1},{1,0},{1,1}}
     for i := m-2; i >= 0; i-- {
         for j := 0; j < n; j++ {
-            if j == 0 {
-                matrix[i][j] += int(math.Min(float64(matrix[i+1][0]), float64(matrix[i+1][1])))
-            } else if j == n-1 {
-                matrix[i][j] += int(math.Min(float64(matrix[i+1][j]), float64(matrix[i+1][j-1])))
-            } else {
-                matrix[i][j] += int(math.Min(float64(matrix[i+1][j]), math.Min(float64(matrix[i+1][j-1]),float64(matrix[i+1][j+1]))))
+            min := math.MaxInt64
+            // this for loop is constant -- its 3 loops for each r,c
+            for _, dir := range dirs {
+                nr := i+dir[0]
+                nc := j+dir[1]
+                if nr < m && nc < n && nr >= 0 && nc >= 0 {
+                    if matrix[nr][nc] < min {
+                        min = matrix[nr][nc]
+                    }
+                }
             }
+            matrix[i][j] += min
         }
     }
-    min := matrix[0][0]
+    minVal := matrix[0][0]
     for j := 1; j < n; j++ {
-        if matrix[0][j] < min {
-            min = matrix[0][j]
+        if matrix[0][j] < minVal {
+            minVal = matrix[0][j]
         }
     }
-    return min
+    return minVal
 }
+
+// func minFallingPathSum(matrix [][]int) int {
+//     m := len(matrix)
+//     n := len(matrix[0])
+//     for i := m-2; i >= 0; i-- {
+//         for j := 0; j < n; j++ {
+//             if j == 0 {
+//                 matrix[i][j] += int(math.Min(float64(matrix[i+1][0]), float64(matrix[i+1][1])))
+//             } else if j == n-1 {
+//                 matrix[i][j] += int(math.Min(float64(matrix[i+1][j]), float64(matrix[i+1][j-1])))
+//             } else {
+//                 matrix[i][j] += int(math.Min(float64(matrix[i+1][j]), math.Min(float64(matrix[i+1][j-1]),float64(matrix[i+1][j+1]))))
+//             }
+//         }
+//     }
+//     min := matrix[0][0]
+//     for j := 1; j < n; j++ {
+//         if matrix[0][j] < min {
+//             min = matrix[0][j]
+//         }
+//     }
+//     return min
+// }
