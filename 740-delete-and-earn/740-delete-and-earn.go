779113e3b7a38ecf9409bfe853a1b1a3a1973cbe
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-10 19:29:31 -0400

    Time: 12 ms (65.10%), Space: 4.1 MB (98.04%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.go
+++ 740-delete-and-earn/740-delete-and-earn.go
@@ -1,16 +1,22 @@
 func deleteAndEarn(nums []int) int {
     max := math.MinInt64
+    // o(n) time
     for _, num := range nums {
         if num > max { max = num }
     }
+    // o(maxNumber) space
+    // o(n) time
     values := make([]int, max+1)
     for _, num := range nums {
         values[num] += num
     }
     
+    // o(1) space ( 0 || 1 case )
     dp := make([]int, 2)
     dp[0] = 0
     dp[1] = values[0]
+    // o(maxNumber) time
+    // o(1) space
     for i := 1; i < len(values); i++ {
         choose := dp[1]
         notChoose := dp[0]
@@ -19,3 +25,6 @@ func deleteAndEarn(nums []int) int {
     }
     return int(math.Max(float64(dp[0]), float64(dp[1])))
 }
+
+// o(n) + o(n) + o(maxNumber) = o(2n) + o(maxNumber) time
+// space: o(maxNumber) 
