013966ceea0d80827bb6e05f29fe67881e55ef6f
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-10 17:01:18 -0400

    Time: 2 ms (40.18%), Space: 2 MB (77.52%) - LeetHub

--- 198-house-robber/198-house-robber.go
+++ 198-house-robber/198-house-robber.go
@@ -23,19 +23,35 @@
 
 
 // top - down using dp matrix
+// func rob(nums []int) int {
+//     dp := make([][]int, len(nums))
+//     for idx, _ := range dp {
+//         dp[idx] = make([]int, 2) // 0 | 1 case
+//     }
+//     dp[0][0] = 0
+//     dp[0][1] = nums[0]
+//     for i := 1; i < len(dp); i++ {
+//         dp[i][0] = int(math.Max(float64(dp[i-1][0]),float64(dp[i-1][1]))) 
+//         dp[i][1] = nums[i] + dp[i-1][0]
+//     }
+//     if dp[len(dp)-1][0] > dp[len(dp)-1][1] {
+//         return dp[len(dp)-1][0]
+//     }
+//     return dp[len(dp)-1][1]
+// }
+
+
+// bottom up using dp matrix
 func rob(nums []int) int {
     dp := make([][]int, len(nums))
     for idx, _ := range dp {
         dp[idx] = make([]int, 2) // 0 | 1 case
     }
-    dp[0][0] = 0
-    dp[0][1] = nums[0]
-    for i := 1; i < len(dp); i++ {
-        dp[i][0] = int(math.Max(float64(dp[i-1][0]),float64(dp[i-1][1]))) 
-        dp[i][1] = nums[i] + dp[i-1][0]
+    dp[len(dp)-1][0] = 0
+    dp[len(dp)-1][1] = nums[len(nums)-1]
+    for i := len(dp)-2; i >= 0; i-- {
+        dp[i][0] = int(math.Max(float64(dp[i+1][0]),float64(dp[i+1][1]))) 
+        dp[i][1] = nums[i] + dp[i+1][0]
     }
-    if dp[len(dp)-1][0] > dp[len(dp)-1][1] {
-        return dp[len(dp)-1][0]
-    }
-    return dp[len(dp)-1][1]
+    return int(math.Max(float64(dp[0][1]),float64(dp[0][0])))
 }
