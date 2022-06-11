04a81f550474f633ba086e768cd2acf85cefe2dc
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-11 12:01:38 -0400

    Time: 49 ms (27.38%), Space: 6.3 MB (87.28%) - LeetHub

--- 322-coin-change/322-coin-change.go
+++ 322-coin-change/322-coin-change.go
@@ -1,29 +1,22 @@
 func coinChange(coins []int, amount int) int {
-    m := len(coins)
     n := amount
-    dp := make([][]int, m+1)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n+1)
-    }
-    for j := 1; j < len(dp[0]); j++ {
-        dp[0][j] = amount+1
-    }
-    
-    for i := 1; i < len(dp); i++ { // coins
-        for j := 1; j < len(dp[0]); j++ { // amount
-            coinVal := coins[i-1]
+    dp := make([]int, n+1)
+    for i := 1; i < len(dp); i++ {
+        dp[i] = amount+1
+    } 
+    for i := 0; i < len(coins); i++ {
+        for j := 0; j < len(dp); j++ {
+            coinVal := coins[i]
             am := j
             if coinVal > am {
-                dp[i][j] = dp[i-1][j]
-            } else {
-                dp[i][j] = int(math.Min(float64(dp[i-1][j]), float64(dp[i][j-coinVal]+1)))
+                continue
             }
+            dp[j] = int(math.Min(float64(dp[j]),float64(dp[j-coinVal]+1)))
         }
     }
-    result := dp[len(dp)-1][len(dp[0])-1]
+    result := dp[len(dp)-1]
     if result == amount+1 {
         return -1
     }
     return result
-    
 }
