bf0f094c880b35828f7f8d7b8559e698688a853a
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-11 14:54:44 -0400

    Time: 21 ms (27.86%), Space: 5.4 MB (70.71%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.go
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.go
@@ -1,23 +1,21 @@
 func minFallingPathSum(matrix [][]int) int {
+    dirs := [][]int{{1,0}, {1,-1}, {1,1}}
     m := len(matrix)
     n := len(matrix[0])
     
-    dirs := [][]int{{1,-1},{1,0},{1,1}}
     for i := m-2; i >= 0; i-- {
         for j := 0; j < n; j++ {
             min := math.MaxInt64
-            // this for loop is constant -- its 3 loops for each r,c
             for _, dir := range dirs {
                 nr := i+dir[0]
                 nc := j+dir[1]
-                if nr < m && nc < n && nr >= 0 && nc >= 0 {
+                if nr >= 0 && nr < m && nc >= 0 && nc < n {
                     if matrix[nr][nc] < min {
                         min = matrix[nr][nc]
                     }
                 }
             }
             matrix[i][j] += min
-           
         }
     }
     minVal := matrix[0][0]
@@ -26,6 +24,5 @@ func minFallingPathSum(matrix [][]int) int {
             minVal = matrix[0][j]
         }
     }
-    return minVal
+    return minVal    
 }
-
