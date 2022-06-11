a9e085016fb4369fb812ad7b2f0665f83d331f38
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-11 13:45:27 -0400

    Time: 0 ms (100.00%), Space: 2.2 MB (14.47%) - LeetHub

--- 198-house-robber/198-house-robber.go
+++ 198-house-robber/198-house-robber.go
@@ -1,97 +1,61 @@
-
-// brute force dfs
-// time: exponential
 // func rob(nums []int) int {
-//     max := math.MinInt64
-//     var dfs func(start int, profit int)
-//     dfs = func(start int, profit int) {
+//     var dfs func(start int, p int) int
+//     dfs = func(start,p int) int {
 //         // base
-//         if profit > max {
-//             max = profit
-//         }
 //         if start >= len(nums) {
-//             return
+//             return p
 //         }
         
 //         // logic
-//         dfs(start+2, profit+nums[start])
-//         dfs(start+1, profit)
+//         choose := dfs(start+2, p+nums[start])
+//         notChoose := dfs(start+1, p)
+        
+//         if choose > notChoose {
+//             return choose
+//         }
+//         return notChoose
+//     }
+    
+//     return dfs(0,0)
+// }
+    
+// func rob(nums []int) int {
+//     profit := 0
+//     var dfs func(start int, p int)
+//     dfs = func(start,p int) {
+//         // base
+//         if start >= len(nums) {
+//             if p > profit {
+//                 profit = p
+//             }
+//             return
+//         }
+//         dfs(start+2, p+nums[start])
+//         dfs(start+1, p)
 //     }
     
 //     dfs(0,0)
-//     return max
+//     return profit
 // }
 
-
-// top - down using dp matrix
-// time: o(n)
-// space: o(n)
-// func rob(nums []int) int {
-//     dp := make([][]int, len(nums))
-//     for idx, _ := range dp {
-//         dp[idx] = make([]int, 2) // 0 | 1 case
-//     }
-//     dp[0][0] = 0
-//     dp[0][1] = nums[0]
-//     for i := 1; i < len(dp); i++ {
-//         dp[i][0] = int(math.Max(float64(dp[i-1][0]),float64(dp[i-1][1]))) 
-//         dp[i][1] = nums[i] + dp[i-1][0]
-//     }
-//     if dp[len(dp)-1][0] > dp[len(dp)-1][1] {
-//         return dp[len(dp)-1][0]
-//     }
-//     return dp[len(dp)-1][1]
-// }
-
-
-// bottom up using dp matrix
-// time: o(n)
-// space: o(n)
-// func rob(nums []int) int {
-//     dp := make([][]int, len(nums))
-//     for idx, _ := range dp {
-//         dp[idx] = make([]int, 2) // 0 | 1 case
-//     }
-//     dp[len(dp)-1][0] = 0
-//     dp[len(dp)-1][1] = nums[len(nums)-1]
-//     for i := len(dp)-2; i >= 0; i-- {
-//         dp[i][0] = int(math.Max(float64(dp[i+1][0]),float64(dp[i+1][1]))) 
-//         dp[i][1] = nums[i] + dp[i+1][0]
-//     }
-//     return int(math.Max(float64(dp[0][1]),float64(dp[0][0])))
-// }
-
-
-// bottom up DP using dp array of fixed size -- can be replaced by 2 vars instead
-// time: o(n)
-// space: o(1)
-// func rob(nums []int) int {
-//     dp := make([]int, 2)
-//     dp[0] = 0
-//     dp[1] = nums[len(nums)-1]
-//     for i := len(nums)-2; i >= 0; i-- {
-//         choose := dp[1]
-//         notChoose := dp[0]
-        
-//         dp[0] = int(math.Max(float64(choose),float64(notChoose))) 
-//         dp[1] = nums[i] + notChoose
-//     }
-//     return int(math.Max(float64(dp[0]),float64(dp[1])))
-// }
-
-
-// top - down using fixed size ( 2 ) 1D array -- can be replaced with 2 vars
-// time: o(n)
-// space: o(1)
+// top down
 func rob(nums []int) int {
-    dp := make([]int, 2)
-    dp[0] = 0
-    dp[1] = nums[0]
-    for i := 1; i < len(nums); i++ {
-        choose := dp[1]
-        notChoose := dp[0]
-        dp[0] = int(math.Max(float64(notChoose),float64(choose))) 
-        dp[1] = nums[i] + notChoose
+    dp := make([][]int, len(nums))
+    for idx, _ := range dp {
+        dp[idx] = make([]int, 2)
+        if idx == 0 {
+            dp[0][0] = 0
+            dp[0][1] = nums[0]
+        }
     }
-    return int(math.Max(float64(dp[0]),float64(dp[1])))
+    
+    for i := 1; i < len(nums); i++ {
+        dp[i][0] = int(math.Max(float64(dp[i-1][0]), float64(dp[i-1][1])))
+        dp[i][1] = nums[i] + dp[i-1][0]
+    }
+    if dp[len(dp)-1][0] > dp[len(dp)-1][1] {
+        return dp[len(dp)-1][0]
+    }
+    return dp[len(dp)-1][1]
 }
+    
