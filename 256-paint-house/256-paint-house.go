df9d035b4eb7c43a5e7b94ba951287581841824c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-11 14:26:03 -0400

    Time: 4 ms (78.57%), Space: 3 MB (45.24%) - LeetHub

--- 256-paint-house/256-paint-house.go
+++ 256-paint-house/256-paint-house.go
@@ -1,20 +1,10 @@
 func minCost(costs [][]int) int {
     m := len(costs)
-    n := len(costs[0])
-    
-    dp := make([][]int, m)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n)
+    for i := m-2; i >= 0; i-- {
+        costs[i][0] += int(math.Min(float64(costs[i+1][1]),float64(costs[i+1][2])))
+        costs[i][1] += int(math.Min(float64(costs[i+1][0]),float64(costs[i+1][2])))
+        costs[i][2] += int(math.Min(float64(costs[i+1][0]),float64(costs[i+1][1])))
     }
     
-    for j := 0; j < len(dp[0]); j++ {
-        dp[m-1][j] = costs[m-1][j]
-    }
-    
-    for i := len(dp)-2; i >= 0; i-- {
-        dp[i][0] = costs[i][0] + int(math.Min(float64(dp[i+1][1]),float64(dp[i+1][2])))
-        dp[i][1] = costs[i][1] + int(math.Min(float64(dp[i+1][0]),float64(dp[i+1][2])))
-        dp[i][2] = costs[i][2] + int(math.Min(float64(dp[i+1][0]),float64(dp[i+1][1])))
-    } 
-    return int(math.Min(float64(dp[0][0]), math.Min(float64(dp[0][1]), float64(dp[0][2]))))
+    return int(math.Min(float64(costs[0][0]), math.Min(float64(costs[0][1]), float64(costs[0][2]))))
 }
