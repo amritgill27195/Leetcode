62f66cd83d72599e7bb034299385fb0bf32de557
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-11 12:40:40 -0400

    Time: 7 ms (37.70%), Space: 2.3 MB (43.98%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.go
+++ 518-coin-change-2/518-coin-change-2.go
@@ -1,23 +1,16 @@
 func change(amount int, coins []int) int {
-    m := len(coins)
     n := amount
-    dp := make([][]int, m+1)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n+1)
-        dp[idx][0] = 1
-    }
+    dp := make([]int, n+1)
+    dp[0] = 1
     
-    for i := 1; i < len(dp); i++ {
-        for j := 1; j < len(dp[0]); j++ {
-            coinVal := coins[i-1]
+    for i := 0; i < len(coins); i++ {
+        for j := 1; j < len(dp);j++ {
             am := j
-            if coinVal > am {
-                dp[i][j] = dp[i-1][j]
-            } else {
-                dp[i][j] = dp[i-1][j] + dp[i][j-coinVal]
-            }
+            coinVal := coins[i]
+            if coinVal > am {continue}
+            dp[j] = dp[j] + dp[j-coinVal]
         }
     }
+    return dp[len(dp)-1]
     
-    return dp[len(dp)-1][len(dp[0])-1]
 }
