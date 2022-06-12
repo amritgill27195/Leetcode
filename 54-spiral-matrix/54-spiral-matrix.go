2013dd40b15eb84e548da18b0fb69982b9109d66
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-12 12:33:22 -0400

    Time: 0 ms (100.00%), Space: 2 MB (66.92%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.go
+++ 54-spiral-matrix/54-spiral-matrix.go
@@ -1,44 +1,48 @@
+// approach : 4 pointers ( left col, right col, top row, bottom row) -- use them to shrink boundaries!
 func spiralOrder(matrix [][]int) []int {
     m := len(matrix)
     n := len(matrix[0])
-    
     left := 0
-    right := n-1
     top := 0
+    right := n-1
     bottom := m-1
-    result := []int{}
+    out := []int{}
     for left <= right && top <= bottom {
         
-        // left to right
+        // top row
+        // left-right
         for i := top; i <= right; i++ {
-            result = append(result, matrix[top][i])
+            out = append(out, matrix[top][i])
         }
         top++
         
-        
-        if left <= right && top <= bottom{ 
+        // right col
+        // top to bottom
+        if left <= right && top <= bottom {
             for i := top; i <= bottom; i++ {
-                result = append(result, matrix[i][right])
+                out = append(out, matrix[i][right])
             }
         }
         right--
-
         
+        
+        // bottom row
+        // right to left
         if left <= right && top <= bottom {
             for i := right; i >= left; i-- {
-                result = append(result, matrix[bottom][i])
+                out = append(out, matrix[bottom][i])
             }
         }
         bottom--
         
-        
-        if left <= right && top <= bottom{
-            for i := bottom; i>=top; i-- {
-                result = append(result, matrix[i][left])
+        // left col
+        // bottom to top
+        if left <= right && top <= bottom {
+            for i := bottom; i >= top; i-- {
+                out = append(out, matrix[i][left])
             }
         }
         left++
-    
     }
-    return result
+    return out
 }
