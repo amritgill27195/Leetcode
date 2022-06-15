ed935c7de3b26702505102e7ec1ea34a6157e8a5
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-14 22:12:22 -0400

    Time: 79 ms (91.92%), Space: 8.1 MB (99.33%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.go
+++ 938-range-sum-of-bst/938-range-sum-of-bst.go
@@ -27,17 +27,14 @@ func rangeSumBST(root *TreeNode, low int, high int) int {
         }
         
         // inorder
-        // if r.Val >= low && r.Val <= high {
-        //     sum+=r.Val
-        // }
+        if r.Val >= low && r.Val <= high {
+            sum+=r.Val
+        }
         
         if r.Val < high {
             dfs(r.Right)
         }
-        // post order
-        if r.Val >= low && r.Val <= high {
-            sum+=r.Val
-        }
+        
     }
     dfs(root)
     return sum
