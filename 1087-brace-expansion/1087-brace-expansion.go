8a44c0d899659ff153ed7ad9ea2f871abbdeba54
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-16 15:20:53 -0400

    Time: 4 ms (70.27%), Space: 3.5 MB (54.05%) - LeetHub

--- 1087-brace-expansion/1087-brace-expansion.go
+++ 1087-brace-expansion/1087-brace-expansion.go
@@ -1,41 +1,27 @@
-/*
-    approach: backtrack
-    - what are we backtracking?
-    - the path we form from input string
-    - We will first convert our input string to be parsable list of strings
-    - This way we wont have to complicate our logic with what to when we run into different types of characters
-    - Our main logic will simply work with a for loop
-    - We will have a 2D nested array of characters
-    - So our backtrack func will be hold the pointer to outter element
-    - While inside each for backtrack call, a for loop will loop over each char within a nested array
-    
-    Time: exponential
-    space: o(n) to expand input string into a nested array
-*/  
 func expand(s string) []string {
     blocks := [][]string{}
     i := 0
-    // time:  o(n) to loop the string
+    
     for i < len(s) {
         block := []string{}
         if s[i] == '{' {
             i++
             for s[i] != '}' {
-                if s[i] == ',' {i++; continue}
-                block = append(block, string(s[i]))
+                if s[i] != ','{
+                    block = append(block, string(s[i]))    
+                }
                 i++
             }
         } else {
             block = append(block, string(s[i]))
         }
-        i++
         sort.Strings(block)
         blocks = append(blocks, block)
-        
+        i++
     }
     result := []string{}
-    var backtrack func(path string, start int)
-    backtrack = func(path string, start int) {
+    var backtrack func(start int, path string)
+    backtrack = func(start int, path string) {
         // base
         if start == len(blocks) {
             result = append(result, path)
@@ -44,11 +30,15 @@ func expand(s string) []string {
         
         // logic
         for i := 0; i < len(blocks[start]); i++ {
+            // action
             path += blocks[start][i]
-            backtrack(path, start+1)
+            // recurse
+            backtrack(start+1, path)
+            // backtrack
             path = path[:len(path)-1]
         }
     }
-    backtrack("", 0)
+    backtrack(0, "")   
     return result
+    
 }
