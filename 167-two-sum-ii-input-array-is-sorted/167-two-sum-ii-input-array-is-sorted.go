b80f73451ca7dcfa61ce62f1aebe88a32a1862a1
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-17 17:56:38 -0400

    Time: 20 ms (38.07%), Space: 5.5 MB (55.55%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
@@ -1,19 +1,16 @@
 func twoSum(numbers []int, target int) []int {
-    for i := 0; i < len(numbers); i++ {
-        num := numbers[i]
-        complement := target-num
-        left := i+1
-        right := len(numbers)-1
-        for left <= right {
-            mid := left + (right-left)/2
-            if numbers[mid] == complement {
-                return []int{i+1, mid+1}
-            } else if numbers[mid] > complement {
-                right = mid-1
-            } else {
-                left = mid+1
-            }
+    left := 0
+    right := len(numbers)-1
+    
+    for left < right {
+        sum := numbers[left] + numbers[right]
+        if sum == target {
+            return []int{left+1, right+1}
+        } else if sum < target {
+            left++
+        } else {
+            right--
         }
     }
-    return []int{-1,-1}
+    return nil
 }
