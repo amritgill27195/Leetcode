c62d5de72f983b2e1ec490c9d32c2a58e84d29f6
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-18 10:59:52 -0400

    Time: 4 ms (41.55%), Space: 2.7 MB (87.86%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.go
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.go
@@ -1,9 +1,20 @@
+// func runningSum(nums []int) []int {
+//     rs := 0
+//     out := []int{}
+//     for i := 0; i < len(nums); i++ {
+//         rs += nums[i]
+//         out = append(out, rs)
+//     }
+//     return out
+// }
+
 func runningSum(nums []int) []int {
-    rs := 0
-    out := []int{}
-    for i := 0; i < len(nums); i++ {
-        rs += nums[i]
-        out = append(out, rs)
+    if len(nums) <= 1 {
+        return nums
     }
-    return out
+    for i := 1; i < len(nums); i++ {
+        prev := nums[i-1]
+        nums[i] = nums[i] + prev
+    }
+    return nums
 }
