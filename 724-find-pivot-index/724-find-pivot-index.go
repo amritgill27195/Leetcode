8b9a8689a07f6855cccdd6885f4efb94daa64b49
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-18 11:36:45 -0400

    Time: 32 ms (30.50%), Space: 6.2 MB (65.50%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.go
+++ 724-find-pivot-index/724-find-pivot-index.go
@@ -1,19 +1,36 @@
+// func pivotIndex(nums []int) int {
+//     leftSum := []int{nums[0]}
+//     for i := 1; i < len(nums) ;i++ {
+//         leftSum = append(leftSum, leftSum[i-1]+nums[i])
+//     }
+//     rightSum := make([]int, len(nums))
+//     rightSum[len(rightSum)-1] = nums[len(nums)-1]
+//     for i := len(nums)-2; i >=0 ; i-- {
+//         rightSum[i] = nums[i] + rightSum[i+1]
+//     }
+    
+//     for i := 0; i < len(nums); i++ {
+//         if leftSum[i] == rightSum[i] {
+//             return i
+//         }
+//     }
+    
+//     return -1
+// }
+
 func pivotIndex(nums []int) int {
-    leftSum := []int{nums[0]}
-    for i := 1; i < len(nums) ;i++ {
-        leftSum = append(leftSum, leftSum[i-1]+nums[i])
-    }
-    rightSum := make([]int, len(nums))
-    rightSum[len(rightSum)-1] = nums[len(nums)-1]
-    for i := len(nums)-2; i >=0 ; i-- {
-        rightSum[i] = nums[i] + rightSum[i+1]
+    totalSum := 0
+    for i := 0; i < len(nums); i++ {
+        totalSum += nums[i]
     }
     
+    leftSum := 0
     for i := 0; i < len(nums); i++ {
-        if leftSum[i] == rightSum[i] {
+        rightSumExcludingI := totalSum-leftSum-nums[i]
+        if rightSumExcludingI == leftSum {
             return i
         }
+        leftSum += nums[i]
     }
-    
     return -1
 }
