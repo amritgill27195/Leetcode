890a7cbb0e3177c09e415a0f258605adabf896a8
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-18 12:49:20 -0400

    Time: 26 ms (44.00%), Space: 6.5 MB (23.50%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.go
+++ 724-find-pivot-index/724-find-pivot-index.go
@@ -1,3 +1,20 @@
+/*
+    approach: brute force using extra space
+    - Create a leftSum array
+    - loop over nums arr from left to right
+    - calc running left sum and add to leftSum array
+        - prevLeftSum (i-1) + curr number nums[i]
+    - Create a rightSum array
+    - loop over nums arr from right to left
+    - calc running right sum and add to rightSum array
+        - prevRightSum (i+1)  + curr number nums[i]
+    - wherever the 2 elements match in leftSum and rightSum, thats the pivot idx
+    
+    Time: o(2n), i.e; o(n)
+    Space: o(2n), i.e; o(n)
+*/
+
+
 // func pivotIndex(nums []int) int {
 //     leftSum := []int{nums[0]}
 //     for i := 1; i < len(nums) ;i++ {
@@ -18,6 +35,19 @@
 //     return -1
 // }
 
+/*
+    How is this a prefix/running sum?
+    - we are looking for a idx whose sum on left ( excluding the idx ) is equal to the sum of elements on right side of this idx
+    - if we had total sum and then for each idx, we can calc the leftSum ( i.e running sum ) and we can get the right sum by
+    - totalSum - leftSum -- this gives the right sum ( given we have total and leftSum )
+    - but we have to exclude the idx under evaluation to be the pivot idx
+    - therefore to exclude the pivot idx, we will do totalSum - leftSumSoFar - currentValAtIdx
+    - once we do that , we have leftSum and we have rightSum 
+    - if both are equal, this means our current idx is separator/pivot idx
+
+    time: o(2n) - o(n)
+    space: o(1)
+*/
 func pivotIndex(nums []int) int {
     totalSum := 0
     for i := 0; i < len(nums); i++ {
