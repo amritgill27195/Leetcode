27e7f169705638e59e9fcb0917713380adc48ec1
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-17 21:02:36 -0400

    Time: 9 ms (44.05%), Space: 4.3 MB (16.67%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.go
+++ 243-shortest-word-distance/243-shortest-word-distance.go
@@ -1,10 +1,16 @@
 /*
-    approach: hash map
-    - we need to search for word1 and word2 and find the closest distance between those 2 words
-    - words could be repeated multiple times in the wordsDict
-    - so for easy lookup of a word's idx, we will store word with all of its indicies in a map
-    - then the map will look like: { $word: [$indicies] }
+    approach : Two pointers
+    - we can create 2 pointers p1 ( for word1 ) and p2 ( for word2 ), both with initial value of -1
+    - these 2 pointers will hold the idx position of word1 and word2 when we find them
+    - to find them, we will loop over wordsDict and in each iteration;
+        - if we found word1, then set the p1 value to current idx
+        - if we found word2, then set the p2 value to current idx
+        - once both p1 and p2 are not -1, we have to positions to calculate distance from
+        - calc dist and save it global min as needed
+    - finally return min
     
+    time: o(n)
+    space: o(1)
 */
 
 func shortestDistance(wordsDict []string, word1, word2 string) int {
@@ -30,6 +36,33 @@ func shortestDistance(wordsDict []string, word1, word2 string) int {
      }
     return min
 }
+
+/*
+    approach: hash map
+    - we need to search for word1 and word2 and find the closest distance between those 2 words
+    - words could be repeated multiple times in the wordsDict
+    - so for easy lookup of a word's idx, we will store word with all of its indicies in a map
+    - then the map will look like: { $word: [$indicies] }
+    - so we will loop over all elements in wordsDict and store them in the map
+    - then grab the list of indicies for both word1 and word2
+    - set two pointers on both of the list of indicies from the beginning
+    - while both pointers are inbound,
+        - calc abs dist between 2 indicies
+        - save it in global min as needed
+        - Now which pointer to move?
+            - Since the indicies in both word1 and word2 list are going to be sorted
+            - how are they already sorted?
+            - because we went over the list from left to right when generating the idxMap
+            - therefore already sorted.
+            - moving which pointer is going to result into smaller diff?
+            - if we move the pointer thats pointing to big value, the diff will only get bigger
+            - if we move the pointer thats pointing to smaller value, there are chances of the diff to get smaller
+            - therefore always move the smaller value pointer
+    - finally return the min
+    
+    time: o(n)
+    space: o(n) 
+*/
 
 // func shortestDistance(wordsDict []string, word1 string, word2 string) int {
 //     idxMap := map[string][]int{}
