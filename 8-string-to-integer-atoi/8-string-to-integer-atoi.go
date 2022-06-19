e31204cedff6c208faf581c0ebc4c3f736caa3d2
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-19 13:17:56 -0400

    Time: 3 ms (66.94%), Space: 2.2 MB (34.21%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.go
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.go
@@ -19,6 +19,27 @@ func myAtoi(s string) int {
         if isDigit(char) {
             charInt := int(char-'0')
             if !isNegative {
+                /*
+                    why these 2 checks as separate checks?
+                    - first, maxInt32 is 2147483647
+                    - keep in mind we are "APPENDING" numbers to our n
+                    - we drop the last number of maxInt32 (maxInt32/10) and we are left with 214748364
+                        - why?
+                        - because we have 2 cases
+                        - if our n == limit(214748364) then, the only choice of appending another number must be within 0 to 7 ( inclusive )
+                            - so explicitly check if the new int being appending is within this range, if not, return maxInt32
+                            - but then, why are we comparing with 8 when the number being formed in negative?
+                            - because minInt32 (when we are forming negative number), minInt32 limit is 2147483648 instead of 2147483647
+                            - therefore...
+                        - We are appending, not adding...
+                        - the second case is when our already past limit ( n > limit(214748364) )
+                            - i.e n could be 214748365, 214748366, 214748367 for example
+                            - then we cannot proceed to add append another number right? 
+                        - therefore in this case, as soon as we are past the limit, regardless if the new number being appending is within our range, we cannot add it because
+                        - it will be past int32.
+                    
+                        
+                */
                 if n == limit && charInt >= 7 {return math.MaxInt32}
                 if n > limit {return math.MaxInt32}
             } else {
