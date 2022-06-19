62b34960386cc8ed1cde7ee7337c76947da6a83c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-19 10:53:35 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (84.54%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.go
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.go
@@ -1,39 +1,40 @@
-// time: o(n)
-// space: o(1)
 func myAtoi(s string) int {
     s = strings.TrimSpace(s)
     if s == "" {
         return 0
     }
     first := s[0]
-    if (!(first == '-' || first == '+')) && !isDigit(first) {
+    if !(first == '-' || first == '+') && !isDigit(first) {
         return 0
     }
-    n := 0
     isNegative := false
-    if first == '-' { isNegative = true }
+    if first == '-' {isNegative=true}
+    n := 0
     limit := math.MaxInt32/10
     for i := 0; i < len(s); i++ {
-        c := s[i]
-        cInt := int(c-'0')
-        if isDigit(c) {
+        char := s[i]
+
+        if isDigit(char) {
+            charInt := int(char-'0')
             if !isNegative {
+                if n == limit && charInt >= 7 {return math.MaxInt32}
                 if n > limit {return math.MaxInt32}
-                if n == limit && cInt >= 7 {return math.MaxInt32}
             } else {
+                if n == limit && charInt >= 8 {return math.MinInt32}
                 if n > limit {return math.MinInt32}
-                if n == limit && cInt >= 8 {return math.MinInt32}
             }
-            n = n * 10 + cInt
+            n = n * 10 + charInt
         } else if i != 0 {
-            break 
+            break
         }
     }
     if isNegative {
         n *= -1
     }
     return n
+    
 }
-func isDigit(c byte) bool {
-    return c >= '0' && c <= '9'
+
+func isDigit(n byte) bool {
+    return n >= '0' && n <= '9'
 }
