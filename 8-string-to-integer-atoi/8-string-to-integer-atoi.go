d5e768bf9d7fdb44f4eb3f09592f0f75f3614c7b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-18 22:10:18 -0400

    Time: 8 ms (14.31%), Space: 2.2 MB (34.21%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.go
@@ -0,0 +1,39 @@
+func myAtoi(s string) int {
+    s = strings.TrimSpace(s)
+    if s == "" {
+        return 0
+    }
+    first := s[0]
+    if (!(first == '-' || first == '+')) && !isDigit(first) {
+        // fmt.Println(s)
+        // fmt.Println(string(first), "isDigit: ", isDigit(first))
+        return 0
+    }
+    n := 0
+    isNegative := false
+    if first == '-' { isNegative = true }
+    limit := math.MaxInt32/10
+    for i := 0; i < len(s); i++ {
+        c := s[i]
+        cInt := int(c-'0')
+        if isDigit(c) {
+            if !isNegative {
+                if n > limit {return math.MaxInt32}
+                if n == limit && cInt >= 7 {return math.MaxInt32}
+            } else {
+                if n > limit {return math.MinInt32}
+                if n == limit && cInt >= 8 {return math.MinInt32}
+            }
+            n = n * 10 + cInt
+        } else if i != 0 {
+            break 
+        }
+    }
+    if isNegative {
+        n *= -1
+    }
+    return n
+}
+func isDigit(c byte) bool {
+    return c >= '0' && c <= '9'
+}
