1f2aa366a625430686008a11942da6dc53f86114
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-20 17:18:03 -0400

    Time: 45 ms (39.37%), Space: 7.2 MB (97.04%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.go
+++ 49-group-anagrams/49-group-anagrams.go
@@ -1,16 +1,27 @@
 func groupAnagrams(strs []string) [][]string {
-    m := map[string][]string{}
-    
-    for i := 0; i < len(strs); i++ {
+    m := map[float64][]string{}
+    for i := 0 ; i < len(strs); i++ {
         word := strs[i]
-        wordSplit := strings.Split(word, "")
-        sort.Strings(wordSplit)
-        wordSorted := strings.Join(wordSplit, "")
-        m[wordSorted] = append(m[wordSorted],word)
+        hashVal := hash(word)
+        m[hashVal] = append(m[hashVal], strs[i])
     }
     out := [][]string{}
     for _, v := range m {
         out = append(out, v)
     }
+    
     return out
+   
+}
+
+var primes = []float64{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101}
+
+func hash(word string) float64 {
+    prod := 1.0
+    for i := 0; i < len(word); i++ {
+        asciValOfChar := word[i] // e -- byte
+        charIdx := asciValOfChar - 'a'
+        prod *= primes[charIdx]
+    }
+    return prod
 }
