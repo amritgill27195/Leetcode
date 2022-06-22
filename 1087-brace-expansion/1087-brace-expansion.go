6e45de63ddfbf37f777193eb9b5c2139e1b5160b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-22 13:34:22 -0400

    Time: 5 ms (54.05%), Space: 3.4 MB (81.08%) - LeetHub

--- 1087-brace-expansion/1087-brace-expansion.go
+++ 1087-brace-expansion/1087-brace-expansion.go
@@ -1,42 +1,54 @@
+/*
+    approach: backtrack
+    - what are we backtracking?
+    - the path we form from input string
+    - We will first convert our input string to be parsable list of strings
+    - This way we wont have to complicate our logic with what to when we run into different types of characters
+    - Our main logic will simply work with a for loop
+    - We will have a 2D nested array of characters
+    - So our backtrack func will be hold the pointer to outter element
+    - While inside each for backtrack call, a for loop will loop over each char within a nested array
+    
+    Time: exponential
+    space: o(n) to expand input string into a nested array
+*/  
 func expand(s string) []string {
-    sList := [][]string{}
+    blocks := [][]string{}
     i := 0
+    // time:  o(n) to loop the string
     for i < len(s) {
-        grp := []string{}
-        char := s[i]
-        if char == ',' {continue}
-        if char == '{' {
+        block := []string{}
+        if s[i] == '{' {
             i++
             for s[i] != '}' {
                 if s[i] == ',' {i++; continue}
-                grp = append(grp, string(s[i]))
+                block = append(block, string(s[i]))
                 i++
             }
         } else {
-            grp = append(grp, string(s[i]))
+            block = append(block, string(s[i]))
         }
         i++
-        sort.Strings(grp)
-        sList = append(sList, grp)
+        sort.Strings(block)
+        blocks = append(blocks, block)
+        
     }
     result := []string{}
-    var backtrack func(outterIdx int, path string)
-    backtrack = func(outterIdx int, path string) {
-        
+    var backtrack func(path string, start int)
+    backtrack = func(path string, start int) {
         // base
-        if outterIdx == len(sList) {
+        if start == len(blocks) {
             result = append(result, path)
             return
         }
-
+        
         // logic
-        for i := 0; i < len(sList[outterIdx]); i++ {
-            path += sList[outterIdx][i]
-            backtrack(outterIdx+1, path)
+        for i := 0; i < len(blocks[start]); i++ {
+            path += blocks[start][i]
+            backtrack(path, start+1)
             path = path[:len(path)-1]
         }
-        
     }
-    backtrack(0, "")
+    backtrack("", 0)
     return result
 }
