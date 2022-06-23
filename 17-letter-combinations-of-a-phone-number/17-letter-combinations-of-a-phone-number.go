af05d191228deeca7383027bd4bcbd4b0a222cc0
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-23 17:23:54 -0400

    Time: 1 ms (57.99%), Space: 2 MB (90.90%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.go
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.go
@@ -1,34 +1,36 @@
 func letterCombinations(digits string) []string {
     if len(digits) == 0 {return nil}
-    digMap := map[byte][]string{
-        '2': []string{"a","b","c"},
-        '3': []string{"d", "e", "f"},
-        '4': []string{"g", "h", "i"},
-        '5': []string{"j", "k", "l"},
-        '6': []string{"m","n","o"},
-        '7': []string{"p", "q", "r","s"},
-        '8': []string{"t","u","v"},
-        '9': []string{"w","x","y", "z"},
+    charsMap := map[byte][]string{
+        '2' : []string{"a","b","c"},
+        '3' : []string{"d","e","f"},
+        '4' : []string{"g","h","i"},
+        '5' : []string{"j","k","l"},
+        '6' : []string{"m","n","o"},
+        '7' : []string{"p","q","r", "s"},
+        '8' : []string{"t","u","v"},
+        '9' : []string{"w","x","y", "z"},
     }
-    var result []string
-    var dfs func(path string, start int)
-    dfs = func(path string, start int) {
+    result := []string{}
+    var backtrack func(start int, path string)
+    backtrack = func(start int, path string) {
         // base
         if len(path) == len(digits) {
             result = append(result, path)
             return
         }
-        
+        if start == len(digits) {return}
         
         // logic
-        for i := start; i < len(digits); i++ {
-            for _, char := range digMap[digits[i]] {
-                path += string(char)
-                dfs(path, i+1)
-                path = string(path[:len(path)-1])
-            }
+        chars := charsMap[digits[start]]
+        for j := 0; j < len(chars); j++ {
+            // action
+            path += chars[j]
+            // recurse
+            backtrack(start+1, path)
+            // backtrack
+            path = path[:len(path)-1]
         }
     }
-    dfs("",0)
+    backtrack(0, "")
     return result
 }
