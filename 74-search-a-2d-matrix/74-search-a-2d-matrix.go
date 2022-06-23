77960361497eb145fcfab3613c6e17fc4a2cce0c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-22 22:58:12 -0400

    Time: 0 ms (100.00%), Space: 2.7 MB (72.51%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.go
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.go
@@ -1,25 +1,23 @@
 func searchMatrix(matrix [][]int, target int) bool {
-    numRows := len(matrix)
-    numCols := len(matrix[0])
-    total := numRows * numCols
+    m := len(matrix)
+    n := len(matrix[0])
     
     left := 0
-    right := total-1
+    right := m*n-1
+    
     for left <= right {
         mid := left + (right-left)/2
-        row := mid / numCols
-        col := mid % numCols
-        if row >= numRows || col >= numCols {
-            break
-        }
-        if matrix[row][col] == target {
+        r := mid/n
+        c := mid%n
+        if r >= m || c >= n {break}
+
+        if matrix[r][c] == target {
             return true
-        } else if matrix[row][col] > target {
+        } else if matrix[r][c] > target {
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    
     return false
 }
