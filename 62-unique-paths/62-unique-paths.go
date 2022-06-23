ebc0dd783057483b961ba98de72e2adda2c700a4
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-23 11:40:13 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (54.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.go
+++ 62-unique-paths/62-unique-paths.go
@@ -1,90 +1,20 @@
-/*
-    Current confusion: When to/not-to use backtracking in BFS/DFS and why we did not use DP tables in previous backtracking/recursion problems where there may have been repeated subproblems..
-        - If you see a need for backtracking, then forget BFS, backtracking goes hand in hand with DFS ( for example populating a path to all leaf nodes in a tree )
-    
-    
-    Approach: Top down DP
-    - Top Down DP is starting from the bigger problem and going down towards a smaller problem
-    - Top Down DP == memoization
-    - Usually done with recursion
-    - Usually used with recursion as a cache to signify that "hey we have already solved this recursive case"
-    - The robot can only move either down or right at any point in time
-    - So our decision tree has two choices
-    - Go explore row down or col right
-    Time: o(mn)
-    space: o(mn)
-
-*/
-
-// Top down DP with memoization
-// func uniquePaths(m int, n int) int {
-    
-//     memo := make([][]int, m)
-//     for i := 0; i < m; i++ {
-//         memo[i] = make([]int, n)
-//     }
-
-//     var dfs func(i, j int) int
-//     dfs = func(i, j int) int{
-//         // base
-//         if i == m-1 && j == n-1 {
-//             return 1
-//         }
-//         if i == m || j == n {return 0}
-        
-        
-//         // logic
-//         if memo[i][j] == 0 {
-//             // we have not solved this repeated subproblem
-//             memo[i][j] = dfs(i+1,j) + dfs(i,j+1)
-//         }
-//         // we have solved this i,j subproblem, returned the cached/memoized result
-//         return memo[i][j]
-//     }
-//     return dfs(0,0)
-// }
-
-/*
-    Approach: Bottom up DP
-    - Starting from a smaller subproblem and bubbling up to bigger problem is bottom up DP
-    - Iterative approach 
-    
-    Time: o(mn)
-    space: o(mn)
-*/
-// Bottom up DP 
 func uniquePaths(m int, n int) int {
-    dp := make([][]int, m+1)
-    for i := 0; i < m+1; i++ {
-        dp[i] = make([]int, n+1)
+    dr := m-1
+    dc := n-1
+    memo := make([][]int, m)
+    for idx, _ := range memo {
+        memo[idx] = make([]int, n)
     }
-    dp[m-1][n-1] = 1
-    for i := m-1; i >= 0; i-- {
-        for j := n-1; j >= 0; j-- {
-            if i == m-1 && j == n-1 {continue}
-            dp[i][j] = dp[i+1][j] + dp[i][j+1]
+    var dfs func(r,c int) int
+    dfs = func(r, c int) int {
+        // base
+        if r == m || r < 0 || c == n || c < 0 {return 0}
+        if r == dr && c == dc {return 1}
+        if memo[r][c] == 0 {
+            memo[r][c] = dfs(r+1, c) + dfs(r,c+1)
         }
+        return memo[r][c]
     }
-    return dp[0][0]
-
+    
+    return dfs(0,0)
 }
-
-// brute force: DFS/recursion (TLE)
-// func uniquePaths(m int, n int) int {
-//     var dfs func(i, j int) int
-//     dfs = func(i, j int) int{
-//         // base
-//         if i == m-1 && j == n-1 {
-//             return 1
-//         }
-//         if i == m || j == n {return 0}
-        
-//         // logic
-//         return dfs(i+1,j) + dfs(i,j+1)
-//     }
-//     return dfs(0,0)
-// }
-
-
-
-
