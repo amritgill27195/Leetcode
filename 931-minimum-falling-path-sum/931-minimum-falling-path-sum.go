8e4362501a4552fa8fc77e73f8632757b9cec6d6
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-23 20:36:24 -0400

    Time: 12 ms (81.43%), Space: 6.5 MB (11.43%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.go
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.go
@@ -43,21 +43,73 @@
 
 /*
     approach: bottom up - matrix traversal
-    - start from the the 1st row
+    - start from the the 2nd last row
     - and for each col;
-    - look up and select min elements from 3 choices ( diag-left-up, up, diag-right-up )
+    - look up and select min elements from 3 choices ( diag-left-down, down, diag-right-down )
     - once we have made a decision, add that decision value to the current col
-    - and work all the way till the last row
-    - finally return the min val from the last row
+    - and work all the way up to the top row
+    - finally return the min val from the top row
     
     time: o(mn)
     space: o(1)
+*/
+
+// func minFallingPathSum(matrix [][]int) int {
+//     m := len(matrix)
+//     n := len(matrix[0])
+//     min := math.MaxInt64
+    
+//     dirs := [][]int{{1,0},{1,-1},{1,1}}
+//     for i := m-2; i >= 0; i-- {
+//         for j := 0; j < n; j++ {
+//             smallest := math.MaxInt64
+//             for _, dir := range dirs {
+//                 r := dir[0]+i
+//                 c := dir[1]+j
+//                 if r >= 0 && r < m && c < n && c >= 0 {
+//                     if matrix[r][c] < smallest {
+//                         smallest = matrix[r][c]
+//                     }
+//                 }
+//             }
+//             matrix[i][j] += smallest
+        
+//         }
+//     }
+//     for j := 0; j < n; j++ {
+//         if matrix[0][j] < min {
+//             min = matrix[0][j]
+//         }
+//     }
+//     return min
+// }
+
+
+
+/*
+    approach: bottom up - matrix traversal - if we cannot mutate the matrix - use dp matrix
+    - start from the the 2nd last row
+    - and for each col;
+    - look up and select min elements from 3 choices ( diag-left-down, down, diag-right-down )
+    - once we have made a decision, add that decision value to the current col
+    - and work all the way up to the top row
+    - finally return the min val from the top row
+    
+    time: o(mn)
+    space: o(mn)
 */
 
 func minFallingPathSum(matrix [][]int) int {
     m := len(matrix)
     n := len(matrix[0])
     min := math.MaxInt64
+    dp := make([][]int, m)
+    for idx, _ := range dp {
+        dp[idx] = make([]int, n)
+    }
+    for j := 0; j < n; j++ {
+        dp[m-1][j] = matrix[m-1][j]
+    }
     
     dirs := [][]int{{1,0},{1,-1},{1,1}}
     for i := m-2; i >= 0; i-- {
@@ -67,18 +119,18 @@ func minFallingPathSum(matrix [][]int) int {
                 r := dir[0]+i
                 c := dir[1]+j
                 if r >= 0 && r < m && c < n && c >= 0 {
-                    if matrix[r][c] < smallest {
-                        smallest = matrix[r][c]
+                    if dp[r][c] < smallest {
+                        smallest = dp[r][c]
                     }
                 }
             }
-            matrix[i][j] += smallest
+            dp[i][j] += smallest + matrix[i][j]
         
         }
     }
     for j := 0; j < n; j++ {
-        if matrix[0][j] < min {
-            min = matrix[0][j]
+        if dp[0][j] < min {
+            min = dp[0][j]
         }
     }
     return min
