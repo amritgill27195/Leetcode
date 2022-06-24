d737cbd560e80f0971663bee6490b3de601953bf
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-24 14:17:30 -0400

    Time: 26 ms (95.00%), Space: 6.7 MB (95.00%) - LeetHub

--- 490-the-maze/490-the-maze.go
+++ 490-the-maze/490-the-maze.go
@@ -7,36 +7,33 @@ func hasPath(maze [][]int, start []int, destination []int) bool {
     n := len(maze[0])
     
     if maze[dr][dc] == 1 {return false}
-    
-    q := [][]int{{sr,sc}}
-    maze[sr][sc] = 2 // 2 represents a already visited node
     dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
     
-    for len(q) != 0 {
-        dq := q[0]; q = q[1:]
-        cr := dq[0]
-        cc := dq[1]
+    var dfs func(r, c int) bool
+    dfs = func(r, c int) bool {
+        // base
+        if r < 0 || r == m || c < 0 || c == n || maze[r][c] == 2 {
+            return false
+        }
+        if r == dr && c == dc {return true}
         
-        if cr == dr && cc == dc {return true}
-        
+        // logic
+        maze[r][c] = 2 // mark it visited
         for _, dir := range dirs {
-            nr := cr + dir[0]
-            nc := cc + dir[1]
+            nr := r + dir[0]
+            nc := c + dir[1]
             for nr >= 0 && nr < m && nc >= 0 && nc < n && maze[nr][nc] != 1 {
                 nr += dir[0]
                 nc += dir[1]
             }
-            
             nr -= dir[0]
             nc -= dir[1]
-            
-            if maze[nr][nc] != 2 {
-                if nr == dr && nc == dc {return true}
-                q = append(q, []int{nr,nc})
-                maze[nr][nc] = 2
+            reached := dfs(nr, nc)
+            if reached {
+                return true
             }
         }
+        return false
     }
-    return false
-    
+    return dfs(sr, sc)
 }
