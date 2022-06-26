d15965459a964c9f5a7f4bf60bba6227e327150d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-25 21:02:26 -0400

    Time: 8 ms (65.46%), Space: 4.2 MB (77.38%) - LeetHub

--- 733-flood-fill/733-flood-fill.go
+++ 733-flood-fill/733-flood-fill.go
@@ -1,69 +1,120 @@
 func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
+    m := len(image)
+    n := len(image[0])
     oldColor := image[sr][sc]
     if oldColor == newColor {return image}
     dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
-    m := len(image)
-    n := len(image[0])
     
-    q := new(queue)
-    image[sr][sc] = newColor
-    q.enqueue(sr,sc)
-
-    for !q.isEmpty() {
-        r,c := q.dequeue()
-        for _, dir := range dirs {
-            nr := r+dir[0]
-            nc := c+dir[1]
-            if nr >= 0 && nr < m && nc >=0 && nc < n && image[nr][nc] == oldColor {
-                image[nr][nc] = newColor
-                q.enqueue(nr,nc)
-            }
+    var dfs func(r, c int)
+    dfs = func(r, c int) {
+        // base
+        if r < 0 || r == m || c < 0 || c == n || image[r][c] != oldColor {
+            return
         }
+        
+        // logic
+        image[r][c] = newColor
+        for _, dir := range dirs{
+            dfs(r+dir[0],c+dir[1])
+        }
+        
     }
-    
+    dfs(sr, sc)
     return image
-}
-
-
-type listNode struct {
-    r,c int
-    next *listNode
-}
-type queue struct {
-    head *listNode
-    tail *listNode
-}
-
-
-func (q *queue) isEmpty() bool {
-    return q.head == nil
-}
-func (q *queue) enqueue(r,c int) {
-    newNode := &listNode{r:r,c:c}
-    if q.head == nil {
-        q.head = newNode
-        q.tail = newNode
-        return
-    }
-    q.tail.next = newNode
-    q.tail = newNode
-}
-
-func (q *queue) dequeue() (int,int) {
-    if q.head == nil {
-        return -1,-1
-    }
-    r := q.head.r
-    c := q.head.c
     
-    if q.head.next == nil {
-        q.head = nil
-        q.tail = nil
-        return r,c
-    }
-    
-    newHead := q.head.next
-    q.head.next = nil 
-    q.head = newHead
-    return r,c
 }
+
+
+/*
+    Connected components  == BFS/DFS
+    - We need starting point(s) -- all independant nodes 
+    There is a domino effect of changing 1 cell color to new color 
+    """
+        plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, 
+        plus any pixels connected 4-directionally to those pixels (also with the same color), and so on.
+    """
+    
+    approach : using BFS
+    - We are given a starting point ( sr, sc )
+    - Enqueue that starting
+    - Save the color we are changing from ( oldColor, so we can compare with its neighbors )
+    - Change the sr,sc to new color
+    - Start processing the queue
+    - As soon as we add an item to our queue, we will change the color right away to newColor
+    - So that we do not visit this node again
+    - if we are not allowed to modify the matrix ( save the visited poins in a set )
+    time: o(mn)
+    space: o(mn) - for the queue
+    
+
+*/
+
+// func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
+//     oldColor := image[sr][sc]
+//     if oldColor == newColor {return image}
+//     dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
+//     m := len(image)
+//     n := len(image[0])
+    
+//     q := new(queue)
+//     image[sr][sc] = newColor
+//     q.enqueue(sr,sc)
+
+//     for !q.isEmpty() {
+//         r,c := q.dequeue()
+//         for _, dir := range dirs {
+//             nr := r+dir[0]
+//             nc := c+dir[1]
+//             if nr >= 0 && nr < m && nc >=0 && nc < n && image[nr][nc] == oldColor {
+//                 image[nr][nc] = newColor
+//                 q.enqueue(nr,nc)
+//             }
+//         }
+//     }
+    
+//     return image
+// }
+
+
+// type listNode struct {
+//     r,c int
+//     next *listNode
+// }
+// type queue struct {
+//     head *listNode
+//     tail *listNode
+// }
+
+
+// func (q *queue) isEmpty() bool {
+//     return q.head == nil
+// }
+// func (q *queue) enqueue(r,c int) {
+//     newNode := &listNode{r:r,c:c}
+//     if q.head == nil {
+//         q.head = newNode
+//         q.tail = newNode
+//         return
+//     }
+//     q.tail.next = newNode
+//     q.tail = newNode
+// }
+
+// func (q *queue) dequeue() (int,int) {
+//     if q.head == nil {
+//         return -1,-1
+//     }
+//     r := q.head.r
+//     c := q.head.c
+    
+//     if q.head.next == nil {
+//         q.head = nil
+//         q.tail = nil
+//         return r,c
+//     }
+    
+//     newHead := q.head.next
+//     q.head.next = nil 
+//     q.head = newHead
+//     return r,c
+// }
