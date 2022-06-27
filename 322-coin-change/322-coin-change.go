234c58fe67c8aacdfeacc1487561899cee3d2392
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-27 13:05:32 -0400

    Time: 33 ms (37.50%), Space: 6.4 MB (71.48%) - LeetHub

--- 322-coin-change/322-coin-change.go
+++ 322-coin-change/322-coin-change.go
@@ -2,28 +2,22 @@ func coinChange(coins []int, amount int) int {
     m := len(coins)
     n := amount
     
-    dp := make([][]int, m+1)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n+1)
+    dp := make([]int, n+1)
+    for j := 1; j < len(dp); j++ {
+        dp[j] = amount+1
     }
     
-    for j := 1; j < len(dp[0]); j++ {
-        dp[0][j] = amount+1
-    }
-    
-    for i := 1; i < len(dp); i++ {
-        for j := 1; j < len(dp[0]); j++ {
+    for i := 1; i <= m; i++ {
+        for j := 1; j <= amount; j++ {
             coinVal := coins[i-1]
             am := j
-            if coinVal > am {
-                dp[i][j] = dp[i-1][j]
-            } else {
-                dp[i][j] = int(math.Min(float64(dp[i-1][j]), float64(dp[i][j-coinVal]+1)))
+            if coinVal <= am {
+                dp[j] = int(math.Min(float64(dp[j]), float64(dp[j-coinVal]+1)))
             }
         }
     }
     
-    out := dp[m][n]
+    out := dp[n]
     if out == amount+1 {return -1}
     return out
     
