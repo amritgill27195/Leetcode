77e956cbe6fdd1086cf825415be470b45ce740d3
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-27 15:01:54 -0400

    Time: 3 ms (28.28%), Space: 2 MB (83.79%) - LeetHub

--- 198-house-robber/198-house-robber.go
+++ 198-house-robber/198-house-robber.go
@@ -1,15 +1,17 @@
 func rob(nums []int) int {
-    m := len(nums)+1
     n := 2
-    dp := make([][]int, m)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n)
-    }
-    for i := len(dp)-2; i >= 0; i-- { // house val
-        dp[i][0] = int(math.Max(float64(dp[i+1][0]), float64(dp[i+1][1])))
-        dp[i][1] = nums[i] + dp[i+1][0]
+    dp := make([]int, n)
+
+    for i := len(nums)-1; i >= 0; i-- {
+        houseVal := nums[i]
+        zeroCase := dp[0]
+        oneCase := dp[1]
+        
+        dp[0] = int(math.Max(float64(zeroCase), float64(oneCase)))
+        dp[1] = zeroCase + houseVal
+        
     }
     
-    return int(math.Max(float64(dp[0][0]),float64(dp[0][1])))
+    return int(math.Max(float64(dp[0]),float64(dp[1])))
     
 }
