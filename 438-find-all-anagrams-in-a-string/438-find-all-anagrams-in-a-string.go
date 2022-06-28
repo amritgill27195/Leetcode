9d076bd078d98fed691ec2f0cc11c630fb8836f5
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-27 20:26:39 -0400

    Time: 47 ms (36.89%), Space: 5.2 MB (58.84%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.go
@@ -0,0 +1,30 @@
+func findAnagrams(s string, p string) []int {
+    left := 0
+    wState := map[byte]int{}
+    pMap := map[byte]int{}
+    for i := 0; i < len(p); i++ {
+        pMap[p[i]]++
+    }
+    out := []int{}
+    for right := 0; right < len(s); right++ {
+        wState[s[right]]++
+        if right-left+1 == len(p) {
+            wStateIsAnagram := true
+            for k, v := range wState {
+                pMapVal, exists := pMap[k]
+                if !exists || pMapVal != v {
+                    wStateIsAnagram = false
+                    break
+                } 
+            }
+            if wStateIsAnagram {
+                out = append(out, left)
+            }
+            leftChar := s[left]
+            wState[leftChar]--
+            if wState[leftChar] == 0 {delete(wState, leftChar)}
+            left++
+        }
+    }
+    return out
+}
