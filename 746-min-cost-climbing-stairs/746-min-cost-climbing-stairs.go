d11b2c01dabc16ce58225f8cb5f4a5da51eefdcb
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-28 12:58:55 -0400

    Time: 11 ms (14.74%), Space: 3 MB (45.47%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.go
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.go
@@ -1,3 +1,41 @@
+/*
+    We have to find the min cost to climb all the stairs
+    - Stairs in this example is an array
+    - Bottom of the stair is idx 0 and top of the stair is past the last element
+    - We can either take 1 step or 2 steps
+        - NOTICE : WE ARE GIVEN CHOICES - DP hint
+    - And use a step in of the stairs has a cost
+    - And we have to strategically climb all the stairs while minimizing cost to go all the way up
+        - NOTICE : Asking "minimum" -- something optimal - another DP hint
+    - Which likely means there are many ways to go about climbing these stairs
+    - However we also have 1 extra choice
+        - We can either start from the bottom of the stairs ( idx 0 )
+        - Or we can start from idx 1 ( i.e we do not have to count in the cost of idx 0 step )
+        - whichever costs less
+    
+    - After drawing the decision tree, you will notice repeated subproblems
+    - Which is a clear indication of dp
+    - We can start by solving the smallest subproblem from bottom up
+    - Which means, we can start from the top of the stairs, and work back down
+    - At each stair step, we have 2 choices
+        - take 1 step
+        - take 2 steps
+    - Which ever one results in smaller cost, we will pick that ofc - since we are trying to minimize the cost
+    - So for each element from the back of the costs array ( 2nd last element , since last element cost would not be different if we take 1 or 2 step )
+        - We will check whether
+            - is it cheaper to take 1 step ( currentCost + nextStepCost )
+            - is it cheaper to take 2 step ( currentCost + 2StepsNextCost ) -- if 2 steps goes out of bounds, we can assume 0 for 2 steps price
+        - Which ever one is cheaper we will set that price for that step
+    - And work back to the front of the array
+    - When we get to the first step ( 0th idx ) - we have more choices here to make
+            - is it cheaper to take 1 step ( currentCost + nextStepCost )
+            - is it cheaper to take 2 step ( currentCost + 2StepsNextCost ) -- if 2 steps goes out of bounds, we can assume 0 for 2 steps price
+            - or is cheaper to skip this step: min(nextStepCost, min(nextStepCost,2NextStepCost)+currentCost )
+    
+    - Finally at the end we have bubbled our answer up in the 0th idx
+    - We took the cheapest decision on each step by evaluation whether its better to take 1 or 2 steps - from the back of the array 
+    - We essentially solved the smallest subproblem for each step - and worked up to bubble up the final ans.
+*/
 func minCostClimbingStairs(cost []int) int {
     dp := make([]int, len(cost))
     dp[len(dp)-1] = cost[len(cost)-1]
