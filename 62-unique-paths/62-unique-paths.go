2d4da8e2a9693bb05e33884bfc05ccc476875990
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-28 13:33:55 -0400

    Time: 0 ms (100.00%), Space: 2 MB (76.92%) - LeetHub

--- 62-unique-paths/62-unique-paths.go
+++ 62-unique-paths/62-unique-paths.go
@@ -1,14 +1,10 @@
 func uniquePaths(m int, n int) int {
-    dp := make([][]int, m+1)
-    for idx, _ := range dp {
-        dp[idx] = make([]int, n+1)
-    }
-    dp[m-1][n-1] = 1
+    dp := make([]int, n+1)
+    dp[n-1] = 1
     for i := m-1; i >= 0; i-- {
         for j := n-1; j >= 0; j-- {
-            if i == m-1 && j == n-1 {continue}
-            dp[i][j] = dp[i+1][j] + dp[i][j+1]
+            dp[j] = dp[j] + dp[j+1]
         }
     }
-    return dp[0][0]
+    return dp[0]
 }
