f03cb2ef23fba8000ec6c7cece4e20972217505d
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-06-28 12:05:44 -0400

    Time: 5 ms (65.54%), Space: 3 MB (45.47%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.go
@@ -0,0 +1,26 @@
+func minCostClimbingStairs(cost []int) int {
+    dp := make([]int, len(cost))
+    dp[len(dp)-1] = cost[len(cost)-1]
+    
+    for i := len(cost)-2; i >= 0; i-- {
+        oneStepCost := dp[i+1]
+        twoStepCost := 0
+        if i+2 < len(cost) {
+            twoStepCost = dp[i+2]
+        }
+        currentCost := cost[i]
+        
+        if i == 0 {
+            chooseMin := min(oneStepCost,twoStepCost)+currentCost
+            dp[i] =  min(oneStepCost,chooseMin )
+        } else {
+            dp[i] = int(math.Min(float64(oneStepCost), float64(twoStepCost)))+currentCost
+        }
+    }
+    return dp[0]
+}
+
+func min(x, y int) int{
+    if x < y {return x}
+    return y
+}
