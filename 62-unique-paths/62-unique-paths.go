bc46cff4987f9bf1016df66eab95ef27cd3bbdf6
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-01 14:12:16 -0400

    Time: 0 ms (100.00%), Space: 1.9 MB (77.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.go
+++ 62-unique-paths/62-unique-paths.go
@@ -37,6 +37,8 @@
 /*
     approach: bottom up dp
     - same thing as above but using 1D array
+    time: o(mn)
+    space: o(n)
 */
 func uniquePaths(m int, n int) int {
     dp := make([]int, n+1)
@@ -49,3 +51,22 @@ func uniquePaths(m int, n int) int {
     }
     return dp[len(dp)-1]
 }
+
+
+// top down
+// time and space: o(mn)
+// func uniquePaths(m int, n int) int {
+//     dp := make([][]int, m+1)
+//     for idx, _ := range dp {
+//         dp[idx] = make([]int, n+1)
+//     }
+//     // there is only 1 way to get out from the last cell ( which is right ) therefore only 1 way
+//     dp[m-1][n-1] = 1
+//     for i := m-1; i >= 0; i-- {
+//         for j := n-1; j >= 0; j-- {
+//             if i == m-1 && j == n-1 {continue}
+//             dp[i][j] = dp[i+1][j] + dp[i][j+1]
+//         }
+//     }
+//     return dp[0][0]
+// }
