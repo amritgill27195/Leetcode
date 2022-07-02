f9e80ce379ddf57e81fd7b8432bd18704076dff3
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-02 13:52:14 -0400

    Time: 8 ms (66.67%), Space: 2.9 MB (11.11%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.go
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.go
@@ -1,19 +1,25 @@
+// time: o(nk)
+// space: o(n)
 func maxSumAfterPartitioning(arr []int, k int) int {
-    
     n := len(arr)
     dp := make([]int, n)
     dp[0] = arr[0]
     
-    for i := 1; i < len(dp); i++ {
-        partMax := arr[i]
-        for j := i; j >= i-k+1 && j >= 0; j-- {
-            partMax = max(partMax,arr[j])
+    for i := 1; i < n; i++ {
+        
+        maxAfterKChoices := math.MinInt64
+        maxInPart := math.MinInt64
+        for j := i; j > i-k && j >= 0 ; j-- {
+            if arr[j] > maxInPart { maxInPart = arr[j] }
+            partSize := i-j+1
+            partitionSum := maxInPart * partSize
             if j > 0 {
-                dp[i] = max(dp[i], partMax*(i-j+1)+dp[j-1])    
-            } else {
-                dp[i] = max(dp[i], partMax*(i-j+1))
+                partitionSum += dp[j-1]
             }
+            maxAfterKChoices = max(partitionSum, maxAfterKChoices)
         }
+        
+        dp[i] = maxAfterKChoices
     }
     
     return dp[len(dp)-1]
