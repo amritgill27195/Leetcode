e3cfb3efaa9f3f8595faea565467d77cd3c1a2c8
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-03 14:40:19 -0400

    Time: 3 ms (17.60%), Space: 1.8 MB (90.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.go
+++ 70-climbing-stairs/70-climbing-stairs.go
@@ -1,3 +1,5 @@
+// time : o(n)
+// space : o(n)
 // func climbStairs(n int) int {
     
 //     dp := make([]int, n+1)
@@ -14,7 +16,15 @@
 
 
 
-// we only need two vars
+// we only need previous values
+// how would we store the new total and where?
+// well it turns out that the new total is the new twoStepsBack value
+// but first promote the twoStepBack value to 1 step back position
+// and then store the new total in twoStepBack position
+// This way our space is constant
+
+// time : o(n)
+// space : o(1)
 func climbStairs(n int) int {
     
     dp := []int{1,1}
