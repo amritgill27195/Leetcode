a6f028bac03439987ce581fe34bdb3584a3bdd0e
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-03 14:22:30 -0400

    Time: 16 ms (33.00%), Space: 4 MB (64.25%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.go
+++ 64-minimum-path-sum/64-minimum-path-sum.go
@@ -2,11 +2,27 @@ func minPathSum(grid [][]int) int {
     m := len(grid)
     n := len(grid[0])
     
-    for i := 1; i < m; i++ { grid[i][0] += grid[i-1][0]}
-    for j := 1; j < n; j++ { grid[0][j] += grid[0][j-1]}
+    // for i := 1; i < m; i++ { grid[i][0] += grid[i-1][0]}
+    // for j := 1; j < n; j++ { grid[0][j] += grid[0][j-1]}
     
-    for i := 1; i < m; i++ {
-        for j := 1; j < n; j++ {
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if i == 0 {
+                jVal := 0
+                if j-1 >= 0 {jVal = grid[i][j-1]}
+                grid[i][j] += jVal
+                continue
+            }
+            
+            if j == 0 {
+                iVal := 0
+                if i-1 >= 0 {
+                    iVal = grid[i-1][j]
+                }
+                grid[i][j] += iVal
+                continue
+            }
+            
             grid[i][j] += min(grid[i-1][j], grid[i][j-1])  
         }
     }
