49a6b562c948352c40aa2d5024eda450ee542ff3
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-03 14:38:16 -0400

    Time: 1 ms (56.29%), Space: 1.9 MB (90.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.go
+++ 70-climbing-stairs/70-climbing-stairs.go
@@ -1,11 +1,29 @@
+// func climbStairs(n int) int {
+    
+//     dp := make([]int, n+1)
+//     dp[0] = 1
+//     dp[1] = 1
+    
+//     for i := 2; i < len(dp); i++ {
+//         dp[i] = dp[i-1] + dp[i-2]
+//     }
+    
+//     return dp[len(dp)-1]
+    
+// }
+
+
+
+// we only need two vars
 func climbStairs(n int) int {
     
-    dp := make([]int, n+1)
-    dp[0] = 1
-    dp[1] = 1
+    dp := []int{1,1}
     
-    for i := 2; i < len(dp); i++ {
-        dp[i] = dp[i-1] + dp[i-2]
+    for i := 2; i <= n; i++ {
+        oneStepBack := dp[0]
+        twoStepBack := dp[1]
+        dp[0] = twoStepBack
+        dp[1] = oneStepBack+twoStepBack
     }
     
     return dp[len(dp)-1]
