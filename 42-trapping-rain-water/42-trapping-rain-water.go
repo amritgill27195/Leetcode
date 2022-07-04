7f4553e72514d76e1bfc836a1e9b0bbf4484b958
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-04 13:46:57 -0400

    Time: 11 ms (81.60%), Space: 5.2 MB (77.72%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.go
+++ 42-trapping-rain-water/42-trapping-rain-water.go
@@ -1,4 +1,23 @@
 /*
+    intuition:
+    - The idea is to use 2 pointers ( slow and fast )
+    - slow starts at idx 0 and fast starts at 1
+    - the idea is that the slow pointer acts as the left wall
+    - If the fast pointer wall is less than slow pointer wall, we can trap water ( slow-fast height diff )
+    - If the wall on the right side/fast-ptr becomes >= slow ptr wall, we can no longer use slow ptr as a left wall, therefore we must save what we have 
+    - and move the slow ptr to the new left wall, i.e where fast ptr is
+    - But what if the walls are in descending order, i.e they are always going down from left to right
+    - then slow ptr will always be greater than fast ptr height, so we will trap but will never move the slow ptr to a new position
+    - therefore we will never really save the accumulated trapped
+    - Could we have a test case where this would fail?
+    - Yes, when there is a 1 peek that is largest and on the right side of the peek, we next heights may never be >= the peek height
+    - But could have little waters after the peek that can be trapped but never saved
+    - In this case we will run a loop from the back ( 2nd pass )
+    - Until the peek ( which will be the slow ptr )
+    - And new slow ptr will become the right wall
+    - And fast ptr will start from 2nd last position.
+    
+
     approach: 2 pass using 2 pointers ( slow and fast ptrs )
     - We want to position our 2 pointers such that we know we can trap some water
     - fast pointer will always move
