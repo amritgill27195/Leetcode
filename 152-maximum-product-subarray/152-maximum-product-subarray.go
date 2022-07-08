0d6d6767c7d914e7c8ce0fae2580f46f506e00c1
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-08 19:05:15 -0400

    Time: 0 ms (100.00%), Space: 3.3 MB (100.00%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.go
+++ 152-maximum-product-subarray/152-maximum-product-subarray.go
@@ -1,15 +1,31 @@
-func maxProduct(nums []int) int {
-    dp := make([][]int, len(nums))
-    for idx, _ := range dp {
-        dp[idx] = make([]int, 2)
-    }
-    dp[0] = []int{nums[0], nums[0]}
+// func maxProduct(nums []int) int {
+//     dp := make([][]int, len(nums))
+//     for idx, _ := range dp {
+//         dp[idx] = make([]int, 2)
+//     }
+//     dp[0] = []int{nums[0], nums[0]}
     
+//     maxProd := nums[0]
+//     for i := 1; i < len(nums); i++ {
+//         dp[i][0] = max(nums[i], max(nums[i] * dp[i-1][0], nums[i] * dp[i-1][1]))
+//         dp[i][1] = min(nums[i], min(nums[i] * dp[i-1][0], nums[i] * dp[i-1][1]))
+//         maxProd = max(maxProd, max(dp[i][0], dp[i][1]))
+//     }
+    
+//     return maxProd
+// }
+func maxProduct(nums []int) int {
+    dp := make([]int, 2)
+    dp[0] = nums[0]
+    dp[1] = nums[0]
     maxProd := nums[0]
+    
     for i := 1; i < len(nums); i++ {
-        dp[i][0] = max(nums[i], max(nums[i] * dp[i-1][0], nums[i] * dp[i-1][1]))
-        dp[i][1] = min(nums[i], min(nums[i] * dp[i-1][0], nums[i] * dp[i-1][1]))
-        maxProd = max(maxProd, max(dp[i][0], dp[i][1]))
+        currMax := dp[0]
+        currMin := dp[1]
+        dp[0] = max(nums[i], max(nums[i] * currMax, nums[i] * currMin))
+        dp[1] = min(nums[i], min(nums[i] * currMax, nums[i] * currMin))
+        maxProd = max(maxProd, max(dp[0], dp[1]))
     }
     
     return maxProd
