3fc3c009bbc702bd70e0f5822a119041c60df73c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-09 12:28:06 -0400

    Time: 222 ms (15.00%), Space: 2 MB (90.00%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.go
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.go
@@ -1,35 +1,35 @@
 func getMaximumGold(grid [][]int) int {
+    maxGold := 0
     m := len(grid)
     n := len(grid[0])
-    max := 0
-    var backtrack func(r, c int, total int) 
-    dirs := [][]int{{-1,0},{1,0},{0,-1},{0,1}}
-    backtrack = func(r, c int, total int) {
+    dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
+    var dfs func(sum, r, c int) 
+    dfs = func(sum, r, c int) {
         // base
-        if r == m || r < 0 || c == n || c < 0 || grid[r][c] <= 0 {
+        if r < 0 || r == m || c < 0 || c == n || grid[r][c] <= 0 {
             return
         }
-        
+        sum += grid[r][c]
+        if sum > maxGold {maxGold = sum}
+        grid[r][c] *= -1
         // logic
-        total += grid[r][c]
-        // action
-        grid[r][c] *= -1
-        if total > max {max = total}
         for _, dir := range dirs {
-            // recurse
-            backtrack(r+dir[0], c+dir[1], total)
+            dfs(sum, r+dir[0], c+dir[1])
         }
-        // backtrack
-        grid[r][c] *= -1
-        
+        grid[r][c] *= -1        
     }
     
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
+    for i := 0;i < m; i++ {
+        for j := 0;j < n; j++ {
             if grid[i][j] != 0 {
-                backtrack(i,j,0)
+                dfs(0,i, j)
             }
         }
     }
-    return max
+    return maxGold
+}
+
+func max(x, y int) int {
+    if x > y {return x}
+    return y
 }
