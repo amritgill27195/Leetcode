7c94efdc51969702b70e75bd31339600c6cf0d49
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-09 09:38:05 -0400

    Time: 142 ms (5.06%), Space: 8.3 MB (5.62%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.go
+++ 784-letter-case-permutation/784-letter-case-permutation.go
@@ -1,25 +1,28 @@
-
 func letterCasePermutation(s string) []string {
     var result []string
-    var backtrack func(i int, path string)
-    backtrack = func(i int, path string)  {
+    var dfs func(start int, path string)
+    dfs = func(start int, path string) {
+        // fmt.Println("Path: ", path)
         // base
-        if i == len(s) {
-            result = append(result, path)
+        if start == len(s) {
+            if len(path) == len(s) {
+                result = append(result, path)
+            }
             return
         }
         
-        
-        // logic
-        char := s[i]
-        strChar := string(char)
-        if char >= '0' && char <= '9' {
-            backtrack(i+1, path+strChar)
-        } else {
-            backtrack(i+1, path+strings.ToUpper(strChar))
-            backtrack(i+1, path+strings.ToLower(strChar))
+        //logic
+        for i := start; i < len(s); i++ {
+            char := s[i]
+            strChar := string(char)
+            if char >= '0' && char <= '9' {
+                dfs(i+1, path+strChar)
+            } else {
+                dfs(i+1, path+strings.ToLower(strChar))
+                dfs(i+1, path+strings.ToUpper(strChar))
+            }
         }
     }
-    backtrack(0, "")
+    dfs(0, "")
     return result
 }
