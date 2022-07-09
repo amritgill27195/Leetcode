0852eab5b47a985e0939550cfcacb0206c8d8e1b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-09 12:05:44 -0400

    Time: 3 ms (30.75%), Space: 2.1 MB (15.79%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.go
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.go
@@ -1,35 +1,32 @@
 func letterCombinations(digits string) []string {
-    if len(digits) == 0 {return nil}
-    charsMap := map[byte][]string{
-        '2' : []string{"a","b","c"},
-        '3' : []string{"d","e","f"},
-        '4' : []string{"g","h","i"},
-        '5' : []string{"j","k","l"},
-        '6' : []string{"m","n","o"},
-        '7' : []string{"p","q","r", "s"},
-        '8' : []string{"t","u","v"},
-        '9' : []string{"w","x","y", "z"},
-    }
+    if len(digits) == 0 {return nil} 
     result := []string{}
-    var backtrack func(start int, path string)
-    backtrack = func(start int, path string) {
+    digMap := map[byte][]string{
+        '1':[]string{},
+        '2':[]string{"a","b", "c"},
+        '3':[]string{"d","e", "f"},
+        '4':[]string{"g","h", "i"},
+        '5':[]string{"j","k", "l"},
+        '6':[]string{"m","n", "o"},
+        '7':[]string{"p","q", "r", "s"},
+        '8':[]string{"t","u", "v"},
+        '9':[]string{"w","x", "y", "z"},
+    }
+    var dfs func(path string, start int)
+    dfs = func(path string, start int) {
         // base
         if start == len(digits) {
             result = append(result, path)
             return
         }
         
+        
         // logic
-        chars := charsMap[digits[start]]
-        for j := 0; j < len(chars); j++ {
-            // action
-            path += chars[j]
-            // recurse
-            backtrack(start+1, path)
-            // backtrack
-            path = path[:len(path)-1]
+        chars := digMap[digits[start]]
+        for i := 0; i < len(chars); i++ {
+            dfs(path+chars[i], start+1)
         }
     }
-    backtrack(0, "")
+    dfs("", 0)
     return result
 }
