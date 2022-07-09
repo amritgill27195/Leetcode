8b6ce6d4fed459923a70c428e72a7c437ef491c5
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-09 09:38:43 -0400

    Time: 2 ms (99.44%), Space: 6.3 MB (96.07%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.go
+++ 784-letter-case-permutation/784-letter-case-permutation.go
@@ -1,28 +1,54 @@
+// func letterCasePermutation(s string) []string {
+//     var result []string
+//     var dfs func(start int, path string)
+//     dfs = func(start int, path string) {
+//         // fmt.Println("Path: ", path)
+//         // base
+//         if start == len(s) {
+//             if len(path) == len(s) {
+//                 result = append(result, path)
+//             }
+//             return
+//         }
+        
+//         //logic
+//         for i := start; i < len(s); i++ {
+//             char := s[i]
+//             strChar := string(char)
+//             if char >= '0' && char <= '9' {
+//                 dfs(i+1, path+strChar)
+//             } else {
+//                 dfs(i+1, path+strings.ToLower(strChar))
+//                 dfs(i+1, path+strings.ToUpper(strChar))
+//             }
+//         }
+//     }
+//     dfs(0, "")
+//     return result
+// }
+
+
 func letterCasePermutation(s string) []string {
     var result []string
-    var dfs func(start int, path string)
-    dfs = func(start int, path string) {
-        // fmt.Println("Path: ", path)
+    var backtrack func(i int, path string)
+    backtrack = func(i int, path string)  {
         // base
-        if start == len(s) {
-            if len(path) == len(s) {
-                result = append(result, path)
-            }
+        if i == len(s) {
+            result = append(result, path)
             return
         }
         
-        //logic
-        for i := start; i < len(s); i++ {
-            char := s[i]
-            strChar := string(char)
-            if char >= '0' && char <= '9' {
-                dfs(i+1, path+strChar)
-            } else {
-                dfs(i+1, path+strings.ToLower(strChar))
-                dfs(i+1, path+strings.ToUpper(strChar))
-            }
+        
+        // logic
+        char := s[i]
+        strChar := string(char)
+        if char >= '0' && char <= '9' {
+            backtrack(i+1, path+strChar)
+        } else {
+            backtrack(i+1, path+strings.ToUpper(strChar))
+            backtrack(i+1, path+strings.ToLower(strChar))
         }
     }
-    dfs(0, "")
+    backtrack(0, "")
     return result
 }
