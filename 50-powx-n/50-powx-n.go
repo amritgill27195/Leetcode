73435dad7adf253db50929e4c5d8a49851e0852f
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-12 16:29:55 -0400

    Time: 0 ms (100.00%), Space: 2 MB (19.05%) - LeetHub

--- 50-powx-n/50-powx-n.go
+++ 50-powx-n/50-powx-n.go
@@ -1,48 +1,22 @@
-// func myPow(x float64, n int) float64 {
-//     if n < 0 {
-//         n *= -1
-//         x = 1/x
-//     }
-//     var dfs func(base float64, exp int) float64
-//     dfs = func(base float64, exp int) float64 {
-//         // base
-//         if exp == 1 {
-//             return base
-//         }
-//         if exp == 0 {return 1}
-        
-//         // logic
-//         if exp % 2 == 0 {
-//             return dfs(base, exp/2) * dfs(base, exp/2)   
-//         }
-//         return base * dfs(base, exp/2) * dfs(base, exp/2)
-//     }
-//     return dfs(x, n)
-    
-// }
-
 func myPow(x float64, n int) float64 {
-    // if n is negative 
-    // make it positive and make x = 1/x
     if n < 0 {
         n *= -1
         x = 1/x
     }
-    
     var dfs func(base float64, exp int) float64
     dfs = func(base float64, exp int) float64 {
         // base
-        if exp == 0 {
-            return 1
+        if exp == 1 {
+            return base
         }
+        if exp == 0 {return 1}
         
         // logic
         result := dfs(base, exp/2)
         if exp % 2 == 0 {
             return result * result
-        } else {
-            return result * result * base
         }
+        return base * result * result
     }
     return dfs(x, n)
 }
