6de8c969a643e18cccd6fae0a06d7d5aa064ea7f
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-12 20:57:06 -0400

    Time: 0 ms (100.00%), Space: 2.3 MB (36.84%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.go
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.go
@@ -1,3 +1,19 @@
+/*
+    approach: explore all paths (for loop based recursion with backtracking)
+    - We will use a freqMap that contains 0 and 1 as our keys and values would be n where n is the len of nums array
+    - n because all binary strings within array are also of len n
+    - and we also have to look for a uniq binary string of size n
+    - if n is the len of binary string we have to return
+    - it means that we have n 1's and n 0's
+    - if we used an array, than we will end up generating duplicate permutations - which would work too because we are checking if this permutation of binary string
+    - exists in nums array or not
+    - Which also means that we should convert our nums array to something that faster to search = so a map.
+    - We will use backtracking to form every single permutation of the binary strings of len n and once we have a path that does not exist in nums map, return that path
+    - otherwise keep being exhaustive and explore all permutations
+    
+    time: o(2^n)
+    space: o(n)
+*/
 func findDifferentBinaryString(nums []string) string {
     n := len(nums)
     
