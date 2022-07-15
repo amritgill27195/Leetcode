6ffb29f7d9017b44d8a621942b452d14d1b7bd2a
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-14 22:20:39 -0400

    Time: 4 ms (100.00%), Space: 4.8 MB (31.43%) - LeetHub

---
+++ 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.go
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func smallestFromLeaf(root *TreeNode) string {
+    min := "-"
+    var dfs func(r *TreeNode, path string)
+    dfs = func(r *TreeNode, path string) {
+        // base
+        if r == nil {
+            return
+        }
+        //logic
+
+        path += string('a'+r.Val)
+
+        dfs(r.Left, path)
+        if r.Left == nil && r.Right == nil {
+            //// The result will be 0 if a == b, -1 if a < b, and +1 if a > b.
+            // strings.Compare(a,b)
+            revPath := reverse(path)
+            if min == "-" {
+                min = revPath
+            }else if val := strings.Compare(min, revPath); val == 1 {
+                min = revPath
+            }
+        }
+        dfs(r.Right, path)
+    }
+    dfs(root, "")
+    return min
+}
+
+func reverse(s string) string {
+    newS := new(strings.Builder)
+    for i := len(s)-1; i >= 0; i-- {
+        newS.WriteByte(s[i])
+    }
+    return newS.String()
+} 
