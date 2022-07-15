9394efa995475b52c84f409c91620ecb686cb9e8
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-15 13:21:41 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (83.87%) - LeetHub

--- 161-one-edit-distance/161-one-edit-distance.go
+++ 161-one-edit-distance/161-one-edit-distance.go
@@ -11,30 +11,29 @@ func isOneEditDistance(s string, t string) bool {
     for sPtr < len(s) && tPtr < len(t) {
         sVal := s[sPtr]
         tVal := t[tPtr]
+        
         if sVal == tVal {
             sPtr++; tPtr++
-        } else if len(s) < len(t) {
-            if insert {
-                insert = false
-                tPtr++
-                continue
-            }
-            return false
-        } else if len(s) > len(t) {
-            if del {
-                del = false
-                sPtr++;
-                continue
-            }
-            return false
-        } else {
-            if update {
-                update = false
-                sPtr++; tPtr++
-                continue
-            }
-            return false
+            continue
+        } 
+        
+        // update
+        if len(s) == len(t) {
+            if update {update = false; sPtr++; tPtr++} else {return false}
+            continue
         }
+        
+        // insert 
+        if len(s) < len(t) {
+            if insert {insert = false; tPtr++;} else {return false}
+            continue
+        }
+        
+        // delete
+        if len(s) > len(t) {
+            if del {del = false; sPtr++} else {return false}
+        }
+        
     }
     for sPtr < len(s) {
         if del {
