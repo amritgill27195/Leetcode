ae26a79389f7a255ae355fc0a85611a60c0eb836
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-14 21:58:25 -0400

    Time: 10 ms (5.08%), Space: 2.3 MB (62.44%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.go
+++ 257-binary-tree-paths/257-binary-tree-paths.go
@@ -6,48 +6,25 @@
  *     Right *TreeNode
  * }
  */
-
-
-/*
-    approach : using inorder 
-    
-    why inorder?
-    - Because inorder goes Left -> Root (Process) -> Right
-    - Which means, we will go down all the left first,
-    - Hit a left.nil base case and come back to the left leaf
-    - Then we can check if this node is a leaf node
-    - if it is , we will save its path
-    - how do we get its path?
-    - At each node, we will main a string "path"
-    - Once we hit leaf - we will add this string path to some output list
-    
-
-*/
 func binaryTreePaths(root *TreeNode) []string {
-    out := []string{}
-    inorderDfs(root, "", &out)
-    return out
-}
-
-
-func inorderDfs(root *TreeNode, paths string, out *[]string) {
-    
-    // base
-    if root == nil {
-        return
+    result := []string{}
+    var dfs func(path string, r *TreeNode)
+    dfs = func(path string, r *TreeNode) {
+        // base
+        if r == nil {
+            return
+        }
+        
+        // logic
+        path += fmt.Sprintf("%v",r.Val)
+        if r.Left == nil && r.Right == nil {
+            result = append(result, path)
+            return
+        }
+        if r.Left != nil || r.Right != nil {path+="->"}
+        dfs(path, r.Left)
+        dfs(path, r.Right)
     }
-    
-    // logic
-    paths += fmt.Sprintf("%v", root.Val)
-    if root.Left != nil || root.Right != nil {
-        paths += "->"
-    }
-    
-    
-    inorderDfs(root.Left, paths, out)
-    if root.Left == nil && root.Right == nil {
-        *out = append(*out, paths)
-    }
-    inorderDfs(root.Right, paths, out)
-    
+    dfs("", root)
+    return result
 }
