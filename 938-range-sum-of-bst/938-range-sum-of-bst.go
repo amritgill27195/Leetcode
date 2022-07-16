ec03c01ee15caf67d1ddd424722ca93c5da8fd03
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-16 19:13:09 -0400

    Time: 83 ms (87.25%), Space: 8.5 MB (88.05%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.go
+++ 938-range-sum-of-bst/938-range-sum-of-bst.go
@@ -6,12 +6,10 @@
  *     Right *TreeNode
  * }
  */
-
-
-// approach : traverse with bst constraints 
-// time: o(n) 
-// space: o(h)
 func rangeSumBST(root *TreeNode, low int, high int) int {
+    if root == nil {
+        return 0
+    }
     sum := 0
     var dfs func(r *TreeNode)
     dfs = func(r *TreeNode) {
@@ -19,96 +17,18 @@ func rangeSumBST(root *TreeNode, low int, high int) int {
         if r == nil {
             return
         }
-        // preorder
-        // if r.Val >= low && r.Val <= high {
-        //     sum+=r.Val
-        // }
         
+        // logic
         if r.Val > low {
             dfs(r.Left)
         }
-        
-        // inorder
-        if r.Val >= low && r.Val <= high {
-            sum+=r.Val
-        }
-        
         if r.Val < high {
             dfs(r.Right)
         }
-        
+        if r.Val >= low && r.Val <= high {
+            sum += r.Val
+        }
     }
     dfs(root)
     return sum
 }
-
-
-// plain postOrder traversal
-// time: o(n)
-// space: o(h)
-// func rangeSumBST(root *TreeNode, low int, high int) int {
-//     sum := 0
-//     var dfs func(r *TreeNode)
-//     dfs = func(r *TreeNode) {
-//         // base
-//         if r == nil {
-//             return
-//         }
-       
-//         // logic
-//         dfs(r.Left)
-//         dfs(r.Right)
-//         if r.Val >= low && r.Val <= high {
-//             sum+=r.Val
-//         }
-//     }
-//     dfs(root)
-//     return sum
-// }
-
-// plain preorder traversal
-// time: o(n)
-// space: o(h)
-// func rangeSumBST(root *TreeNode, low int, high int) int {
-//     sum := 0
-//     var dfs func(r *TreeNode)
-//     dfs = func(r *TreeNode) {
-//         // base
-//         if r == nil {
-//             return
-//         }
-       
-//         if r.Val >= low && r.Val <= high {
-//             sum+=r.Val
-//         }
-//         // logic
-//         dfs(r.Left)
-//         dfs(r.Right)
-//     }
-//     dfs(root)
-//     return sum
-// }
-
-
-// plain inorder traversal
-// time: o(n)
-// space: o(h)
-// func rangeSumBST(root *TreeNode, low int, high int) int {
-//     sum := 0
-//     var dfs func(r *TreeNode)
-//     dfs = func(r *TreeNode) {
-//         // base
-//         if r == nil {
-//             return
-//         }
-//         // logic
-//         dfs(r.Left)
-//         if r.Val >= low && r.Val <= high {
-//             sum+=r.Val
-//         }
-
-//         dfs(r.Right)
-//     }
-//     dfs(root)
-//     return sum
-// }
