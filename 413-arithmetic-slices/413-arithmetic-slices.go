55f880b94d71365419d0b138b3f797791f60c369
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-18 23:24:40 -0400

    Time: 6 ms (6.54%), Space: 2.3 MB (21.50%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.go
+++ 413-arithmetic-slices/413-arithmetic-slices.go
@@ -1,13 +1,11 @@
 func numberOfArithmeticSlices(nums []int) int {
     dp := make([]int, len(nums))
-    n := len(nums)
-    sum := 0
-    
-    for i := n-3; i >= 0; i-- {
-        if nums[i+1] - nums[i] == nums[i+2] - nums[i+1] {
-            dp[i] = dp[i+1] + 1
-            sum += dp[i]
+    count := 0
+    for i := 2; i < len(nums); i++ {
+        if nums[i]-nums[i-1] == nums[i-1]-nums[i-2] {
+            dp[i] = dp[i-1]+1
+            count += dp[i]
         }
     }
-    return sum
+    return count
 }
