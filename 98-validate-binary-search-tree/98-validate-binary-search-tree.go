be1c08ecc800c74630cc52d5207ec029ee2fe182
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-21 09:30:32 -0400

    Time: 18 ms (6.27%), Space: 5.4 MB (16.50%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.go
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.go
@@ -6,16 +6,46 @@
  *     Right *TreeNode
  * }
  */
+// func isValidBST(root *TreeNode) bool {
+//     var dfs func(min, max int, r *TreeNode) bool
+//     dfs = func(min, max int, r *TreeNode) bool {
+//         // base
+//         if r == nil {return true}
+//         // logic
+//         if r.Val <= min || r.Val >= max {
+//             return false
+//         }
+//         return dfs(min, r.Val, r.Left) && dfs(r.Val, max, r.Right)
+//     }
+//     return dfs(math.MinInt64, math.MaxInt64, root)
+// }
+
+
 func isValidBST(root *TreeNode) bool {
-    var dfs func(min, max int, r *TreeNode) bool
-    dfs = func(min, max int, r *TreeNode) bool {
-        // base
-        if r == nil {return true}
-        // logic
-        if r.Val <= min || r.Val >= max {
+    type sNode struct {
+        n *TreeNode
+        min int
+        max int
+    }
+    
+    // [ [*TreeNode, min, max] ]
+    s := []*sNode{ &sNode{n:root, min: math.MinInt64, max: math.MaxInt64} }
+
+    for len(s) != 0 {
+        top := s[len(s)-1]
+        s= s[:len(s)-1]
+        
+        node := top.n
+        min := top.min
+        max := top.max
+        
+        if node.Val <= min || node.Val >= max {
             return false
         }
-        return dfs(min, r.Val, r.Left) && dfs(r.Val, max, r.Right)
+        
+        if node.Left != nil { s = append(s, &sNode{n: node.Left, min: min, max: node.Val})}
+        if node.Right != nil { s = append(s, &sNode{n: node.Right, min: node.Val, max: max})}
+        
     }
-    return dfs(math.MinInt64, math.MaxInt64, root)
+    return true
 }
