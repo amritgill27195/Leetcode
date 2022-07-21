b9f577d15dab9e62db2c6ce33d7b6088b0f2d346
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-21 17:24:59 -0400

    Time: 6 ms (68.48%), Space: 4.4 MB (41.86%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.go
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -7,32 +7,29 @@
  * }
  */
 func buildTree(preorder []int, inorder []int) *TreeNode {
+    if len(preorder) != len(inorder) {return nil}
     
     idxMap := map[int]int{}
-    for i := 0; i < len(inorder); i++ {
-        idxMap[inorder[i]] = i
-    }
+    for i := 0; i < len(inorder); i++ { idxMap[inorder[i]] = i }
     
-    preRootIdx := 0
-    
+    preRootPtr := 0
     var dfs func(left, right int) *TreeNode
     dfs = func(left, right int) *TreeNode {
         // base
-        if left > right {return nil}
+        if left > right {
+            return nil
+        }
         
         // logic
-        rootVal := preorder[preRootIdx]
+        rootVal := preorder[preRootPtr]
+        preRootPtr++
         root := &TreeNode{Val: rootVal}
         rootIdx := idxMap[rootVal]
-        preRootIdx++
-        
-        root.Left = dfs(left,rootIdx-1)
+        root.Left = dfs(left, rootIdx-1)
         root.Right = dfs(rootIdx+1, right)
-        return root
         
+        return root
     }
     
     return dfs(0, len(preorder)-1)
-    
-    
 }
