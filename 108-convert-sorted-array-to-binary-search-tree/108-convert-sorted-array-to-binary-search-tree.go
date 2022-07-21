c16fce1d46462b5c7a67f5c3c13fbe4b40e6ea2b
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-21 18:03:29 -0400

    Time: 3 ms (77.14%), Space: 3.5 MB (76.88%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.go
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.go
@@ -6,19 +6,36 @@
  *     Right *TreeNode
  * }
  */
-func sortedArrayToBST(nums []int) *TreeNode {
-    var dfs func(n []int) *TreeNode
-    dfs = func(n []int) *TreeNode {
-        // base
-        if n == nil || len(n) == 0 {return nil}
+// func sortedArrayToBST(nums []int) *TreeNode {
+//     var dfs func(n []int) *TreeNode
+//     dfs = func(n []int) *TreeNode {
+//         // base
+//         if n == nil || len(n) == 0 {return nil}
         
+//         // logic
+//         rootIdx := len(n) / 2
+//         root := &TreeNode{Val: n[rootIdx]}
+//         root.Left = dfs(n[:rootIdx])
+//         root.Right = dfs(n[rootIdx+1:])
+//         return root
+//     }
+    
+//     return dfs(nums)
+// }
+
+func sortedArrayToBST(nums []int) *TreeNode {
+    var dfs func(left, right int) *TreeNode
+    dfs = func(left, right int) *TreeNode {
+        // base
+        if left > right {return nil}
         // logic
-        rootIdx := len(n) / 2
-        root := &TreeNode{Val: n[rootIdx]}
-        root.Left = dfs(n[:rootIdx])
-        root.Right = dfs(n[rootIdx+1:])
+
+        rootIdx := left + (right-left)/2 
+        root := &TreeNode{Val: nums[rootIdx]}
+        root.Left = dfs(left, rootIdx-1)
+        root.Right = dfs(rootIdx+1, right)
         return root
     }
     
-    return dfs(nums)
+    return dfs(0, len(nums)-1)
 }
