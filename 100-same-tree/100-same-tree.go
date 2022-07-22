c26570cacb6ae34e14237225c2caf2b28a6fe8fd
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-22 16:40:11 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (69.83%) - LeetHub

--- 100-same-tree/100-same-tree.go
+++ 100-same-tree/100-same-tree.go
@@ -8,14 +8,13 @@
  */
 func isSameTree(p *TreeNode, q *TreeNode) bool {
     var dfs func(a, b *TreeNode) bool
-    dfs = func(a,b *TreeNode) bool {
+    dfs = func(a, b *TreeNode) bool {
         // base
-        if (a == nil && b != nil) || (a != nil && b == nil) { return false }
-        if a == nil && b == nil {return true}
-        if a.Val != b.Val {return false}
         
         // logic
-        return dfs(a.Left, b.Left) && dfs(a.Right, b.Right)
+        if a == nil && b == nil {return true}
+        if a == nil || b == nil {return false}
+        return a.Val == b.Val && dfs(a.Left, b.Left) && dfs(a.Right, b.Right)
     }
     return dfs(p,q)
 }
