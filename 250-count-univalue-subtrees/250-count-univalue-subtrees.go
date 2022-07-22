8d42ad32558c30c2bb3c24490fee903822e68a87
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-22 12:51:46 -0400

    Time: 9 ms (8.06%), Space: 3 MB (8.06%) - LeetHub

--- 250-count-univalue-subtrees/250-count-univalue-subtrees.go
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.go
@@ -11,36 +11,33 @@ func countUnivalSubtrees(root *TreeNode) int {
     var dfs func(r *TreeNode) bool
     dfs = func(r *TreeNode) bool {
         // base
-        if r == nil {return true}
+        if r == nil {
+            return true
+        }
         
+        
+        // logic
         left := dfs(r.Left)
         right := dfs(r.Right)
-        
         if left && right {
             
             if r.Left == nil && r.Right == nil {
                 count++
                 return true
-            } else if r.Left != nil && r.Right != nil {
-                if r.Left.Val == r.Val && r.Val == r.Right.Val {
-                    count++
-                    return true
-                }
-            } else if r.Left == nil {
-                if r.Val == r.Right.Val {
-                    count++
-                    return true
-                }
-            } else if r.Right == nil {
-                if r.Val == r.Left.Val {
-                    count++
-                    return true
-                }
             }
             
+            if r.Left != nil && r.Right != nil && r.Val == r.Left.Val && r.Val == r.Right.Val {
+                count++
+                return true
+            } else if r.Left == nil && r.Val == r.Right.Val {
+                count++
+                return true
+            } else if r.Right == nil && r.Val == r.Left.Val {
+                count++
+                return true
+            }
         }
         return false
-        
     }
     dfs(root)
     return count
