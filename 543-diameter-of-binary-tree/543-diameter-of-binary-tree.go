4fb0f10ebec1f47bc046c4d35ea650465d82daad
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-07-22 13:44:16 -0400

    Time: 10 ms (29.98%), Space: 4.5 MB (11.74%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.go
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.go
@@ -7,33 +7,21 @@
  * }
  */
 func diameterOfBinaryTree(root *TreeNode) int {
-    max := 0
-    var dfs func(r *TreeNode) int 
+    var dfs func(r *TreeNode) int
+    sum := 0
     dfs = func(r *TreeNode) int {
-        // bottom up recursion 
-        // child returns its height back to parent
-        // parent gets height from both childs
-        // adds them up
-        // and saves it if sum > max
-        // and then returns the max back to its parent
-        
         // base
         if r == nil {
             return 0
         }
         
-        
-        // logic
         left := dfs(r.Left)
         right := dfs(r.Right)
-        cmax := left+right
-        if cmax > max {
-            max = cmax
+        if left+right > sum {
+            sum = left+right
         }
-        
         return int(math.Max(float64(left), float64(right)))+1
-        
     }
     dfs(root)
-    return max
+    return sum
 }
